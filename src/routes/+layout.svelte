94fb7d4e2c0c8cb318e2831d3d3fe9f2222297aa
Georgios Jason Efstathiou <j@jason-e.dev>
2022-09-13 15:23:54 +0200

    feat: auto dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,33 @@
 <script>
+  import { onMount } from 'svelte';
+
   import '../app.css';
   import 'radicle-design-system/static/reset.css';
   import 'radicle-design-system/static/global.css';
   import 'radicle-design-system/static/colors.css';
   import 'radicle-design-system/static/elevation.css';
   import 'radicle-design-system/static/typography.css';
+
+  let prefersDarkMode = false;
+
+  onMount(() => {
+    const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    prefersDarkMode = darkModeQuery.matches;
+
+    darkModeQuery.addEventListener('change', (event) => {
+      prefersDarkMode = event.matches;
+    });
+  });
 </script>
 
-<div data-theme="light">
+<div class="main" data-theme={prefersDarkMode ? 'dark' : 'light'}>
   <slot />
 </div>
+
+<style>
+  .main {
+    min-height: 100vh;
+    min-width: 100vw;
+    background-color: var(--color-background);
+  }
+</style>
