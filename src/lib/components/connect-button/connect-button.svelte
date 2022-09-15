92d74615f8d79d510eef9d24f1326a1ef116dcf0
Georgios Jason Efstathiou <j@jason-e.dev>
2022-09-15 18:47:50 +0200

    feat: ens store

--- src/lib/components/connect-button/connect-button.svelte
+++ src/lib/components/connect-button/connect-button.svelte
@@ -1,10 +1,31 @@
 <script lang="ts">
   import Button from 'radicle-design-system/Button.svelte';
-  import walletStore from '$lib/stores/wallet';
+  import wallet from '$lib/stores/wallet';
+  import ens from '$lib/stores/ens';
+
+  async function connect() {
+    await wallet.connect();
+  }
+
+  $: {
+    if ($wallet?.connected) {
+      const { provider, address } = $wallet ?? {};
+      if (!provider || !address) throw new Error('No provider after connection');
+
+      // Connect all stores to wallet
+      ens.connect(provider);
+
+      // Lookup own name
+      ens.lookup(address);
+    } else {
+      // Disconnect all stores from wallet
+      ens.disconnect();
+    }
+  }
 </script>
 
-{#if $walletStore?.connected}
-  <Button on:click={() => walletStore.disconnect()}>Disconnect wallet</Button>
+{#if $wallet?.connected}
+  <Button on:click={wallet.disconnect}>Disconnect wallet</Button>
 {:else}
-  <Button on:click={() => walletStore.connect()}>Connect wallet</Button>
+  <Button on:click={connect}>Connect wallet</Button>
 {/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
 <script>
   import ConnectButton from '$lib/components/connect-button/connect-button.svelte';
+  import ens from '$lib/stores/ens';
   import walletStore from '$lib/stores/wallet';
 </script>
 
 <h1 class="typo-header-1">Hello new Drips app!</h1>
 <ConnectButton />
 {#if $walletStore?.connected}
-  Current address: {$walletStore?.address}
+  <p>Current address: {$walletStore.address}</p>
+  <p>ENS: {$ens[$walletStore.address]?.name ?? 'None'}</p>
 {/if}
 
 <style>
