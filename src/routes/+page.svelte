a2b52f8bf85d117f344a5f88ecc4edfa9ec8ddb0
Georgios Jason Efstathiou <j@jason-e.dev>
2022-09-22 11:39:14 +0200

    feat: add button to component showcase

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,13 @@
-<script>
+<script lang="ts">
+  import PlusIcon from 'radicle-design-system/icons/Plus.svelte';
+
   import ConnectButton from '$lib/components/connect-button/connect-button.svelte';
   import ens from '$lib/stores/ens';
   import walletStore from '$lib/stores/wallet';
+  import Button from '$lib/components/button/button.svelte';
+
+  // Button
+  let disabled = false;
 </script>
 
 <h1 class="typo-header-1">Hello new Drips app!</h1>
@@ -11,8 +17,28 @@
   <p>ENS: {$ens[$walletStore.address]?.name ?? 'None'}</p>
 {/if}
 
+<h1>Component showcase</h1>
+
+<h2>Button</h2>
+<div>
+  <input id="button-disabled-checkbox" type="checkbox" bind:checked={disabled} />
+  <label for="button-disabled-checkbox">Disabled</label>
+</div>
+<Button {disabled} icon={PlusIcon}>Example button</Button>
+
 <style>
   h1 {
     color: var(--color-primary);
+    margin-top: 4rem;
+  }
+
+  h2 {
+    color: var(--color-primary);
+    margin-top: 2rem;
+  }
+
+  p,
+  label {
+    color: var(--color-foreground-level-6);
   }
 </style>
