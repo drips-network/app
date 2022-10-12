e4eaba004d2ad405c097b6c4669dfd7f8b37423e
Georgios Jason Efstathiou <j@jason-e.dev>
2022-10-12 17:19:29 +0200

    feat: tick store

---
+++ src/lib/stores/tick/tick.unit.test.ts
@@ -0,0 +1,88 @@
+import tick from './tick.store';
+
+afterEach(() => {
+  tick.reset();
+  vi.useRealTimers();
+});
+
+describe('tick', () => {
+  it('starts after being started', () => {
+    tick.start();
+
+    expect(tick.isRunning()).toBe(true);
+  });
+
+  it('stops after being reset', () => {
+    expect(tick.isRunning()).toBe(false);
+  });
+
+  it('returns a listener after adding it', () => {
+    const listener = vi.fn();
+
+    const registrationId = tick.register(listener);
+
+    expect(Object.keys(tick.getListeners()).length).toBe(1);
+    expect(tick.getListeners()[registrationId]).toBe(listener);
+  });
+
+  it('no longer returns a listener after removing it', () => {
+    const listener = vi.fn();
+
+    const registrationId = tick.register(listener);
+    tick.deregister(registrationId);
+
+    expect(Object.keys(tick.getListeners()).length).toBe(0);
+    expect(tick.getListeners()[registrationId]).toBe(undefined);
+  });
+
+  it('calls registered listeners every 100ms while running', async () => {
+    vi.useFakeTimers();
+
+    const listener1 = vi.fn();
+
+    const regId1 = tick.register(listener1);
+    tick.start();
+
+    vi.advanceTimersByTime(1500);
+
+    expect(listener1).toBeCalledTimes(15);
+
+    const listener2 = vi.fn();
+    const regId2 = tick.register(listener2);
+
+    vi.advanceTimersByTime(1000);
+
+    expect(listener1).toBeCalledTimes(25);
+    expect(listener2).toBeCalledTimes(10);
+
+    tick.stop();
+
+    vi.advanceTimersByTime(1000);
+
+    expect(listener1).toBeCalledTimes(25);
+    expect(listener2).toBeCalledTimes(10);
+
+    tick.start();
+
+    vi.advanceTimersByTime(1500);
+
+    expect(listener1).toBeCalledTimes(40);
+    expect(listener2).toBeCalledTimes(25);
+
+    tick.deregister(regId1);
+
+    vi.advanceTimersByTime(1000);
+
+    expect(listener1).toBeCalledTimes(40);
+    expect(listener2).toBeCalledTimes(35);
+
+    tick.deregister(regId2);
+
+    vi.advanceTimersByTime(1000);
+
+    expect(listener1).toBeCalledTimes(40);
+    expect(listener2).toBeCalledTimes(35);
+  });
+});
+
+export {};
