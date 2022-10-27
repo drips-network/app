f96c62a5a672de58fa820e3fe91079716dc138ec
evvvritt <evv.info@gmail.com>
2022-10-27 12:33:57 +0200

    style: split table lozenge text white always

--- src/lib/components/splits-table/splits-table-split-lozenge.svelte
+++ src/lib/components/splits-table/splits-table-split-lozenge.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <div
-  class="split-table-split-lozenge rounded-full py-0.5 px-1.5 typo-text-small-bold"
-  style="background: var(--color-primary); color: var(--color-foreground)}"
+  class="split-table-split-lozenge rounded-full py-0.5 px-1.5 typo-text-small-bold text-white"
+  style="background: var(--color-primary);}"
 >
   {text}
 </div>
