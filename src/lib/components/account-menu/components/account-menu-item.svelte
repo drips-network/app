20bbe6600e60017abbb345d4d4de0d0725e2db4e
Jason Efstathiou <j@jason-e.dev>
2022-10-31 12:34:24 +0100

    feat: account menu, settings page stub (#35)
    * feat: account menu, settings page
    
    * chore: upgrade radicle-design-system dep
    
    * chore: upgrade drips SDK
    
    * refactor: use a elem as link in account menu

---
+++ src/lib/components/account-menu/components/account-menu-item.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+  import ChevronRight from 'radicle-design-system/icons/ChevronRight.svelte';
+  import type { SvelteComponent } from 'svelte';
+
+  export let disabled = false;
+  export let icon: typeof SvelteComponent | undefined = undefined;
+  export let href: string | undefined = undefined;
+</script>
+
+<a {href} class:disabled class:clickable={href} class="account-menu-item-wrapper">
+  <slot name="left">
+    <div class="icon-wrapper">
+      {#if icon}<svelte:component this={icon} style="fill: var(--color-primary)" />{/if}
+    </div>
+  </slot>
+  <div class="description typo-text-bold">
+    <slot name="title" />
+  </div>
+  <slot name="right">
+    <ChevronRight />
+  </slot>
+</a>
+
+<style>
+  .account-menu-item-wrapper {
+    display: flex;
+    gap: 0.5rem;
+    align-items: center;
+    padding: 0.25rem;
+    border-radius: 0.25rem;
+    transition: background-color 0.3s;
+  }
+
+  .account-menu-item-wrapper.clickable {
+    cursor: pointer;
+  }
+
+  .account-menu-item-wrapper.clickable:hover {
+    background-color: var(--color-foreground-level-1);
+  }
+
+  .account-menu-item-wrapper.clickable:focus {
+    background-color: var(--color-foreground-level-2);
+    outline: none;
+  }
+
+  .account-menu-item-wrapper.disabled {
+    opacity: 0.5;
+  }
+
+  .icon-wrapper {
+    height: 3rem;
+    width: 3rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: var(--color-primary-level-1);
+    border-radius: 1.5rem;
+  }
+
+  .description {
+    flex: 1;
+    color: var(--color-foreground-level-6);
+  }
+</style>
---
+++ src/lib/components/divider/divider.svelte
@@ -0,0 +1,9 @@
+<div class="divider" />
+
+<style>
+  .divider {
+    height: 0.1rem;
+    border-radius: 0.25rem;
+    background-color: var(--color-foreground-level-2);
+  }
+</style>
