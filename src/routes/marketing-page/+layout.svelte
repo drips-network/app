91e6a8c66236dc3fec40ae373006d3b3ab64b894
Georgios Jason Efstathiou <j@jason-e.dev>
2022-11-10 11:06:01 +0100

    chore: temporarily move marketing page to /marketing-page

---
+++ src/routes/marketing-page/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  // Global CSS imports
+  import 'radicle-design-system/static/reset.css';
+  import 'radicle-design-system/static/global.css';
+  import 'radicle-design-system/static/colors.css';
+  import 'radicle-design-system/static/elevation.css';
+  import 'radicle-design-system/static/typography.css';
+  import '../styles/app.css';
+
+  import themeStore from '$lib/stores/theme/theme.store';
+</script>
+
+<div class="main" data-theme={$themeStore.currentTheme}>
+  <div class="page">
+    <slot />
+  </div>
+</div>
+
+<style>
+  .main {
+    min-height: 100vh;
+    min-width: 100vw;
+    background-color: var(--color-background);
+    color: var(--color-foreground);
+  }
+</style>
