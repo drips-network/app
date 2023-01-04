6eec231198757000191aa9d5dcd3e00a32312065
Georgios Jason Efstathiou <j@jason-e.dev>
2023-01-04 13:54:26 +0100

    fix: too tight spacing of details

--- src/routes/app/[userId]/tokens/[token]/streams/[dripId]/+page.svelte
+++ src/routes/app/[userId]/tokens/[token]/streams/[dripId]/+page.svelte
@@ -382,7 +382,7 @@
   .details {
     display: flex;
     flex-direction: column;
-    gap: 1rem;
+    gap: 1.5rem;
   }
 
   .key-value-row {
@@ -392,6 +392,7 @@
 
   .key-value {
     display: flex;
+    gap: 0.5rem;
     flex-direction: column;
   }
 
@@ -432,7 +433,7 @@
 
     .key-value-row {
       flex-direction: column;
-      gap: 1rem;
+      gap: 1.5rem;
     }
 
     .align-right {
