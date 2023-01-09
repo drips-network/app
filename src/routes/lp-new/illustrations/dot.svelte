3057a77da41a14e2b095fe627e892f00cd363aa7
Georgios Jason Efstathiou <j@jason-e.dev>
2023-01-09 16:10:52 +0100

    refactor: replace lp with rebuilt version

--- src/routes/lp-new/illustrations/dot.svelte
+++
@@ -1,9 +0,0 @@
-<svg width="21" height="21" viewBox="0 0 21 21" xmlns="http://www.w3.org/2000/svg">
-  <circle cx="10.5" cy="10.0857" r="10" />
-</svg>
-
-<style>
-  svg {
-    fill: var(--color-primary);
-  }
-</style>
