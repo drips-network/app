2298d6fd3c7ee24f016fbd5e1d6bfc1002a4ba21
Georgios Jason Efstathiou <j@jason-e.dev>
2023-01-24 10:09:44 +0100

    fix: glitchy animation on collect amounts modal

--- src/lib/components/line-items/line-items.svelte
+++ src/lib/components/line-items/line-items.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { flip } from 'svelte/animate';
   import { fade } from 'svelte/transition';
-  import TransitionedHeight from '../transitioned-height/transitioned-height.svelte';
   import LineItemComponent from './components/line-item.svelte';
   interface LineItem {
     title: string;
@@ -19,19 +18,17 @@
 </script>
 
 <div class="line-items">
-  <TransitionedHeight>
-    {#each lineItems as lineItem, index (lineItem.title + lineItem.subtitle + lineItem.symbol)}
-      <div
-        class="line-item"
-        in:fade|local={{ duration: 300 }}
-        out:fade|local={{ duration: 300 }}
-        animate:flip={{ duration: 300 }}
-      >
-        <LineItemComponent {animateValueChanges} {...lineItem} />
-        {#if index !== lineItems.length - 1}<div class="divider" />{/if}
-      </div>
-    {/each}
-  </TransitionedHeight>
+  {#each lineItems as lineItem, index (lineItem.title + lineItem.subtitle + lineItem.symbol)}
+    <div
+      class="line-item"
+      in:fade|local={{ duration: 300 }}
+      out:fade|local={{ duration: 300 }}
+      animate:flip={{ duration: 300 }}
+    >
+      <LineItemComponent {animateValueChanges} {...lineItem} />
+      {#if index !== lineItems.length - 1}<div class="divider" />{/if}
+    </div>
+  {/each}
 </div>
 
 <style>
