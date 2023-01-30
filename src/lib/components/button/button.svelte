fd408d9f02d6ee924ed61a0b553b093df2425257
Georgios Jason Efstathiou <j@jason-e.dev>
2023-01-30 15:44:45 +0100

    feat: 3d button

--- src/lib/components/button/button.svelte
+++ src/lib/components/button/button.svelte
@@ -15,15 +15,17 @@
   {disabled}
   on:click|stopPropagation
 >
-  {#if icon}
-    <svelte:component
-      this={icon}
-      style={variant === 'destructive' || variant === 'primary'
-        ? 'fill: white'
-        : 'fill: var(--color-foreground)'}
-    />
-  {/if}
-  <slot />
+  <div class="inner">
+    {#if icon}
+      <svelte:component
+        this={icon}
+        style={variant === 'destructive' || variant === 'primary'
+          ? 'fill: white'
+          : 'fill: var(--color-foreground)'}
+      />
+    {/if}
+    <slot />
+  </div>
 </button>
 
 <style>
@@ -32,17 +34,21 @@
     min-width: 2rem;
     border-radius: 1rem 0 1rem 1rem;
     color: var(--color-foreground);
+    user-select: none;
+    transition: background-color 0.3s, color 0.3s, transform 0.2s, box-shadow 0.2s, opacity 0.3s;
+  }
+
+  button .inner {
     display: flex;
     gap: 4px;
     align-items: center;
     justify-content: center;
-    user-select: none;
-    transition: background-color 0.3s, color 0.3s, transform 0.1s, box-shadow 0.3s, opacity 0.3s;
     white-space: nowrap;
+    transition: transform 0.2s;
   }
 
-  button.normal {
-    box-shadow: inset 0px 0px 0px 1px var(--color-foreground);
+  button:not(.ghost) {
+    box-shadow: var(--elevation-low);
   }
 
   button.primary {
@@ -63,27 +69,30 @@
     padding: 0 0.75rem;
   }
 
-  button.normal:enabled:hover,
-  button.normal:enabled:active {
-    background-color: var(--color-foreground-level-2);
+  button:enabled:hover,
+  button:enabled:focus {
+    box-shadow: 0px 0px 0px 1px var(--color-foreground), 0 2px 0px 1px var(--color-foreground),
+      inset 0 0px 0px 0px var(--color-foreground);
+    transform: translateY(-2px);
   }
 
-  button:enabled:hover,
   button:enabled:active {
-    box-shadow: inset 0px 0px 0px 2px var(--color-foreground);
+    transform: translateY(0px);
+    box-shadow: 0px 0px 0px 1px var(--color-foreground), 0 0px 0px 0px var(--color-foreground),
+      inset 0 2px 0px 0px var(--color-foreground);
+  }
+
+  button:enabled:active .inner {
+    transform: translateY(2px);
   }
 
   button:focus {
-    box-shadow: inset 0px 0px 0px 2.5px var(--color-foreground);
+    box-shadow: var(--elevation-low);
   }
 
   button.normal:focus {
     background-color: var(--color-foreground-level-1);
   }
-  button:enabled:active {
-    transform: scale(0.98);
-  }
-
   button:disabled {
     opacity: 0.5;
   }
