afe06d8115b047ee76a07ff5c778bde8d91eb678
Jason Efstathiou <j@jason-e.dev>
2023-02-07 17:48:20 +0100

    fix: #95, fix: #130 (#312)

--- src/routes/api/ipfs/pin/+server.ts
+++ src/routes/api/ipfs/pin/+server.ts
@@ -1,16 +1,25 @@
 import 'dotenv/config';
 import pinataSdk from '@pinata/sdk';
-import type { RequestEvent, RequestHandler } from '@sveltejs/kit';
+import { error, type RequestEvent, type RequestHandler } from '@sveltejs/kit';
 import getEnvVar from '$lib/utils/get-env-var';
+import { accountMetadataSchema } from '$lib/stores/streams/metadata';
 
 const pinata = pinataSdk(getEnvVar('PINATA_SDK_KEY'), getEnvVar('PINATA_SDK_SECRET'));
 
 export const POST: RequestHandler = async ({ request }: RequestEvent) => {
-  const res = await pinata.pinJSONToIPFS(await request.json(), {
-    pinataOptions: {
-      cidVersion: 0,
-    },
-  });
+  try {
+    const json = await request.json();
 
-  return new Response(res.IpfsHash);
+    accountMetadataSchema.parse(json);
+
+    const res = await pinata.pinJSONToIPFS(json, {
+      pinataOptions: {
+        cidVersion: 0,
+      },
+    });
+
+    return new Response(res.IpfsHash);
+  } catch (e) {
+    throw error(500, "This doesn't seem to be valid account metadata 🤨");
+  }
 };
