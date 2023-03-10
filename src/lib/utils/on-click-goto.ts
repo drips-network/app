19691ab6cc1258b6535f14a0e35d5ba50d583035
Jason Efstathiou <j@jason-e.dev>
2023-03-10 12:42:23 +0100

    fix: #346 (#351)

--- src/lib/utils/on-click-goto.ts
+++ src/lib/utils/on-click-goto.ts
@@ -10,7 +10,7 @@ import { goto } from '$app/navigation';
  * @param event The mouse event that triggered the interaction.
  */
 export default function (path: string, event: PointerEvent | MouseEvent) {
-  if (event.metaKey || event.ctrlKey) {
+  if (event.metaKey || event.ctrlKey || event.button === 1) {
     // Prevent handling if the event has already previoulsy hit an `a` element.
     if (event.composedPath().find((e) => e instanceof HTMLAnchorElement)) return;
 
