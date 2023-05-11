6bd4ff79da47bb1a385f35bf7fa99f1071e8bd2d
Georgios Jason Efstathiou <j@jason-e.dev>
2023-05-11 13:17:39 +0200

    test: configure subgraph-deployer image with repoDriver address

--- src/e2e-tests/docker/scripts/clone-subgraph.sh
+++ src/e2e-tests/docker/scripts/clone-subgraph.sh
@@ -6,7 +6,7 @@ git clone -b feature/dependency-funding --single-branch https://github.com/radic
 
 echo "📁 Creating subgraph config file for local env"
 cd ./drips-subgraph
-printf '{ "network": "goerli", "immutableSplitsDriver": { "address": "0x34466661145b6D19f32Ae0f4b2BFD3874573bdf0", "block": 1 }, "dripsHub": { "address": "0xa328B55BFF30EfF12591Cdfb3dcF4c12d804f583", "block": 1 }, "nftDriver": { "address": "0x55329C69414e88279a21c862b8195c1C64b4da96", "block": 1 },"metaData": { "address": "0xC58cEa5a448A761d2dE80DFa8BfE298780e9dd66", "block": 1 } }' > config.json
+printf '{ "network": "goerli", "immutableSplitsDriver": { "address": "0x34466661145b6D19f32Ae0f4b2BFD3874573bdf0", "block": 1 }, "repoDriver": { "address": "0x45Aa599e4EB38300025eAB83908652bcC61601D4", "block": 1 }, "dripsHub": { "address": "0xa328B55BFF30EfF12591Cdfb3dcF4c12d804f583", "block": 1 }, "nftDriver": { "address": "0x55329C69414e88279a21c862b8195c1C64b4da96", "block": 1 },"metaData": { "address": "0xC58cEa5a448A761d2dE80DFa8BfE298780e9dd66", "block": 1 } }' > config.json
 echo "🛠 Installing subgraph deps…"
 npm install
 
