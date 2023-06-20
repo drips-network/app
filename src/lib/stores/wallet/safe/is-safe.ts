5742ffe8953d4a5b9f10977501b291e90d3da62e
Jason Efstathiou <jason.efst@gmail.com>
2023-06-20 15:52:02 +0200

    fix: isSafe throwing error on sepolia

--- src/lib/stores/wallet/safe/is-safe.ts
+++ src/lib/stores/wallet/safe/is-safe.ts
@@ -6,6 +6,9 @@ const GNOSIS_API_SAFES_BASE: { [chainId: number]: string } = {
 };
 
 export async function isSafe(chainId: number, address: string) {
+  const apiBase = GNOSIS_API_SAFES_BASE[chainId];
+  if (!apiBase) return false;
+
   const res = await fetch(`${GNOSIS_API_SAFES_BASE[chainId]}/api/v1/safes/${address}`);
 
   return res.status === 200;
