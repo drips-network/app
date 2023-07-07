3665f22fe62f0cba97923e25101d856c221e8530
Jason Efstathiou <j@jason-e.dev>
2023-07-07 12:42:19 +0200

    feat: allow editing project metadata (#485)
    * feat: allow editing project metadata
    
    * chore: remove unnecessary prop
    
    * chore: remove unused css class

---
+++ src/lib/flows/edit-project-metadata/edit-project-metadata-steps.ts
@@ -0,0 +1,26 @@
+import { makeStep } from '$lib/components/stepper/types';
+import SuccessStep from '$lib/components/success-step/success-step.svelte';
+import { get } from 'svelte/store';
+import walletStore from '$lib/stores/wallet/wallet.store';
+import type { ClaimedGitProject } from '$lib/utils/metadata/types';
+import SetNewMetadata from './steps/set-new-metadata.svelte';
+
+export default (project: ClaimedGitProject) => ({
+  context: undefined,
+  steps: [
+    makeStep({
+      component: SetNewMetadata,
+      props: {
+        project,
+      },
+    }),
+    makeStep({
+      component: SuccessStep,
+      props: {
+        safeAppMode: Boolean(get(walletStore).safe),
+        message:
+          'Your project has successfully been updated. Please note that it may take some time for your dashboard to update.',
+      },
+    }),
+  ],
+});
---
+++ src/lib/flows/edit-project-metadata/steps/set-new-metadata.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+  import Button from '$lib/components/button/button.svelte';
+  import ProjectCustomizer from '$lib/components/project-customizer/project-customizer.svelte';
+  import StepLayout from '$lib/components/step-layout/step-layout.svelte';
+  import transact, { makeTransactPayload } from '$lib/components/stepper/utils/transact';
+  import RepoDriverMetadataManager from '$lib/utils/metadata/RepoDriverMetadataManager';
+  import type { ClaimedGitProject } from '$lib/utils/metadata/types';
+  import Wallet from 'radicle-design-system/icons/Wallet.svelte';
+  import { createEventDispatcher } from 'svelte';
+  import { writable } from 'svelte/store';
+  import assert from '$lib/utils/assert';
+  import MetadataManagerBase from '$lib/utils/metadata/MetadataManagerBase';
+  import { getRepoDriverClient } from '$lib/utils/get-drips-clients';
+  import type { StepComponentEvents } from '$lib/components/stepper/types';
+
+  const dispatch = createEventDispatcher<StepComponentEvents>();
+
+  export let project: ClaimedGitProject;
+
+  let projectWritable = writable(structuredClone(project));
+
+  $: changesMade =
+    project.emoji !== $projectWritable.emoji || project.color !== $projectWritable.color;
+
+  function submit() {
+    const metadataManager = new RepoDriverMetadataManager();
+
+    transact(
+      dispatch,
+      makeTransactPayload({
+        before: async () => {
+          const currentMetadata = (
+            await metadataManager.fetchAccountMetadata(project.repoDriverAccount.accountId)
+          )?.data;
+          assert(currentMetadata, 'No metadata found for account');
+
+          const newMetadata = {
+            ...currentMetadata,
+            emoji: $projectWritable.emoji,
+            color: $projectWritable.color,
+          };
+
+          const ipfsHash = await metadataManager.pinAccountMetadata(newMetadata);
+
+          const accountMetadataAsBytes = [
+            {
+              key: MetadataManagerBase.USER_METADATA_KEY,
+              value: ipfsHash,
+            },
+          ];
+
+          const repoDriverClient = await getRepoDriverClient();
+
+          const emitAccountMetadataTx = repoDriverClient.emitAccountMetadata(
+            project.repoDriverAccount.accountId,
+            accountMetadataAsBytes,
+          );
+
+          return { emitAccountMetadataTx };
+        },
+
+        transactions: ({ emitAccountMetadataTx }) => ({ transaction: () => emitAccountMetadataTx }),
+      }),
+    );
+  }
+</script>
+
+<StepLayout>
+  <ProjectCustomizer project={projectWritable} />
+  <svelte:fragment slot="actions">
+    <Button on:click={submit} disabled={!changesMade} variant="primary" icon={Wallet}
+      >Confirm changes in wallet</Button
+    >
+  </svelte:fragment>
+</StepLayout>
