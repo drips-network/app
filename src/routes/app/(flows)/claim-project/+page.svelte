3900131936820912c0294aa27553fd7b2c870722
Georgios Jason Efstathiou <j@jason-e.dev>
2023-07-10 11:10:44 +0200

    feat: confirm navigating away from project claim flow too

--- src/routes/app/(flows)/claim-project/+page.svelte
+++ src/routes/app/(flows)/claim-project/+page.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
   import MultiChain from '$lib/components/illustrations/multi-chain.svelte';
   import Stepper from '$lib/components/stepper/stepper.svelte';
+  import { onDestroy, onMount } from 'svelte';
   import StandaloneFlowSlots from '../components/standalone-flow-slots/standalone-flow-slots.svelte';
   import StandaloneFlowStepHeader from '../components/standalone-flow-step-header/standalone-flow-step-header.svelte';
   import { slotsTemplate, state, steps } from './claim-project-flow';
+  import { browser } from '$app/environment';
+
+  onMount(() => browser && (window.onbeforeunload = () => true));
+  onDestroy(() => browser && (window.onbeforeunload = null));
 
   let currentStepIndex = 0;
 
