ec02125a8ec794a4c7348cd33280eddfae45c7ef
Georgios Jason Efstathiou <j@jason-e.dev>
2023-07-12 10:36:09 +0200

    fix: remove useless "back to drips" button

--- src/routes/external/[url]/+page.svelte
+++ src/routes/external/[url]/+page.svelte
@@ -25,7 +25,6 @@
     </p>
     <p>Are you sure you want to continue?</p>
     <div class="actions">
-      <a href="https://drips.network/"><Button variant="normal">Back to Drips</Button></a>
       <a href={url.href}
         ><Button variant="destructive">Continue to <b class="typo-text-bold">{url.host}</b></Button
         ></a
