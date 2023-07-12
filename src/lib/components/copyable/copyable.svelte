a63a9858eaa24052b98ccf18dc3a59525af54ffe
Georgios Jason Efstathiou <j@jason-e.dev>
2023-07-12 13:25:18 +0200

    feat: make github url copyable on project profile

--- src/lib/components/copyable/copyable.svelte
+++ src/lib/components/copyable/copyable.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
   import CopyIcon from 'radicle-design-system/icons/CopySmall.svelte';
   import SuccessIcon from 'radicle-design-system/icons/CheckCircle.svelte';
+  import { fly } from 'svelte/transition';
 
   export let value: string;
   export let alwaysVisible = false;
+  export let disabled = false;
 
   let success = false;
   let visible = false;
@@ -18,6 +20,7 @@
 <div
   class="copyable"
   on:click={(e) => {
+    if (disabled) return;
     copyClipboard(value);
     e.stopPropagation();
   }}
@@ -28,12 +31,14 @@
   <slot />
   <div
     class="copy-icon"
-    class:visible={visible || success || alwaysVisible}
+    class:visible={!disabled && (visible || success || alwaysVisible)}
     class:animate={!alwaysVisible}
   >
     {#if success}
-      <SuccessIcon style="fill: var(--color-positive)" />
-    {:else}<CopyIcon />{/if}
+      <div transition:fly={{ duration: 300, y: 8 }}>
+        <SuccessIcon style="fill: var(--color-positive)" />
+      </div>
+    {:else}<div transition:fly={{ duration: 300, y: -8 }}><CopyIcon /></div>{/if}
   </div>
 </div>
 
@@ -41,12 +46,21 @@
   .copyable {
     gap: 0.25rem;
     display: flex;
+    align-items: center;
     cursor: pointer;
     justify-content: space-between;
   }
 
   .copy-icon {
     width: 0px;
+    height: 24px;
+    position: relative;
+  }
+
+  .copy-icon > div {
+    position: absolute;
+    top: 0;
+    left: 0;
   }
 
   .copy-icon.animate {
