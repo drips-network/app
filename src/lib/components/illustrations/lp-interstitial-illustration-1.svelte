8d8cbd58d16342da31005d53e86ea2b171e916b0
Jason Efstathiou <jason.efst@gmail.com>
2023-07-28 15:24:45 +0200

    fix: #602

--- src/lib/components/illustrations/lp-interstitial-illustration-1.svelte
+++ src/lib/components/illustrations/lp-interstitial-illustration-1.svelte
@@ -7,11 +7,11 @@
 >
   <path
     d="M288.25 670.5H329.25C342.505 670.5 353.25 681.245 353.25 694.5V754"
-    stroke="var(--color-foreground-level-2)"
+    stroke="var(--color-foreground)"
   />
   <path
     d="M54.0005 541.5L54.0005 206.5C54.0005 193.245 64.7456 182.5 78.0005 182.5H1014.5"
-    stroke="var(--color-foreground-level-2)"
+    stroke="var(--color-foreground)"
   />
   <path
     d="M640.871 186C641.606 186 642.525 185.658 643.828 184.959C644.695 184.495 645.686 183.9 646.862 183.194C647.229 182.973 647.615 182.742 648.02 182.5C647.615 182.258 647.229 182.027 646.862 181.806C645.686 181.1 644.695 180.505 643.828 180.041C642.525 179.342 641.606 179 640.871 179C639.211 179 637.5 180.577 637.5 182.5C637.5 184.423 639.211 186 640.871 186Z"
@@ -25,15 +25,15 @@
   />
   <path
     d="M54.0004 541.5L54.0004 197.5C54.0004 189.216 47.2846 182.5 39.0002 182.5V182.5C30.7158 182.5 24 175.784 24 167.5V41"
-    stroke="var(--color-foreground-level-2)"
+    stroke="var(--color-foreground)"
   />
   <path
     d="M54.0005 541.5L54.0005 646.5C54.0005 659.755 64.7457 670.5 78.0005 670.5H142C155.255 670.5 166 681.245 166 694.5V751.5"
-    stroke="var(--color-foreground-level-2)"
+    stroke="var(--color-foreground)"
   />
   <path
     d="M1352.5 670.5H190C176.745 670.5 166 681.245 166 694.5V751.5"
-    stroke="var(--color-foreground-level-2)"
+    stroke="var(--color-foreground)"
   />
   <rect x="990.5" y="158.114" width="47" height="47" rx="23.5" fill="#D8C7F1" />
   <image
@@ -43,14 +43,7 @@
     height="24"
     xlink:href="https://twemoji.maxcdn.com/v/13.0.0/72x72/1f47e.png"
   />
-  <rect
-    x="990.5"
-    y="158.114"
-    width="47"
-    height="47"
-    rx="23.5"
-    stroke="var(--color-foreground-level-2)"
-  />
+  <rect x="990.5" y="158.114" width="47" height="47" rx="23.5" stroke="var(--color-foreground)" />
   <rect x="30.5" y="571.277" width="47" height="47" rx="23.5" fill="#BCE8BB" />
   <image
     x="41"
@@ -59,14 +52,7 @@
     height="24"
     xlink:href="https://twemoji.maxcdn.com/v/13.0.0/72x72/1f422.png"
   />
-  <rect
-    x="30.5"
-    y="571.277"
-    width="47"
-    height="47"
-    rx="23.5"
-    stroke="var(--color-foreground-level-2)"
-  />
+  <rect x="30.5" y="571.277" width="47" height="47" rx="23.5" stroke="var(--color-foreground)" />
   <path
     d="M57.5 408.629C57.5 407.894 57.1576 406.975 56.4593 405.672C55.995 404.805 55.3998 403.814 54.694 402.638C54.4733 402.271 54.2418 401.885 54 401.48C53.7582 401.885 53.5267 402.271 53.306 402.638C52.6002 403.814 52.005 404.805 51.5407 405.672C50.8424 406.975 50.5 407.894 50.5 408.629C50.5 410.289 52.0767 412 54 412C55.9233 412 57.5 410.289 57.5 408.629Z"
     fill="var(--color-primary)"
@@ -85,17 +71,10 @@
     height="24"
     xlink:href="https://twemoji.maxcdn.com/v/13.0.0/72x72/1f9e2.png"
   />
-  <rect
-    x="1328.5"
-    y="646.5"
-    width="47"
-    height="47"
-    rx="23.5"
-    stroke="var(--color-foreground-level-2)"
-  />
+  <rect x="1328.5" y="646.5" width="47" height="47" rx="23.5" stroke="var(--color-foreground)" />
   <rect x="0.5" y="0.5" width="47" height="47" rx="23.5" fill="#FBE9BE" />
   <path d="M8 40.24H40V8.24H8V40.24Z" fill="url(#pattern3)" />
-  <rect x="0.5" y="0.5" width="47" height="47" rx="23.5" stroke="var(--color-foreground-level-2)" />
+  <rect x="0.5" y="0.5" width="47" height="47" rx="23.5" stroke="var(--color-foreground)" />
 </svg>
 
 <style>
