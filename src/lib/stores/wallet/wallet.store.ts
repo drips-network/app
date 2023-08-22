2c96609a56744dee8701bb07a1a38d8baf6ba1b9
Georgios Jason Efstathiou <j@jason-e.dev>
2023-08-22 13:35:52 +0200

    fix: wip

--- src/lib/stores/wallet/wallet.store.ts
+++ src/lib/stores/wallet/wallet.store.ts
@@ -280,7 +280,8 @@ const walletStore = () => {
 
 const mockWalletStore = () => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const address = (window as any).playwrightAddress ?? '0x433220a86126eFe2b8C98a723E73eBAd2D0CbaDc';
+  const address =
+    (browser && (window as any))?.playwrightAddress ?? '0x433220a86126eFe2b8C98a723E73eBAd2D0CbaDc';
   const provider = testnetMockProvider(address);
   const initialized = writable(false);
   const waitingForOnboard = writable(false);
