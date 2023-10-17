d044a4b62052d9f15991fc80e9a944fbe543f927
Georgios Jason Efstathiou <j@jason-e.dev>
2023-10-17 12:33:50 +0200

    feat: #711

---
+++ src/routes/app/(flows)/funder-onboarding/steps/connect-wallet/connect-wallet.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import AccountBox from '$lib/components/account-box/account-box.svelte';
+  import FormField from '$lib/components/form-field/form-field.svelte';
+  import StandaloneFlowStepLayout from '../../../components/standalone-flow-step-layout/standalone-flow-step-layout.svelte';
+  import walletStore from '$lib/stores/wallet/wallet.store';
+  import ArrowRightIcon from 'radicle-design-system/icons/ArrowRight.svelte';
+  import ArrowLeftIcon from 'radicle-design-system/icons/ArrowLeft.svelte';
+  import { createEventDispatcher } from 'svelte';
+  import type { StepComponentEvents } from '$lib/components/stepper/types';
+  import Button from '$lib/components/button/button.svelte';
+
+  const dispatch = createEventDispatcher<StepComponentEvents>();
+
+  $: formValid = $walletStore.connected;
+</script>
+
+<StandaloneFlowStepLayout
+  headline="Connect your wallet"
+  description="The wallet you connect will own the Drip List, and be able to edit it in the future."
+>
+  <FormField type="div">
+    <AccountBox />
+  </FormField>
+  <svelte:fragment slot="left-actions">
+    <Button icon={ArrowLeftIcon} on:click={() => dispatch('goBackward')}>Back</Button>
+  </svelte:fragment>
+  <svelte:fragment slot="actions">
+    <Button
+      disabled={!formValid}
+      icon={ArrowRightIcon}
+      variant="primary"
+      on:click={() => dispatch('goForward')}>Continue</Button
+    >
+  </svelte:fragment>
+</StandaloneFlowStepLayout>
