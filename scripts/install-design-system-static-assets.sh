8c883ece95a2ebb6140242d7d9871abf9dc8a751
Jason Efstathiou <j@jason-e.dev>
2023-10-19 12:35:34 +0200

    fix: untracked old otf inter fonts appear after every npm install (#748)

--- scripts/install-design-system-static-assets.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Install design system assets.
-
-set -Eeou pipefail
-echo "Installing Radicle Design System assets"
-cp ./node_modules/radicle-design-system/static/fonts/*.otf ./static/fonts
