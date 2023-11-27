0e11b39aa31b03d618edfd9bb9fb4b428260ed8d
Georgios Jason Efstathiou <j@jason-e.dev>
2023-11-27 11:24:43 +0100

    add simple transition to support buttons

---
+++ src/lib/components/support-card/components/support-buttons.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+  import Button from '$lib/components/button/button.svelte';
+  import walletStore from '$lib/stores/wallet/wallet.store';
+  import DripList from 'radicle-design-system/icons/DripList.svelte';
+  import Droplet from 'radicle-design-system/icons/Droplet.svelte';
+  import Heart from 'radicle-design-system/icons/Heart.svelte';
+  import TokenStreams from 'radicle-design-system/icons/TokenStreams.svelte';
+  import Wallet from 'radicle-design-system/icons/Wallet.svelte';
+  import { fade } from 'svelte/transition';
+
+  export let isOwner = false;
+  export let type: 'dripList' | 'project';
+
+  export let transitions = true;
+
+  export let onClickConnectWallet: (() => void) | undefined = undefined;
+  export let onClickNewStream: (() => void) | undefined = undefined;
+  export let onClickAddToDripList: (() => void) | undefined = undefined;
+  export let onClickNewDonation: (() => void) | undefined = undefined;
+
+  export let supportMenuOpen = false;
+</script>
+
+{#if !$walletStore.connected}
+  <Button on:click={onClickConnectWallet} size="large" icon={Wallet} variant="primary"
+    >Connect your wallet</Button
+  >
+{:else if !supportMenuOpen}
+  <div out:fade={transitions ? { duration: 300 } : { duration: 0 }} class="button-container">
+    <Button
+      variant="primary"
+      on:click={() => {
+        supportMenuOpen = true;
+      }}
+      icon={Heart}
+      size="large">Support</Button
+    >
+  </div>
+{:else}
+  <div
+    in:fade={transitions ? { duration: 300, delay: 300 } : { duration: 0 }}
+    class="button-container"
+  >
+    {#if isOwner && type === 'dripList'}
+      <Button on:click={onClickNewStream} size="large" icon={TokenStreams}>Support stream</Button>
+    {/if}
+    <Button on:click={onClickAddToDripList} size="large" icon={DripList}>Add to a Drip List</Button>
+    <Button size="large" icon={Droplet} on:click={onClickNewDonation}>Single donation</Button>
+  </div>
+{/if}
+
+<style>
+  .button-container {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    padding: 0.125rem 0;
+  }
+</style>
