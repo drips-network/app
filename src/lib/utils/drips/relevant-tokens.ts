3f2cd0f0325ae1456f419f728632ac7e673357d6
Georgios Jason Efstathiou <j@jason-e.dev>
2023-12-01 18:05:30 +0100

    handle collectable funds in unclaimed projects

--- src/lib/utils/drips/relevant-tokens.ts
+++ src/lib/utils/drips/relevant-tokens.ts
@@ -1,25 +1,44 @@
 import { getSubgraphClient } from '$lib/utils/get-drips-clients';
 import { Utils } from 'radicle-drips';
+import unreachable from '../unreachable';
 
 export default async function relevantTokens(
-  forBalance: 'receivable' | 'splittable',
+  forBalance: 'receivable' | 'splittable' | 'collectable',
   accountId: string,
 ) {
   const subgraph = getSubgraphClient();
 
   let assetIds: string[];
 
-  if (forBalance === 'receivable') {
-    assetIds = (await subgraph.getStreamReceiverSeenEventsByReceiverId(accountId)).map((e) =>
-      Utils.Asset.getAddressFromId(e.streamsSetEvent.assetId),
-    );
-  } else {
-    const events = await Promise.all([
-      subgraph.getGivenEventsByReceiverAccountId(accountId),
-      subgraph.getSplitEventsByReceiverAccountId(accountId),
-    ]);
+  switch (forBalance) {
+    case 'receivable':
+      assetIds = (await subgraph.getStreamReceiverSeenEventsByReceiverId(accountId)).map((e) =>
+        Utils.Asset.getAddressFromId(e.streamsSetEvent.assetId),
+      );
+      break;
+    case 'splittable':
+    case 'collectable': {
+      const events = (await Promise.all([
+        subgraph.getGivenEventsByReceiverAccountId(accountId),
+        subgraph.getSplitEventsByReceiverAccountId(accountId),
+        subgraph.getStreamReceiverSeenEventsByReceiverId(accountId),
+      ] as const)).flat();
 
-    assetIds = events.flat().map((e) => Utils.Asset.getAddressFromId(e.assetId));
+      assetIds = events.map((e) => {
+        let assetId: bigint;
+
+        if ('streamsSetEvent' in e) {
+          assetId = e.streamsSetEvent.assetId;
+        } else if ('assetId' in e) {
+          assetId = e.assetId;
+        } else {
+          unreachable();
+        }
+
+        return Utils.Asset.getAddressFromId(assetId);
+      });
+      break;
+    }
   }
 
   return new Set(assetIds);
