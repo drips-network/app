7611e54816425c4338eb07c897b5bef9b86caf9d
Georgios Jason Efstathiou <j@jason-e.dev>
2023-12-20 17:23:08 +0100

    fix unit tests

--- setup-test.js
+++ setup-test.js
@@ -1,4 +1,18 @@
 import matchers from '@testing-library/jest-dom/matchers';
-import { expect } from 'vitest';
+import { expect, vi } from 'vitest';
 
 expect.extend(matchers);
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
