627978a92ccd81005098662d32327437d9b804c8
Jason Efstathiou <jason.efst@gmail.com>
2024-01-04 15:37:11 +0100

    decap fix?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -55,7 +55,7 @@
       window.netlifyIdentity.on('init', (user) => {
         if (!user) {
           window.netlifyIdentity.on('login', () => {
-            document.location.href = '/admin/';
+            document.location.href = '/admin/index.html';
           });
         }
       });
