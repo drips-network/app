4f8a63a493e8a57e57f5a6d634eb5457c924cfc3
Georgios Jason Efstathiou <j@jason-e.dev>
2024-01-30 12:55:17 +0100

    fix settlement date calc

--- src/lib/utils/settlement-date.ts
+++ src/lib/utils/settlement-date.ts
@@ -1,11 +1,21 @@
 export default function nextSettlementDate() {
-  const lastDay = new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0);
+  const currentDate = new Date();
+
+  let lastDay = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
 
   if (lastDay.getDay() < 4) {
     lastDay.setDate(lastDay.getDate() - 7);
   }
 
   lastDay.setDate(lastDay.getDate() - (lastDay.getDay() - 4));
+
+  if (currentDate > lastDay) {
+    lastDay = new Date(currentDate.getFullYear(), currentDate.getMonth() + 2, 0);
+    if (lastDay.getDay() < 4) {
+      lastDay.setDate(lastDay.getDate() - 7);
+    }
+    lastDay.setDate(lastDay.getDate() - (lastDay.getDay() - 4));
+  }
 
   return lastDay;
 }
