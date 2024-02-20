eee3d4e69b316556b5377e10a09924d097fb0417
Everett <evv.info@gmail.com>
2024-02-20 13:37:30 +0100

    Update src/lib/components/aggregate-fiat-estimate/aggregate-fiat-estimate.svelte
    Co-authored-by: Jason Efstathiou <j@jason-e.dev>

--- src/lib/components/aggregate-fiat-estimate/aggregate-fiat-estimate.svelte
+++ src/lib/components/aggregate-fiat-estimate/aggregate-fiat-estimate.svelte
@@ -30,6 +30,12 @@
     }
   }
 
+  /**
+    Pass prices if they've been fetched externally already (e.g. in a load function).
+    If undefined, it will call in and wait for prices from `fiatEstimates` store.
+    
+    If you do pass prices, make sure that there's a value for each token address included in `amounts`.
+  */
   export let prices: Prices | undefined = undefined;
 
   $: priceStore = prices ? readable(prices) : fiatEstimates.price(tokenAddresses ?? []);
