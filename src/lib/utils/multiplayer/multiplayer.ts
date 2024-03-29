e2b19e8194c74c1ddc3c5cb82837debe62c2ff29
Jason Efstathiou <jason.efst@gmail.com>
2024-03-29 13:26:49 +0100

    generate dynamic share images for voting rounds

--- src/lib/utils/multiplayer/multiplayer.ts
+++ src/lib/utils/multiplayer/multiplayer.ts
@@ -303,3 +303,8 @@ export function getVotingRoundStatusReadable(
 
   return statusReadable;
 }
+
+export function isVotingRoundId(listId: string) {
+  // If the ID is a UUID, we can assume it's a voting round ID
+  return /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(listId);
+}
--- src/routes/app/(app)/drip-lists/[listId]/+page.server.ts
+++ src/routes/app/(app)/drip-lists/[listId]/+page.server.ts
@@ -15,11 +15,7 @@ import {
 export const load = (async ({ params, fetch }) => {
   const { listId } = params;
 
-  // If the ID is a UUID, we can assume it's a voting round ID
-  const isVotingRoundId =
-    /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(listId);
-
-  if (isVotingRoundId) {
+  if (multiplayer.isVotingRoundId(listId)) {
     let votingRound: VotingRound & { splits?: SplitsComponentSplitsReceiver[] };
     try {
       const votingRoundRes = await multiplayer.getVotingRound(listId, fetch);
