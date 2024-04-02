4f8aa4a40339414ad76e53841ffd0fb6ad764be5
Jason Efstathiou <jason.efst@gmail.com>
2024-04-02 11:10:19 +0200

    use self-hosted twemoji in app, jsdelivr on LP

--- src/lib/utils/twemoji.ts
+++ src/lib/utils/twemoji.ts
@@ -1,13 +1,14 @@
 import sanitize from 'sanitize-html';
 import twemoji from 'twemoji';
+import { BASE_URL } from './base-url';
 
 export default (...args: Parameters<typeof twemoji.parse>) => {
   const options = args[1] || {};
 
   return twemoji.parse(sanitize(args[0].toString()), {
     ...options,
+    base: BASE_URL,
     folder: '/twemoji',
     ext: '.svg',
-    base: '',
   });
 };
