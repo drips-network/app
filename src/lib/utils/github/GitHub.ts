725b311c928724b3b02fae8720b21cce3ee10e93
Holger Drewes <Holger.Drewes@gmail.com>
2024-04-02 10:15:38 +0200

    Compare lower-case versions of FUNDING.json to avoid errors when addresses are capitalized (#1046)

--- src/lib/utils/github/GitHub.ts
+++ src/lib/utils/github/GitHub.ts
@@ -49,7 +49,7 @@ export default class GitHub {
 
     const fundingJson = JSON.parse(fileContent);
 
-    if (JSON.stringify(fundingJson).replace(/\s/g, '') !== template.replace(/\s/g, '')) {
+    if (JSON.stringify(fundingJson).replace(/\s/g, '').toLowerCase() !== template.replace(/\s/g, '').toLowerCase()) {
       throw new Error('Invalid FUNDING.json file. Does it have the correct Ethereum address?');
     }
 
