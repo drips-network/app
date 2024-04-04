84d9c3e9ba2d3bea60eae66aad4bfd803ef3c447
Jason Efstathiou <jason.efst@gmail.com>
2024-04-04 17:32:46 +0200

    get rid of redis singleton

--- src/routes/api/redis.ts
+++ src/routes/api/redis.ts
@@ -1,41 +1,10 @@
-import redis, { type RedisClientType } from 'redis';
+import redis from 'redis';
 import { env } from '$env/dynamic/private';
-import assert from '$lib/utils/assert';
-
-let client: RedisClientType | undefined;
-
-if (env.CACHE_REDIS_CONNECTION_STRING) {
-  client = redis.createClient({ url: env.CACHE_REDIS_CONNECTION_STRING });
-
-  client.on('error', function (error) {
-    // eslint-disable-next-line no-console
-    console.error(error);
-  });
-
-  (async () => {
-    try {
-      await client.connect();
-    } catch (e) {
-      // eslint-disable-next-line no-console
-      console.error(e);
-    }
-  })();
-}
 
 export const getRedis = async () => {
-  assert(client);
+  const client = redis.createClient({ url: env.CACHE_REDIS_CONNECTION_STRING });
 
-  try {
-    await client.ping();
+  await client.connect();
 
-    return client;
-  } catch (e) {
-    await new Promise((resolve) => {
-      assert(client);
-
-      client.once('ready', resolve);
-    });
-
-    return client;
-  }
+  return client;
 };
