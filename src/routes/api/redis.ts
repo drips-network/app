6217f828f44cee576c81f682049534a42dc7d228
Jason Efstathiou <jason.efst@gmail.com>
2024-04-04 17:40:51 +0200

    add back redis error log

--- src/routes/api/redis.ts
+++ src/routes/api/redis.ts
@@ -4,6 +4,11 @@ import { env } from '$env/dynamic/private';
 export const getRedis = async () => {
   const client = redis.createClient({ url: env.CACHE_REDIS_CONNECTION_STRING });
 
+  client.on('error', (error) => {
+    // eslint-disable-next-line no-console
+    console.error(error);
+  });
+
   await client.connect();
 
   return client;
