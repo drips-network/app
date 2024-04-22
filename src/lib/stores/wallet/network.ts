ee4fd759e00e925d632bc4d4263b31318c615d41
Jason Efstathiou <jason.efst@gmail.com>
2024-04-22 11:04:14 +0200

    add new chain configs

--- src/lib/stores/wallet/network.ts
+++ src/lib/stores/wallet/network.ts
@@ -2,38 +2,48 @@ import { PUBLIC_NETWORK } from '$env/static/public';
 import assert from '$lib/utils/assert';
 import { BASE_URL } from '$lib/utils/base-url';
 
-const NETWORK_NAMES = {
+const SUPPORTED_CHAIN_IDS = [1, 80002, 11155420, 11155111] as const;
+export type ChainId = (typeof SUPPORTED_CHAIN_IDS)[number];
+
+export type ValueForEachSupportedChain<T> = Record<(typeof SUPPORTED_CHAIN_IDS)[number], T>;
+
+const NETWORK_NAMES: ValueForEachSupportedChain<string> = {
   [1]: 'homestead',
+  [80002]: 'amoy',
+  [11155420]: 'optimism-sepolia',
   [11155111]: 'sepolia',
 };
 
-const NETWORK_LABELS = {
+const NETWORK_LABELS: ValueForEachSupportedChain<string> = {
   [1]: 'Ethereum Mainnet',
+  [80002]: 'Polygon Amoy',
+  [11155420]: 'Optimism Sepolia',
   [11155111]: 'Ethereum Sepolia',
 };
 
-const NETWORK_TOKENS = {
+const NETWORK_TOKENS: ValueForEachSupportedChain<string> = {
   [1]: 'ETH',
+  [80002]: 'MATIC',
+  [11155420]: 'ETH',
   [11155111]: 'ETH',
 };
 
-const NETWORK_ID = {
+const NETWORK_ID: ValueForEachSupportedChain<string> = {
   [1]: '0x1',
+  [80002]: '0x13882',
+  [11155420]: '0xaa37dc',
   [11155111]: '0xaa36a7',
 };
 
-const RPC_URLS = {
+const RPC_URLS: ValueForEachSupportedChain<string> = {
   [1]: `${BASE_URL}/api/infura/mainnet`,
+  [80002]: `${BASE_URL}/api/infura/polygon-amoy`,
+  [11155420]: `${BASE_URL}/api/infura/optimism-sepolia`,
   [11155111]: `${BASE_URL}/api/infura/sepolia`,
-};
+} as const;
 
-// Networks currently supported by the app
-type ChainId = 1 | 11155111;
-
-function isSupportedChainId(chainId: number): chainId is ChainId {
-  const CHAIN_IDS = [1, 11155111];
-
-  return CHAIN_IDS.includes(chainId);
+export function isSupportedChainId(chainId: number): chainId is ChainId {
+  return SUPPORTED_CHAIN_IDS.includes(chainId as ChainId);
 }
 
 const configuredChainId = Number(PUBLIC_NETWORK);
