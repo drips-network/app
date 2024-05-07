29ddb97c3ccad2d056f3dfc33500ce6b19eab306
evvvritt <evv.info@gmail.com>
2024-05-07 15:47:53 +0200

    fix svg not rendering

--- src/lib/components/illustrations/lp-interstitial-illustration-1.svelte
+++ src/lib/components/illustrations/lp-interstitial-illustration-1.svelte
@@ -8,57 +8,56 @@
   fill="none"
   xmlns="http://www.w3.org/2000/svg"
   xmlns:xlink="http://www.w3.org/1999/xlink"
-  ><AnimateDripOnSvgPaths pathQueries={['M54.0005', 'M54.0004', 'M1352.5']}
-    >{@html `
-  <path
-    d="M54.0005 541.5L54.0005 206.5C54.0005 193.245 64.7456 182.5 78.0005 182.5H1014.5"
-    stroke="var(--color-foreground)"
-  />
-  <path
-    d="M54.0004 541.5L54.0004 197.5C54.0004 189.216 47.2846 182.5 39.0002 182.5V182.5C30.7158 182.5 24 175.784 24 167.5V41"
-    stroke="var(--color-foreground)"
-  />
-  <path
-    d="M54.0005 541.5L54.0005 646.5C54.0005 659.755 64.7457 670.5 78.0005 670.5H142C155.255 670.5 166 681.245 166 694.5V751.5"
-    stroke="var(--color-foreground)"
-  />
-  <path
-    d="M1352.5 670.5H190C176.745 670.5 166 681.245 166 694.5V751.5"
-    stroke="var(--color-foreground)"
-  />
-  <rect x="990.5" y="158.114" width="47" height="47" rx="23.5" fill="#D8C7F1" />
-  <image
-    x="1002"
-    y="170"
-    width="24"
-    height="24"
-    xlink:href="https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/72x72/1f47e.png"
-  />
-  <rect x="990.5" y="158.114" width="47" height="47" rx="23.5" stroke="var(--color-foreground)" />
-  <rect x="30.5" y="571.277" width="47" height="47" rx="23.5" fill="#BCE8BB" />
-  <image
-    x="41"
-    y="580"
-    width="24"
-    height="24"
-    xlink:href="https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/72x72/1f422.png"
-  />
-  <rect x="30.5" y="571.277" width="47" height="47" rx="23.5" stroke="var(--color-foreground)" />
-  <rect x="1328.5" y="646.5" width="47" height="47" rx="23.5" fill="#C4F4FB" />
-  <image
-    x="1340"
-    y="656"
-    width="24"
-    height="24"
-    xlink:href="https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/72x72/1f9e2.png"
-  />
-  <rect x="1328.5" y="646.5" width="47" height="47" rx="23.5" stroke="var(--color-foreground)" />
-  <rect x="0.5" y="0.5" width="47" height="47" rx="23.5" fill="#FBE9BE" />
-  <path d="M8 40.24H40V8.24H8V40.24Z" fill="url(#pattern3)" />
-  <rect x="0.5" y="0.5" width="47" height="47" rx="23.5" stroke="var(--color-foreground)" />
-`}</AnimateDripOnSvgPaths
-  ></svg
 >
+  <AnimateDripOnSvgPaths pathQueries={['M54.0005', 'M54.0004', 'M1352.5']}>
+    <path
+      d="M54.0005 541.5L54.0005 206.5C54.0005 193.245 64.7456 182.5 78.0005 182.5H1014.5"
+      stroke="var(--color-foreground)"
+    />
+    <path
+      d="M54.0004 541.5L54.0004 197.5C54.0004 189.216 47.2846 182.5 39.0002 182.5V182.5C30.7158 182.5 24 175.784 24 167.5V41"
+      stroke="var(--color-foreground)"
+    />
+    <path
+      d="M54.0005 541.5L54.0005 646.5C54.0005 659.755 64.7457 670.5 78.0005 670.5H142C155.255 670.5 166 681.245 166 694.5V751.5"
+      stroke="var(--color-foreground)"
+    />
+    <path
+      d="M1352.5 670.5H190C176.745 670.5 166 681.245 166 694.5V751.5"
+      stroke="var(--color-foreground)"
+    />
+    <rect x="990.5" y="158.114" width="47" height="47" rx="23.5" fill="#D8C7F1" />
+    <image
+      x="1002"
+      y="170"
+      width="24"
+      height="24"
+      xlink:href="https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/72x72/1f47e.png"
+    />
+    <rect x="990.5" y="158.114" width="47" height="47" rx="23.5" stroke="var(--color-foreground)" />
+    <rect x="30.5" y="571.277" width="47" height="47" rx="23.5" fill="#BCE8BB" />
+    <image
+      x="41"
+      y="580"
+      width="24"
+      height="24"
+      xlink:href="https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/72x72/1f422.png"
+    />
+    <rect x="30.5" y="571.277" width="47" height="47" rx="23.5" stroke="var(--color-foreground)" />
+    <rect x="1328.5" y="646.5" width="47" height="47" rx="23.5" fill="#C4F4FB" />
+    <image
+      x="1340"
+      y="656"
+      width="24"
+      height="24"
+      xlink:href="https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/72x72/1f9e2.png"
+    />
+    <rect x="1328.5" y="646.5" width="47" height="47" rx="23.5" stroke="var(--color-foreground)" />
+    <rect x="0.5" y="0.5" width="47" height="47" rx="23.5" fill="#FBE9BE" />
+    <path d="M8 40.24H40V8.24H8V40.24Z" fill="url(#pattern3)" />
+    <rect x="0.5" y="0.5" width="47" height="47" rx="23.5" stroke="var(--color-foreground)" />
+  </AnimateDripOnSvgPaths>
+</svg>
 
 <style>
   svg {
