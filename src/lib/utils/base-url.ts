45373964b264af04a089865788fec1908bc076bf
Jason Efstathiou <jason.efst@gmail.com>
2024-06-21 11:22:55 +0200

    wip

--- src/lib/utils/base-url.ts
+++ src/lib/utils/base-url.ts
@@ -1,10 +1,6 @@
 import { browser } from '$app/environment';
 import { env } from '$env/dynamic/public';
 
-/** INJECTED_DEPLOY_URL is replaced by Vite during build process. */
-const deployUrl =
-  'INJECTED_DEPLOY_URL'.toString() !== 'undefined' ? 'INJECTED_DEPLOY_URL' : undefined;
-
 export const BASE_URL = browser
   ? window.location.origin
-  : deployUrl ?? env.PUBLIC_BASE_URL ?? 'http://localhost:5173';
+  : INJECTED_DEPLOY_URL ?? env.PUBLIC_BASE_URL ?? 'http://localhost:5173';
