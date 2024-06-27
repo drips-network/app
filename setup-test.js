1dea317f7061aeb70081eab7daad9204b529d4b4
Jason Efstathiou <jason.efst@gmail.com>
2024-06-27 11:34:53 +0200

    fix e2e error

--- setup-test.js
+++ setup-test.js
@@ -14,3 +14,28 @@ Object.defineProperty(window, 'matchMedia', {
     dispatchEvent: vi.fn(),
   })),
 });
+
+class ESBuildAndJSDOMCompatibleTextEncoder extends TextEncoder {
+  constructor() {
+    super();
+  }
+
+  encode(input) {
+    if (typeof input !== 'string') {
+      throw new TypeError('`input` must be a string');
+    }
+
+    const decodedURI = decodeURIComponent(encodeURIComponent(input));
+    const arr = new Uint8Array(decodedURI.length);
+    const chars = decodedURI.split('');
+    for (let i = 0; i < chars.length; i++) {
+      arr[i] = decodedURI[i].charCodeAt(0);
+    }
+    return arr;
+  }
+}
+
+Object.defineProperty(global, 'TextEncoder', {
+  value: ESBuildAndJSDOMCompatibleTextEncoder,
+  writable: true,
+});
