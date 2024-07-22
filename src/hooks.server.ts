d67e110f4672f569fa27550270f26ae59a6b0daf
Ioannis Tourkogiorgis <jtourkos@gmail.com>
2024-07-22 16:16:18 +0200

    refactor: remove unnecessary comments

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -12,14 +12,8 @@ if (dsn) {
     tracesSampleRate: 0.1,
     profilesSampleRate: 0.1,
     integrations: [nodeProfilingIntegration()],
-
-    // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-    // spotlight: import.meta.env.DEV,
   });
 }
 
-// If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
 export const handle = sequence(sentryHandle());
-
-// If you have a custom error handler, pass it to `handleErrorWithSentry`
 export const handleError = handleErrorWithSentry();
