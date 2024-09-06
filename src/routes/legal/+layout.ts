33bbb6cd4b17e49cbd69720098095bca9055fd41
Ioannis Tourkogiorgis <jtourkos@gmail.com>
2024-09-06 13:14:29 +0300

    refactor: avoid page duplicates serving identical content for alternative chains

--- src/routes/legal/+layout.ts
+++ src/routes/legal/+layout.ts
@@ -1,1 +1,13 @@
+import { redirect } from '@sveltejs/kit';
+import getOptionalEnvVar from '$lib/utils/get-optional-env-var/public';
+
 export const prerender = true;
+
+export async function load({ route }) {
+  // TODO: Remove when we go full multi-chain.
+  const isAlternativeChain = getOptionalEnvVar('PUBLIC_ALTERNATIVE_CHAIN_MODE');
+  if (isAlternativeChain) {
+    // Serve from the `mainnet` instance
+    return redirect(308, `https://drips.network${route.id}`);
+  }
+}
