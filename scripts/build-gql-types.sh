40b2ea02c6e44642ba20618e4337f6ef628adbce
Jason Efstathiou <jason.efst@gmail.com>
2024-10-16 12:44:39 +0200

    fix checks

--- scripts/build-gql-types.sh
+++ scripts/build-gql-types.sh
@@ -1,43 +1,48 @@
+if [ "$SKIP_API_CHECK" = "true" ]; then
+  echo "Skipping API check..."
+  printf "\n"
+else
+  source .env
 
+  # Wait for API at env var GQL_URL to be up by repeatedly pinging /health for max 5 minutes
+  max_wait_time=300
 
-# Wait for API at env var GQL_URL to be up by repeatedly pining /health for max 5 minutes
-max_wait_time=300
+  api_url=$GQL_URL
 
-api_url=$GQL_URL
+  # Remove trailing slash if present
+  api_url=${api_url%/}
 
-#remove trailing slash if present
-api_url=${api_url%/}
+  # Start the timer
+  start_time=$(date +%s)
 
-# Start the timer
-start_time=$(date +%s)
+  echo "⏳ Waiting for GQL API at $api_url to be up before building GQL types..."
+  echo "⚠️ Wrong or missing GQL URL? Ensure GQL_URL is set in .env."
+  printf "\n"
 
-echo "⏳ Waiting for GQL API at $api_url to be up before building GQL types..."
-echo "⚠️ Wrong or missing GQL URL? Ensure GQL_URL is set in .env."
-printf "\n"
+  while true; do
+    # Ping the /health endpoint of the API
+    response=$(curl -s -o /dev/null -w "%{http_code}" "$api_url/health")
 
-while true; do
-  # Ping the /health endpoint of the API
-  response=$(curl -s -o /dev/null -w "%{http_code}" "$api_url/health")
+    echo "- Got $response"
 
-  echo "- Got $response"
+    if [ "$response" -eq 200 ]; then
+      printf "\n"
+      echo "✅ API is up! Building types..."
+      printf "\n"
+      break
+    fi
 
-  if [ "$response" -eq 200 ]; then
-    printf "\n"
-    echo "✅ API is up! Building types..."
-    printf "\n"
-    break
-  fi
+    # Check if the maximum wait time is reached
+    current_time=$(date +%s)
+    elapsed_time=$((current_time - start_time))
+    if [ "$elapsed_time" -ge "$max_wait_time" ]; then
+      echo "Timeout: API did not come up within $max_wait_time seconds."
+      exit 1
+    fi
 
-  # Check if the maximum wait time is reached
-  current_time=$(date +%s)
-  elapsed_time=$((current_time - start_time))
-  if [ "$elapsed_time" -ge "$max_wait_time" ]; then
-    echo "Timeout: API did not come up within $max_wait_time seconds."
-    exit 1
-  fi
-
-  sleep 1
-done
+    sleep 1
+  done
+fi
 
 # Purge any previously generated files
 find . -regex './src/.*__generated__.*' -type d -prune -exec rm -r "{}" \;
