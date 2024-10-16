b173cade352e5cf1d2dfa8d13c9edfd6779b77bc
Morgan Brown <git@email.mhgbrown.is>
2024-10-16 11:53:05 +0200

    Add docs

--- src/lib/flows/import-from-csv/parse-upload.ts
+++ src/lib/flows/import-from-csv/parse-upload.ts
@@ -1,11 +1,28 @@
 import { parseFile as parseCsv } from '$lib/utils/csv';
 
+/**
+ * Determine if at least on of the specified header names are in the header
+ * of the given parsed CSV file.
+ *
+ * @param parsedFile A parsed CSV file.
+ * @param csvHeaders A list of header names.
+ * @returns True if at least on the specified header names exists in the
+ *  header, false otherwise.
+ */
 function hasHeader(parsedFile: Array<Array<string>>, csvHeaders: Array<string>): boolean {
   const row = parsedFile[0];
-  // TODO: every?
   return row.some((column) => csvHeaders.includes(column.toLowerCase()));
 }
 
+/**
+ * Get the indicies of the columns in the provided parsed CSV file, which correspond to
+ * the given header names.
+ *
+ * @param parsedFile A parsed CSV file.
+ * @param csvHeaders A list of header names.
+ * @returns An array containing the indicies of the columns followed finally
+ *  by the row index in which to start reading values.
+ */
 function getFileLayout(parsedFile: Array<Array<string>>, csvHeaders: Array<string>): Array<number> {
   const firstRow = parsedFile[0];
 
@@ -24,6 +41,14 @@ function getFileLayout(parsedFile: Array<Array<string>>, csvHeaders: Array<strin
   return [...dataIndices, 1];
 }
 
+/**
+ * Parse a given CSV file, extracting the values specified by the
+ * given header names.
+ *
+ * @param file The CSV HTML File to be parsed
+ * @param csvHeaders The headers to be extracted
+ * @returns A promise that resolves into the extracted data.
+ */
 export async function parseFile(
   file: File,
   csvHeaders: Array<string>,
