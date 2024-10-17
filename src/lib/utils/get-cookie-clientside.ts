8bede6ca88d24a93de7357ee97749675013df982
Jason Efstathiou <j@jason-e.dev>
2024-10-17 12:10:17 +0200

    Add `knip` for detecting unused code, delete some unused code (#1265)
    * add `analyze-unused-code` script
    
    * remove unused components and utils
    
    * configure custom import path in knip for `$app`
    
    * delete commented-out import

--- src/lib/utils/get-cookie-clientside.ts
+++
@@ -1,7 +0,0 @@
-export default function getCookieClientSide(cookieName: string): string | undefined {
-  return document.cookie
-    .split(';')
-    .map((c) => c.trim())
-    .find((c) => c.startsWith(`${cookieName}=`))
-    ?.replace(`${cookieName}=`, '');
-}
--- src/lib/utils/is-valid-url.ts
+++
@@ -1,8 +0,0 @@
-export default function isValidUrl(input: string) {
-  try {
-    new URL(input);
-    return true;
-  } catch {
-    return false;
-  }
-}
