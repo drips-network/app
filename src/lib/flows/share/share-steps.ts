7fd44db101cf382ca74aa9b218bbb8c8ca6fe64f
Morgan Brown <git@email.mhgbrown.is>
2024-10-21 12:58:10 +0200

    Remove commented out code

--- src/lib/flows/share/share-steps.ts
+++ src/lib/flows/share/share-steps.ts
@@ -15,20 +15,6 @@ export type ShareOption = {
   onClick?: (this: ShareOption) => undefined;
 };
 
-// import SuccessStep from '$lib/components/success-step/success-step.svelte';
-// import { get } from 'svelte/store';
-// import walletStore from '$lib/stores/wallet/wallet.store';
-// import SetNewMetadata, { SET_NEW_METADATA_STEP_FRAGMENT } from './steps/set-new-metadata.svelte';
-// import { gql } from 'graphql-request';
-// import type { EditProjectMetadataFlowFragment } from './__generated__/gql.generated';
-
-// export const EDIT_PROJECT_METADATA_FLOW_FRAGMENT = gql`
-//   ${SET_NEW_METADATA_STEP_FRAGMENT}
-//   fragment EditProjectMetadataFlow on Project {
-//     ...SetNewMetadataStep
-//   }
-// `;
-
 export default (props: ShareUrlProps) => ({
   context: undefined,
   steps: [
@@ -36,12 +22,5 @@ export default (props: ShareUrlProps) => ({
       component: ShareUrl,
       props,
     }),
-    // makeStep({
-    //   component: SuccessStep,
-    //   props: {
-    //     safeAppMode: Boolean(get(walletStore).safe),
-    //     message: 'Your project has successfully been updated.',
-    //   },
-    // }),
   ],
 });
