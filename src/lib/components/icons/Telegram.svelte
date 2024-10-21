58ae88b8f23831d820ca2c6774b9b53b957494f4
Morgan Brown <git@email.mhgbrown.is>
2024-10-21 13:40:00 +0200

    Fix iconts in light mode

--- src/lib/components/icons/Telegram.svelte
+++ src/lib/components/icons/Telegram.svelte
@@ -5,32 +5,27 @@
 </script>
 
 <IconWrapper on:click {style}>
-  <g clip-path="url(#clip0_11749_27)">
+  <g>
     <path
       d="M7.5 12.6441L15.9619 20.0625C16.0593 20.1485 16.1775 20.2077 16.3047 20.2343C16.432 20.2608 16.5639 20.2539 16.6877 20.2141C16.8114 20.1743 16.9227 20.103 17.0106 20.0072C17.0985 19.9115 17.16 19.7945 17.1891 19.6678L21 3.11437C21.0037 3.09777 21.0029 3.08048 20.9975 3.06433C20.9921 3.04819 20.9825 3.03381 20.9696 3.02273C20.9567 3.01165 20.941 3.00429 20.9242 3.00145C20.9074 2.9986 20.8902 3.00037 20.8744 3.00656L1.875 10.4419C1.75705 10.4873 1.65701 10.5697 1.5899 10.6768C1.52278 10.7839 1.49221 10.9098 1.50278 11.0358C1.51334 11.1617 1.56447 11.2808 1.64849 11.3752C1.73251 11.4697 1.84489 11.5343 1.96875 11.5594L7.5 12.6441Z"
-      stroke="white"
+      stroke="var(--color-foreground)"
       stroke-linecap="round"
       stroke-linejoin="round"
       fill="transparent"
     />
     <path
       d="M7.5 12.6441L20.9447 3.00844"
-      stroke="white"
+      stroke="var(--color-foreground)"
       stroke-linecap="round"
       stroke-linejoin="round"
       fill="transparent"
     />
     <path
       d="M11.6597 16.2919L8.79 19.2694C8.68631 19.377 8.55285 19.4511 8.40673 19.4823C8.26061 19.5136 8.10849 19.5004 7.96989 19.4446C7.83128 19.3888 7.7125 19.2929 7.62878 19.1691C7.54507 19.0454 7.50022 18.8994 7.5 18.75V12.6441"
-      stroke="white"
+      stroke="var(--color-foreground)"
       stroke-linecap="round"
       stroke-linejoin="round"
       fill="transparent"
     />
   </g>
-  <defs>
-    <clipPath id="clip0_11749_27">
-      <rect width="24" height="24" fill="white" />
-    </clipPath>
-  </defs>
 </IconWrapper>
