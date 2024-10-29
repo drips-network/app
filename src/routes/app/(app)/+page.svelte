c53b474432a957b89ea835e970bc155fa0ca1bfd
Jason Efstathiou <j@jason-e.dev>
2024-10-29 13:15:57 +0100

    Fix: #1321, Fix: #1322 (#1323)
    * fix: #1321
    
    * fix: #1322

--- src/routes/app/(app)/+page.svelte
+++ src/routes/app/(app)/+page.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+  import HeadMeta from '$lib/components/head-meta/head-meta.svelte';
   import type { PageData } from './$types';
   import DefaultExplorePage from './components/default-explore-page.svelte';
   import FilecoinExplorePage from './components/filecoin-explore-page.svelte';
 
   export let data: PageData;
 </script>
+
+<HeadMeta title="Explore" />
 
 {#if data.explorePageVersion === 'filecoin'}
   <FilecoinExplorePage {...data} />
