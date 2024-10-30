fbfac4c4a5a0eb41c1a59c4990638b22def751dd
Morgan Brown <git@email.mhgbrown.is>
2024-10-30 15:44:05 +0200

    Use CSS to make background transparent

--- src/routes/api/embed/project/[projectUrl]/support/+page.svelte
+++ src/routes/api/embed/project/[projectUrl]/support/+page.svelte
@@ -1,32 +1,15 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
   import ProjectSupportButton from '$lib/components/project-support-button/project-support-button.svelte';
 
   export let data;
 
-  let backgroundPrepared = false;
+  let backgroundPrepared = true;
   // TODO: remove
   let buttonLoaded = true;
 
   function onLoad() {
     buttonLoaded = true;
   }
-
-  onMount(async () => {
-    // Ensure the background is transparent, so that puppeteer can capture
-    // a screenshot with a transparent background (with the additional omitBackground)
-    // configuration.
-    // There is a transition on the background that is removed by puppeteer when
-    // this page is rendered.
-    document.documentElement.style.background = 'transparent';
-    document.body.style.background = 'transparent';
-    const main = document.querySelector('.main') as HTMLElement;
-    if (main) {
-      main.style.background = 'transparent';
-    }
-
-    backgroundPrepared = true;
-  });
 </script>
 
 <div style:visibility={backgroundPrepared && buttonLoaded ? 'visible' : 'hidden'}>
