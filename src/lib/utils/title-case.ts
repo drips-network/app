1696ecc0beea2c34439fa387f97c7ff7d84af379
Morgan Brown <git@email.mhgbrown.is>
2024-11-01 18:13:26 +0100

    Add title case util

---
+++ src/lib/utils/title-case.ts
@@ -0,0 +1,6 @@
+export default function toTitleCase(str: string): string {
+  return str.replace(
+    /\w\S*/g,
+    (text) => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase(),
+  );
+}
