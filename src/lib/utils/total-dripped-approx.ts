c2f6e1dc0612b697255786587f856fb7ec1223ff
Jason Efstathiou <jason.efst@gmail.com>
2024-11-01 16:12:04 +0100

    update total dripped approx

--- src/lib/utils/total-dripped-approx.ts
+++ src/lib/utils/total-dripped-approx.ts
@@ -2,6 +2,8 @@ import cached from './cache/remote/cached';
 import isTest from './is-test';
 import type { RedisClientType } from '../../routes/api/redis';
 import { getCmcPrices } from './cmc';
+import mergeAmounts from './amounts/merge-amounts';
+import contractConstants from './sdk/utils/contract-constants';
 
 const STREAMS = [
   // ENS USDC
@@ -12,10 +14,19 @@ const STREAMS = [
     started: new Date('May 20, 2024, 10:04 PM').getTime(),
     amtPerSec: '3215277777777',
   },
+  // Second FTC list USDGLO (they have two lists for some reason) https://www.drips.network/app/drip-lists/36167722434539895740687283110259945938004377627588501179309095983175
+  {
+    token: {
+      address: '0x4f604735c1cf31399c6e711d5962b2b3e0225ad3',
+    },
+    started: new Date('March 8, 2024, 11:34 PM').getTime(),
+    amtPerSec: '385802469135802469135802',
+  },
 ];
 
 const GIVES = [
   // Octant https://drips.network/app/drip-lists/30178668158349445547603108732480118476541651095408979232800331391215
+  // Total of the stream which is now stopped
   {
     tokenAddress: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
     amount: BigInt('9380000000000000000'),
@@ -38,6 +49,24 @@ const GIVES = [
     tokenAddress: '0x31c8EAcBFFdD875c74b94b077895Bd78CF1E64A3',
     amount: BigInt('373134000000000000000000'),
   },
+  // Scroll Hackathon https://www.drips.network/app/drip-lists/41971962915943119138973997144514496143454239023249281594792952267407
+  // Total of the two one-time USDC donations
+  {
+    tokenAddress: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+    amount: BigInt('22500800698'),
+  },
+  // First FTC list USD (they have two for some reason) https://drips.network/app/drip-lists/36167722434539895740687283110259945938004377627588501179309095983174
+  // The 10k matching donation we did for their list
+  {
+    tokenAddress: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+    amount: BigInt('10000000000'),
+  },
+  // First FTC list WETH (they have two for some reason) https://drips.network/app/drip-lists/36167722434539895740687283110259945938004377627588501179309095983174
+  // Total of the stream which is now stopped
+  {
+    tokenAddress: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    amount: BigInt('100000000000000000'),
+  },
 ];
 
 export default function totalDrippedApproximation() {
@@ -46,11 +75,16 @@ export default function totalDrippedApproximation() {
 
     return {
       tokenAddress: stream.token.address.toLowerCase(),
-      amount: BigInt(Math.floor(Number(BigInt(stream.amtPerSec) / BigInt(1e9)) * duration)),
+      amount: BigInt(
+        Math.floor(
+          Number(BigInt(stream.amtPerSec) / BigInt(contractConstants.AMT_PER_SEC_MULTIPLIER)) *
+            duration,
+        ),
+      ),
     };
   });
 
-  return [...GIVES, ...streamedAmounts];
+  return mergeAmounts(GIVES, streamedAmounts);
 }
 
 const TOTAL_DRIPPED_PRICES_CACHE_KEY = 'total-dripped-prices';
