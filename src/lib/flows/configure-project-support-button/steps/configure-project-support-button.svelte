4f73e7fa0ac12f7626b9554349f08fc9e854df23
Morgan Brown <git@email.mhgbrown.is>
2024-11-01 18:13:59 +0100

    Get some initial styling and settings going; played with typescript for a while

--- src/lib/flows/configure-project-support-button/steps/configure-project-support-button.svelte
+++ src/lib/flows/configure-project-support-button/steps/configure-project-support-button.svelte
@@ -7,9 +7,52 @@
   import SegmentedControl from '$lib/components/segmented-control/segmented-control.svelte';
   import Divider from '$lib/components/divider/divider.svelte';
   import Setting from '../../../../routes/(pages)/app/(app)/settings/components/setting.svelte';
+  import {
+    SupportButtonBackground,
+    SupportButtonStat,
+    SupportButtonStyle,
+    SupportButtonText,
+    type SupportButtonOptions,
+  } from '$lib/components/project-support-button/project-support-button';
+  import toTitleCase from '$lib/utils/title-case';
 
   const headline = 'Configure your embed code';
   const description = 'Choose how you want your support button to be displayed.';
+
+  type Options<T> = Array<{ title: string; value: T[keyof T] }>;
+  function createOptions<T extends { [key: string]: string }>(
+    options: T,
+    skipKey: string = 'default',
+  ): Options<T> {
+    return Object.entries(options).reduce((memo, [key, value]) => {
+      if (key !== skipKey) {
+        memo.push({
+          title: toTitleCase(key),
+          value: value as T[keyof T],
+        });
+      }
+
+      return memo;
+    }, [] as Options<T>);
+  }
+
+  const backgrounds = createOptions(SupportButtonBackground);
+  const styles = createOptions(SupportButtonStyle);
+  const texts = createOptions(SupportButtonText);
+  const stats = createOptions(SupportButtonStat);
+
+  const selection: SupportButtonOptions = {
+    background: SupportButtonBackground.default,
+    style: SupportButtonStyle.default,
+    text: SupportButtonText.default,
+    stat: SupportButtonStat.default,
+  };
+
+  function makeOnSelect<K extends keyof SupportButtonOptions>(prop: K) {
+    return function <T extends CustomEvent>(event: T) {
+      selection[prop] = event.detail;
+    };
+  }
 
   function onClickCopy() {}
 
@@ -22,85 +65,42 @@
       <Pencil size={48} />
     </div>
     <StepHeader {headline} {description} />
-    <div class="section">
-      <Divider />
+    <Divider />
+    <div class="configure-project-support-button__section section">
       <h4 class="typo-all-caps">Style</h4>
       <Setting title="Background" subtitle="The background">
         <SegmentedControl
-          active="auto"
-          options={[
-            {
-              title: 'Dark',
-              value: 'auto',
-            },
-            {
-              title: 'Light',
-              value: 'light',
-            },
-            {
-              title: 'Blue',
-              value: 'dark',
-            },
-          ]}
+          active={selection.background}
+          on:select={makeOnSelect('background')}
+          options={backgrounds}
         />
       </Setting>
       <Setting title="Button text" subtitle="Show project, me, or us">
         <SegmentedControl
-          active="auto"
-          options={[
-            {
-              title: 'Project',
-              value: 'auto',
-            },
-            {
-              title: 'Me',
-              value: 'light',
-            },
-            {
-              title: 'Us',
-              value: 'dark',
-            },
-          ]}
+          active={selection.text}
+          on:select={makeOnSelect('text')}
+          options={texts}
         />
       </Setting>
       <Setting title="Style" subtitle="The general styling">
         <SegmentedControl
-          active="auto"
-          options={[
-            {
-              title: 'Github',
-              value: 'auto',
-            },
-            {
-              title: 'Drips',
-              value: 'light',
-            },
-          ]}
+          active={selection.style}
+          on:select={makeOnSelect('style')}
+          options={styles}
         />
       </Setting>
       <Setting title="Stat" subtitle="The live-updated stat">
         <SegmentedControl
-          active="auto"
-          options={[
-            {
-              title: 'Support',
-              value: 'auto',
-            },
-            {
-              title: 'Dependencies',
-              value: 'light',
-            },
-            {
-              title: 'None',
-              value: 'light',
-            },
-          ]}
+          active={selection.stat}
+          on:select={makeOnSelect('stat')}
+          options={stats}
         />
       </Setting>
       <Divider />
     </div>
-    <div class="section">
+    <div class="configure-project-support-button__section section">
       <h4 class="typo-all-caps">Preview</h4>
+      <div class="configure-project-support-button_preview">insert the button here</div>
     </div>
   </div>
 
@@ -120,7 +120,21 @@
     margin-bottom: 1.5rem;
   }
 
-  .configure-project-support-button .section {
+  .configure-project-support-button__section {
     text-align: left;
+    display: flex;
+    flex-direction: column;
+    gap: 2.5rem;
+  }
+
+  .configure-project-support-button_preview {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 1rem 0px 1rem 1rem;
+    border: 1px 0px 0px 0px;
+    padding: 44px 46px;
+    background-color: var(--color-foreground);
+    color: var(--color-background);
   }
 </style>
