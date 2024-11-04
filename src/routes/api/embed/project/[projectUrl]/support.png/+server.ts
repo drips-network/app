35f346a45f8ef988f134bc6c9fee5b602f1c3e93
Morgan Brown <git@email.mhgbrown.is>
2024-11-04 16:42:09 +0100

    See if explicitly waiting for fonts to load will do the trick

--- src/routes/api/embed/project/[projectUrl]/support.png/+server.ts
+++ src/routes/api/embed/project/[projectUrl]/support.png/+server.ts
@@ -80,7 +80,29 @@ export const GET: RequestHandler = async ({ url, params }) => {
     });
 
     // Navigate to the page rendering the button
-    await page.goto(imageUrl);
+    await page.goto(imageUrl, { waitUntil: 'domcontentloaded' });
+    // Wait until all images and fonts have loaded
+    await page.evaluate(async () => {
+      const selectors = Array.from(document.querySelectorAll('img'));
+      await Promise.all([
+        document.fonts.ready,
+        ...selectors.map((img) => {
+          // Image has already finished loading, let’s see if it worked
+          if (img.complete) {
+            // Image loaded and has presence
+            if (img.naturalHeight !== 0) return;
+            // Image failed, so it has no height
+            throw new Error('Image failed to load');
+          }
+          // Image hasn’t loaded yet, added an event listener to know when it does
+          return new Promise((resolve, reject) => {
+            img.addEventListener('load', resolve);
+            img.addEventListener('error', reject);
+          });
+        }),
+      ]);
+    });
+
     const selector = '.support-button';
     const element = await page.waitForSelector(selector);
     // if there's no element for any reason, that's
