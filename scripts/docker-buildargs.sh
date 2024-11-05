104a381ee2314b73f3a4dec834703477389b9b44
Morgan Brown <git@email.mhgbrown.is>
2024-11-05 15:59:33 +0100

    Enhance some comments

--- scripts/docker-buildargs.sh
+++ scripts/docker-buildargs.sh
@@ -1,4 +1,5 @@
 # !/bin/bash
 
-# Given an env file, remove blank lines and comments and turn those lines into build args.
+# Given an env file, remove blank lines and comments and turn
+# those lines into "--build-arg X=Y --build-arg Z=W ..."
 grep -v -e '^[[:space:]]*$' -e '^#' $@ | awk '{ sub ("\\\\$", " "); printf " --build-arg %s", $0  } END { print ""  }'
