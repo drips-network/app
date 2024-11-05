be7e57e4319068aa308e529c4965edbc83253065
Morgan Brown <git@email.mhgbrown.is>
2024-11-05 13:13:22 +0100

    Run development environment locally with docker

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -2,9 +2,13 @@
 
 # cd to the scripts directory
 cd "$(dirname "$(realpath -- "$0")")"
+
 # print the current directory
 pwd
+
 # echo the command we're about to run
-echo $(./docker-buildargs.sh ../.env)
+CMD="docker build $(./docker-buildargs.sh ../.env) ../. -f ../Dockerfile.dev -t drips-app --platform=linux/amd64"
+echo $CMD
+
 # run the command
-docker build $(./docker-buildargs.sh ../.env) ../. -t drips-app --platform=linux/amd64
+eval "$CMD"
