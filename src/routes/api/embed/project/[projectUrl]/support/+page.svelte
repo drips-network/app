582a7cfac9d1c00d5eeb302c135fdc6ed405fb2b
Morgan Brown <git@email.mhgbrown.is>
2024-11-05 18:24:07 +0100

    Ensure github style button has the correct font (verdana)

--- src/routes/api/embed/project/[projectUrl]/support/+page.svelte
+++ src/routes/api/embed/project/[projectUrl]/support/+page.svelte
@@ -4,7 +4,4 @@
   export let data;
 </script>
 
-<!-- class typo-text required for correct font when rendering png via puppeteer headless chrome -->
-<div class="typo-text">
-  <ProjectSupportButton data={data.supportButtonData} options={data.supportButtonOptions} />
-</div>
+<ProjectSupportButton data={data.supportButtonData} options={data.supportButtonOptions} />
