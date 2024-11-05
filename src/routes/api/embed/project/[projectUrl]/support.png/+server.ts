d101cb2c9504d5fcc368a8b62a47d7b4bab22216
Morgan Brown <git@email.mhgbrown.is>
2024-11-05 14:23:31 +0100

    Fix wrong font on rendered button

--- src/routes/api/embed/project/[projectUrl]/support.png/+server.ts
+++ src/routes/api/embed/project/[projectUrl]/support.png/+server.ts
@@ -68,12 +68,7 @@ export const GET: RequestHandler = async ({ url, params }) => {
         executablePath: '/usr/bin/google-chrome-stable',
       }),
       headless: true,
-      args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        // '--font-render-hinting=none',
-        // '--disable-web-security',
-      ],
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
 
     // Set up the page
@@ -86,29 +81,7 @@ export const GET: RequestHandler = async ({ url, params }) => {
 
     // Navigate to the page rendering the button
     await page.goto(imageUrl);
-    // await page.goto(imageUrl, { waitUntil: 'domcontentloaded' });
-    // Wait until all images and fonts have loaded
-    // await page.evaluate(async () => {
-    //   const selectors = Array.from(document.querySelectorAll('img'));
-    //   await Promise.all([
-    //     document.fonts.ready,
-    //     ...selectors.map((img) => {
-    //       // Image has already finished loading, let’s see if it worked
-    //       if (img.complete) {
-    //         // Image loaded and has presence
-    //         if (img.naturalHeight !== 0) return;
-    //         // Image failed, so it has no height
-    //         throw new Error('Image failed to load');
-    //       }
-    //       // Image hasn’t loaded yet, added an event listener to know when it does
-    //       return new Promise((resolve, reject) => {
-    //         img.addEventListener('load', resolve);
-    //         img.addEventListener('error', reject);
-    //       });
-    //     }),
-    //   ]);
-    // });
-
+    // Get the button
     const selector = '.support-button';
     const element = await page.waitForSelector(selector);
     // if there's no element for any reason, that's
