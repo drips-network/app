56a152e705e4f86d04840468450977435d2b0ab1
Morgan Brown <git@email.mhgbrown.is>
2024-11-26 12:37:19 +0800

    Try creating a lil utility to handle bad fetch responses

--- src/lib/utils/cmc.ts
+++ src/lib/utils/cmc.ts
@@ -1,5 +1,18 @@
+import { error } from '@sveltejs/kit';
 import { getAddress } from 'ethers';
 import { z } from 'zod';
+
+const ensureOk = async (responsePromise: Promise<Response>): Promise<Response> => {
+  const intermediateResponse = await responsePromise;
+  if (!intermediateResponse.ok) {
+    const text = await intermediateResponse.text();
+    throw error(intermediateResponse.status, {
+      message: `Fetch response was not ok: ${intermediateResponse.url} ${text}`,
+    });
+  }
+
+  return responsePromise;
+};
 
 export const getCmcPrices = async (tokenAddresses: string[], fetch = window.fetch) => {
   if (!tokenAddresses.length) {
@@ -7,10 +20,12 @@ export const getCmcPrices = async (tokenAddresses: string[], fetch = window.fetc
   }
 
   try {
+    // get id map and ensure the response does not indicate an error
+    const idMapResponse = await ensureOk(fetch('/api/fiat-estimates/id-map'));
     // get response of known token address => token id
-    const idMapRes = await (await fetch('/api/fiat-estimates/id-map')).json();
+    const idMapJson = await idMapResponse.json();
     // produce map of response
-    const tokenAddressToId = z.record(z.string(), z.number()).parse(idMapRes);
+    const tokenAddressToId = z.record(z.string(), z.number()).parse(idMapJson);
     // create parameter for /api/fiat-estimates/price endpoint, removing unknown token ids
     const tokenIdsString = tokenAddresses
       .reduce((memo, address) => {
@@ -26,7 +41,7 @@ export const getCmcPrices = async (tokenAddresses: string[], fetch = window.fetc
     let tokenIdToPrice: Record<string, number> = {};
     if (tokenIdsString.length) {
       // get response of prices for token ids
-      const priceRes = await fetch(`/api/fiat-estimates/price/${tokenIdsString}`);
+      const priceRes = await ensureOk(fetch(`/api/fiat-estimates/price/${tokenIdsString}`));
       // get parsed map of response, token id => token fiat price
       tokenIdToPrice = z.record(z.string(), z.number()).parse(await priceRes.json());
     }
