e01fbe819822044d1459cd083da257e02ff1a8c2
Jason Efstathiou <jason.efst@gmail.com>
2024-12-04 11:13:02 +0100

    💅

--- src/lib/components/stepper/components/transact-step.svelte
+++ src/lib/components/stepper/components/transact-step.svelte
@@ -396,8 +396,10 @@
               eip712Signature: sig,
             }),
           });
-          if (!gaslessCallRes.ok)
+          if (!gaslessCallRes.ok) {
             throw new Error(`Failed to submit gasless call: ${await gaslessCallRes.text()}`);
+          }
+
           const body = await gaslessCallRes.json();
           const { taskId } = body;
 
