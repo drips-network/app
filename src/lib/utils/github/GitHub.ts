7a81cd7ba185cdbf2a54695f903cb8c07f89022f
Morgan Brown <git@email.mhgbrown.is>
2024-12-06 21:48:22 +0800

    Integrate github changes with newer changes

--- src/lib/utils/github/GitHub.ts
+++ src/lib/utils/github/GitHub.ts
@@ -38,12 +38,38 @@ export default class GitHub {
     return this.getRepoByOwnerAndName(owner, repo);
   }
 
-  public async verifyFundingJson(owner: string, repo: string): Promise<void> {
+  public async getFundingJsonAddress(owner: string, repo: string): Promise<string | null> {
     const fundingJson = await this.fetchFundingJson(owner, repo);
-    if (!fundingJson) {
+    return (
+      fundingJson.drips?.[network.name === 'homestead' ? 'ethereum' : network.name].ownedBy ?? null
+    );
+  }
+
+  public async fetchFundingJson(owner: string, repo: string): Promise<FundingJson> {
+    let data;
+    try {
+      ({ data } = await this.octokit.repos.getContent({
+        owner,
+        repo,
+        path: 'FUNDING.json',
+        request: {
+          cache: 'reload',
+        },
+      }));
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error('FUNDING.json not found', error);
       throw new Error('FUNDING.json not found.');
     }
 
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const fileContent = Buffer.from((data as any).content, 'base64').toString('utf-8');
+
+      return JSON.parse(fileContent);
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error('FUNDING.json not parseable', error);
       throw new Error(
         `Unable to parse the FUNDING.json file. Ensure it exists, is valid JSON, and includes an address for ${network.label}.`,
       );
@@ -57,38 +83,12 @@ export default class GitHub {
   ): Promise<void> {
     const fundingJsonOwner = await this.getFundingJsonAddress(owner, repo);
 
-<<<<<<< HEAD
     if (!fundingJsonOwner) {
       throw new Error('Invalid FUNDING.json file. Does it exist?');
     }
 
     if (fundingJsonOwner.toLowerCase() !== expectedAddress?.toLowerCase()) {
-=======
-    if (!fundingJsonOwner || fundingJsonOwner.toLowerCase() !== expectedOwner?.toLowerCase()) {
->>>>>>> e54cf005 (Establish method to fetch funding.json, struggle with typescript)
       throw new Error('Invalid FUNDING.json file. Does it have the correct Ethereum address?');
-    }
-  }
-
-  public async fetchFundingJson(owner: string, repo: string): Promise<FundingJson | null> {
-    try {
-      const { data } = await this.octokit.repos.getContent({
-        owner,
-        repo,
-        path: 'FUNDING.json',
-        request: {
-          cache: 'reload',
-        },
-      });
-
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const fileContent = Buffer.from((data as any).content, 'base64').toString('utf-8');
-
-      return JSON.parse(fileContent);
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.error('FUNDING.json not found or unparseable', error);
-      return null;
     }
   }
 }
