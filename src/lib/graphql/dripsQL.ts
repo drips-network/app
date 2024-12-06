93d2b6469eaed9aca8c49c1d6577f0af1640052e
Jason Efstathiou <jason.efst@gmail.com>
2024-12-06 12:44:56 +0100

    use relative GQL path in dripsQL

--- src/lib/graphql/dripsQL.ts
+++ src/lib/graphql/dripsQL.ts
@@ -2,14 +2,13 @@ import uniqBy from 'lodash/uniqBy';
 import { parse } from 'graphql';
 import { GraphQLClient, type RequestDocument, type Variables } from 'graphql-request';
 import { addTypenameToDocument } from '@apollo/client/utilities';
-import { BASE_URL } from '$lib/utils/base-url';
 
 export default async function query<TResponse, TVariables extends Variables = Variables>(
   query: RequestDocument,
   variables?: TVariables,
   customFetch: typeof fetch = fetch,
 ): Promise<TResponse> {
-  const client = new GraphQLClient(`${BASE_URL}/api/gql`, {
+  const client = new GraphQLClient('/api/gql', {
     fetch: customFetch,
   });
 
