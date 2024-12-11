bcc2dccb441fe0a186f89f654e4510d607c48f33
Morgan Brown <git@email.mhgbrown.is>
2024-12-11 14:50:07 +0100

    Allow import from csv for editing maintainers

--- src/lib/flows/edit-project-splits/steps/edit-maintainer-list.svelte
+++ src/lib/flows/edit-project-splits/steps/edit-maintainer-list.svelte
@@ -10,6 +10,12 @@
   import ListEditor from '$lib/components/list-editor/list-editor.svelte';
   import ArrowLeft from '$lib/components/icons/ArrowLeft.svelte';
   import CustodialWarning from '$lib/components/annotation-box/custodial-warning.svelte';
+  import importFromCSVSteps, {
+    WEIGHT_FACTOR,
+  } from '$lib/flows/import-from-csv/import-from-csv-steps';
+  import ArrowDown from '$lib/components/icons/ArrowDown.svelte';
+  import FormField from '$lib/components/form-field/form-field.svelte';
+  import type { ListEditorItem, AccountId } from '$lib/components/list-editor/types';
 
   const dispatch = createEventDispatcher<StepComponentEvents>();
 
@@ -26,6 +32,40 @@
     }
   }
 
+  function handleImportCSV() {
+    dispatch(
+      'sidestep',
+      importFromCSVSteps({
+        headline: 'Import maintainers from CSV',
+        description:
+          'Your CSV file should be formatted by first listing the recipient, then listing the percentage allocation. For example:',
+        exampleTableCaption:
+          'A recipient can be a wallet address, GitHub repo URL, or Drip List URL. Maximum 200 recipients. Any previously configured recipients will be overwritten with the CSV contents.',
+        addItem(key: AccountId, item: ListEditorItem, weight: number | undefined) {
+          context.update((c) => {
+            c.maintainerSplits.items = {
+              ...c.maintainerSplits.items,
+              [key]: item,
+            };
+
+            if (weight) {
+              c.maintainerSplits.weights[key] = weight * WEIGHT_FACTOR;
+            }
+
+            return c;
+          });
+        },
+        clearItems() {
+          context.update((c) => {
+            c.maintainerSplits.items = {};
+            c.maintainerSplits.weights = {};
+            return c;
+          });
+        },
+      }),
+    );
+  }
+
   $: dependencyKeys = Object.keys($context.dependencySplits.items);
 </script>
 
@@ -37,15 +77,20 @@
     ]}% you assigned to your project’s maintainers."
   />
   <CustodialWarning dismissableId="custodial-warning-project-splits" />
-  <ListEditor
-    bind:weights={$context.maintainerSplits.weights}
-    bind:items={$context.maintainerSplits.items}
-    bind:valid={formValid}
-    blockedAccountIds={dependencyKeys}
-    maxItems={200 - dependencyKeys.length}
-    allowProjects={false}
-    allowDripLists={false}
-  />
+  <FormField title="Maintainers*">
+    <ListEditor
+      bind:weights={$context.maintainerSplits.weights}
+      bind:items={$context.maintainerSplits.items}
+      bind:valid={formValid}
+      blockedAccountIds={dependencyKeys}
+      maxItems={200 - dependencyKeys.length}
+      allowProjects={false}
+      allowDripLists={false}
+    />
+    <svelte:fragment slot="action">
+      <Button variant="ghost" icon={ArrowDown} on:click={handleImportCSV}>Import from CSV</Button>
+    </svelte:fragment>
+  </FormField>
   <svelte:fragment slot="left-actions">
     <Button icon={ArrowLeft} on:click={() => dispatch('goBackward')}>Back</Button>
   </svelte:fragment>
