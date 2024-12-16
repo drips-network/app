ced706982d2847e6754792abd8b47f4a00ac6e81
Morgan Brown <brown.mhg+github@gmail.com>
2024-12-17 00:46:17 +0800

    1393 trim empty spaces in csv import (#1395)
    * Add breaking test for spaces in CSV values
    
    * Trim csv string values
    
    ---------
    
    Co-authored-by: Morgan Brown <git@email.mhgbrown.is>

--- src/lib/flows/import-from-csv/__test__/parse-upload.unit.test.ts
+++ src/lib/flows/import-from-csv/__test__/parse-upload.unit.test.ts
@@ -3,6 +3,7 @@ import splits from './data/splits.csv?raw';
 import splitsHeader from './data/splits-header.csv?raw';
 import splitsHeaderExtra from './data/splits-header-extra.csv?raw';
 import splitsHeaderCase from './data/splits-header-case.csv?raw';
+import splitsHeaderSpace from './data/splits-header-space.csv?raw';
 import collaboratorsHeaderMixed from './data/collaborators-header-mixed.csv?raw';
 
 vi.mock('$app/environment', () => ({
@@ -16,6 +17,7 @@ describe('parse upload', () => {
     let splitsHeaderFile: File;
     let splitsHeaderExtraFile: File;
     let splitsHeaderCaseFile: File;
+    let splitsHeaderSpaceFile: File;
 
     beforeAll(() => {
       splitsFile = new File([new Blob([splits], { type: 'text/csv' })], 'name');
@@ -25,6 +27,10 @@ describe('parse upload', () => {
         'name',
       );
       splitsHeaderCaseFile = new File([new Blob([splitsHeaderCase], { type: 'text/csv' })], 'name');
+      splitsHeaderSpaceFile = new File(
+        [new Blob([splitsHeaderSpace], { type: 'text/csv' })],
+        'name',
+      );
     });
 
     it('should parse a valid splits file correctly', async () => {
@@ -49,6 +55,11 @@ describe('parse upload', () => {
       const parsedFile = await parseFile(splitsHeaderCaseFile, csvHeaders);
       expect(parsedFile[0][0]).toBe('0x79756b6C2f913271fc0ee29A877fbd98258972BF');
       expect(parsedFile[0][1]).toBe('20');
+    });
+
+    it('should parse a valid splits file and remove trim white space', async () => {
+      const parsedFile = await parseFile(splitsHeaderSpaceFile, csvHeaders);
+      expect(parsedFile[0][0]).toBe('https://github.com/user/project/');
     });
   });
 
