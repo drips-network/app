3f5630bdd7247be97c7ebb3313a411ffb6b2108c
Morgan Brown <git@email.mhgbrown.is>
2025-01-02 18:17:13 -0800

    Quickly throw together loading projects for Filecoin

--- src/routes/(pages)/app/(app)/components/load-filecoin-explore-page-data.ts
+++ src/routes/(pages)/app/(app)/components/load-filecoin-explore-page-data.ts
@@ -1,13 +1,52 @@
-import { postsListingSchema } from '../../../../api/blog/posts/schema';
+import {
+  ProjectSortField,
+  ProjectVerificationStatus,
+  SortDirection,
+} from '$lib/graphql/__generated__/base-types';
+import query from '$lib/graphql/dripsQL';
+import network from '$lib/stores/wallet/network';
+import { gql } from 'graphql-request';
+import type {
+  ExploreProjectsQuery,
+  ExploreProjectsQueryVariables,
+} from './__generated__/gql.generated';
+import { DEFAULT_EXPLORE_PAGE_FEATURED_PROJECT_FRAGMENT } from './default-explore-page.svelte';
+import { fetchBlogPosts } from '../../../../../lib/utils/blog-posts';
 
-export default async function loadFilecoinExporePageData(f: typeof fetch) {
-  const fetchBlogPosts = async () => {
-    return postsListingSchema.parse(await (await f('/api/blog/posts')).json());
+const getProjectsQuery = gql`
+  ${DEFAULT_EXPLORE_PAGE_FEATURED_PROJECT_FRAGMENT}
+  query ExploreProjects(
+    $where: ProjectWhereInput
+    $sort: ProjectSortInput
+    $chains: [SupportedChain!]!
+  ) {
+    projects(where: $where, sort: $sort, chains: $chains) {
+      ...DefaultExplorePageFeaturedProject
+    }
+  }
+`;
+
+const fetchProjects = async (f: typeof fetch) => {
+  const getProjectsVariables = {
+    where: { verificationStatus: ProjectVerificationStatus.Claimed },
+    sort: { direction: SortDirection.Asc, field: ProjectSortField.ClaimedAt },
+    chains: [network.gqlName],
   };
 
-  const [blogPosts] = await Promise.all([fetchBlogPosts()]);
+  const projectsRes = await query<ExploreProjectsQuery, ExploreProjectsQueryVariables>(
+    getProjectsQuery,
+    getProjectsVariables,
+    f,
+  );
+
+  return projectsRes.projects;
+};
+
+export default async function loadFilecoinExporePageData(f: typeof fetch) {
+  const [blogPosts, projects] = await Promise.all([fetchBlogPosts(), fetchProjects(f)]);
 
   return {
     blogPosts,
+    projects,
   };
 }
