671eb79f3de45e8285f480da43db995297a2c430
Jason Efstathiou <jason.efst@gmail.com>
2025-01-09 15:48:11 +0100

    bring back env var check

--- src/routes/api/search/[...path]/+server.ts
+++ src/routes/api/search/[...path]/+server.ts
@@ -1,9 +1,14 @@
+import { dev } from '$app/environment';
 import getOptionalEnvVar from '$lib/utils/get-optional-env-var/private.js';
 import stripTrailingSlash from '$lib/utils/strip-trailing-slash.js';
 import { error } from '@sveltejs/kit';
 
 const MEILISEARCH_HOST = getOptionalEnvVar('MEILISEARCH_HOST');
 const MEILISEARCH_API_KEY = getOptionalEnvVar('MEILISEARCH_API_KEY');
+
+if (!dev && !MEILISEARCH_HOST) {
+  throw new Error('MEILISEARCH_HOST env var is required in production!');
+}
 
 export const POST = async ({ request, url }) => {
   const path = url.pathname.replace('/api/search/', '');
