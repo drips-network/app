9ca204d6665bc5f61a1ff4a06e5c9faa48aeba71
Morgan Brown <git@email.mhgbrown.is>
2025-01-12 18:02:22 -0800

    Add caching to filecoin page

--- src/routes/(pages)/app/(app)/components/load-filecoin-explore-page-data.ts
+++ src/routes/(pages)/app/(app)/components/load-filecoin-explore-page-data.ts
@@ -1,13 +1,23 @@
+import cached from '$lib/utils/cache/remote/cached';
+import queryCacheKey from '$lib/utils/cache/remote/query-cache-key';
 import { fetchBlogPosts } from '../../../../../lib/utils/blog-posts';
-import { createFetchProjectsParameters, fetchProjects } from './load-projects';
+import { redis } from '../../../../api/redis';
+import { createFetchProjectsParameters, fetchProjects, fetchProjectsQuery } from './load-projects';
 
 export default async function loadFilecoinExporePageData(f: typeof fetch) {
   const projectsParameters = createFetchProjectsParameters();
+  const cacheKey = queryCacheKey(
+    fetchProjectsQuery,
+    [Object.entries(projectsParameters)],
+    'explore-page-filecoin',
+  );
 
-  const [blogPosts, projects] = await Promise.all([
-    fetchBlogPosts(),
-    fetchProjects(f, projectsParameters),
-  ]);
+  const [blogPosts, projects] = await cached(
+    redis,
+    cacheKey,
+    6 * 60 * 60, // Change the cache expiration time to 6 hours
+    async () => Promise.all([fetchBlogPosts(), fetchProjects(f, projectsParameters)]),
+  );
 
   return {
     blogPosts,
