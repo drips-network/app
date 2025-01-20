a03ab65a00accc375825005c373d0f5cc140dece
Morgan Brown <git@email.mhgbrown.is>
2025-01-20 15:01:09 -0800

    Get a little started with Cosmograph

---
+++ src/routes/(pages)/app/(app)/ecosystems/components/ecosystem-network.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import testData from '../__test__/data/test.json';
+  import { Cosmograph, type CosmographInputConfig } from '@cosmograph/cosmograph';
+  import type { CosmosInputNode, CosmosInputLink } from '@cosmograph/cosmos';
+
+  let networkContainer: HTMLDivElement;
+
+  const { nodes, edges } = testData;
+
+  function initializeNetwork() {
+    const config: CosmographInputConfig<CosmosInputNode, CosmosInputLink> = {
+      // @ts-expect-error: any
+      nodeColor: (d) => d.color,
+      nodeSize: 1,
+      linkWidth: 2,
+      backgroundColor: 'transparent',
+      showDynamicLabels: false,
+      randomSeed: 0,
+      fitViewDelay: 0,
+      // ...
+    };
+    const cosmograph = new Cosmograph(networkContainer, config);
+    cosmograph.setData(nodes, edges);
+  }
+
+  onMount(initializeNetwork);
+</script>
+
+<div class="ecosystem-network" bind:this={networkContainer}></div>
+
+// @ts-expect-error: any
