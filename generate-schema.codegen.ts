609d5be90028628d73e527387bad69e35312372a
Jason Efstathiou <jason.efst@gmail.com>
2025-01-23 10:57:19 +0100

    fix generate-schema script

--- generate-schema.codegen.ts
+++ generate-schema.codegen.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
-if (!process.env.GQL_URL || !process.env.GQL_ACCESS_TOKEN) {
+if (!(process.env.GQL_URL || process.env.CODEGEN_GQL_URL) || !process.env.GQL_ACCESS_TOKEN) {
   throw new Error(
     `In order to build GraphQL types, you must provide GQL_URL and GQL_ACCESS_TOKEN env vars for the Drips GraphQL API.
      Default values are included in .env.template.`,
