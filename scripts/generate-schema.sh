0bdf44cf753166d847b7bfc7deb4102d06f1cddf
Jason Efstathiou <jason.efst@gmail.com>
2025-01-24 11:29:36 +0100

    add publish-docker workflow

--- scripts/generate-schema.sh
+++ scripts/generate-schema.sh
@@ -18,7 +18,7 @@ end_time=$((start_time + 300))  # 5 minutes = 300 seconds
 find . -regex './src/.*__generated__.*' -type d -prune -exec rm -r "{}" \;
 
 while [[ $(date +%s) -lt $end_time ]]; do
-  graphql-codegen --config generate-schema.codegen.ts
+  npx graphql-codegen --config generate-schema.codegen.ts
   if [[ $? -eq 0 ]]; then
     exit 0  # Success
   fi
