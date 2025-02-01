1a067ec2504c5ad15884717982437516f6658086
Morgan Brown <git@email.mhgbrown.is>
2025-01-31 17:30:09 -0800

    Begin ecosystem profile sections

---
+++ src/routes/(pages)/app/(app)/ecosystems/[ecosystemId]/components/ecosystem-distribution.svelte
@@ -0,0 +1,15 @@
+<div class="card">distribution</div>
+
+<style>
+  .card {
+    background-color: var(--color-background);
+    padding: 1rem;
+    box-shadow: var(--elevation-low);
+    border-radius: 1.5rem 0 1.5rem 1.5rem;
+    display: flex;
+    gap: 0.5rem;
+    flex-direction: column;
+    overflow-x: scroll;
+    text-align: left;
+  }
+</style>
---
+++ src/routes/(pages)/app/(app)/ecosystems/[ecosystemId]/components/ecosystem-metadata.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+  import Coin from '$lib/components/icons/Coin.svelte';
+  import Download from '$lib/components/icons/Download.svelte';
+  import Recipients from '$lib/components/icons/Recipients.svelte';
+  import Server from '$lib/components/icons/Server.svelte';
+  import Wallet from '$lib/components/icons/Wallet.svelte';
+</script>
+
+<div class="card ecosystem-metadata">
+  <div>
+    <h4 class="typo-header-4"><Recipients style="fill: var(--color-primary)" />Recipients</h4>
+    2,618
+  </div>
+  <div>
+    <h4 class="typo-header-4"><Server style="fill: var(--color-primary)" />Model</h4>
+    Drips Bot 9000
+  </div>
+  <div>
+    <h4 class="typo-header-4"><Download style="fill: var(--color-primary)" />Download</h4>
+    Download ecosystem as CSV
+  </div>
+  <div>
+    <h4 class="typo-header-4"><Wallet style="fill: var(--color-primary)" />Owner</h4>
+    brandonhaslegs.eth
+  </div>
+  <div>
+    <!-- TODO: wrong icon -->
+    <h4 class="typo-header-4"><Coin style="fill: var(--color-primary)" />Donations</h4>
+    none
+  </div>
+</div>
+
+<style>
+  .card {
+    background-color: var(--color-background);
+    padding: 1.5rem;
+    box-shadow: var(--elevation-low);
+    border-radius: 1.5rem 0 1.5rem 1.5rem;
+    display: flex;
+    gap: 0.5rem;
+    flex-direction: column;
+    overflow-x: scroll;
+    text-align: left;
+  }
+
+  .ecosystem-metadata {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 2rem;
+  }
+
+  .ecosystem-metadata h4 {
+    color: var(--color-primary);
+    margin-bottom: 1rem;
+    display: flex;
+    gap: 0.5rem;
+  }
+</style>
