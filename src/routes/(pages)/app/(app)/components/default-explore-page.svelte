a2c87cb89ab6cb7b25474c3ca390b137f5e40088
Jason Efstathiou <jason.efst@gmail.com>
2025-02-07 15:57:32 +0100

    also make default explore page configurable

--- src/routes/(pages)/app/(app)/components/default-explore-page.svelte
+++ src/routes/(pages)/app/(app)/components/default-explore-page.svelte
@@ -4,7 +4,6 @@
   import EtherscanIcon from '$lib/components/icons/Etherscan.svelte';
   import DripListIcon from '$lib/components/icons/DripList.svelte';
   import Section from '$lib/components/section/section.svelte';
-  import { PUBLIC_NETWORK } from '$env/static/public';
   import walletStore from '$lib/stores/wallet/wallet.store';
   import AggregateFiatEstimate from '$lib/components/aggregate-fiat-estimate/aggregate-fiat-estimate.svelte';
   import totalDrippedApproximation, {
@@ -26,57 +25,19 @@
   import RecentlyClaimedProjects from './recently-claimed-projects.svelte';
   import ProjectsGrid from './projects-grid.svelte';
 
-  const FEATURED_WEB_3_PROJECTS_ACCOUNT_IDS =
-    {
-      1: [
-        '80921576051643469277397866636792942368647018452892810554457309839360', // ethers.js
-        '80928956806149918791864723629668437820661066502202314166815319654400', // wagmi
-        '80989205010981758696261160004449877944077887004065826078532843448906', // openzeppelin-contracts
-        '80921140646830818724035150101819719966329403614944137690624336855040', // typechain
-        '80921576051643464144625531318112867710912704239101372541354551279616', // web3.py
-        '80927335273972468167722947750338907267861671542981060844246982983680', // starknet.js
-        '80927325632295926773992520689210905998818993360029926329589912567808', // snapshot
-        '80922395546375089598655709477693009806793075640399849243804470083584', // graph-node
-      ],
-      5: [
-        '81084611675088797239845552682012929720024883823846356540336220583709',
-        '80921553623925136102837120782793736893291544351678576578072673071411',
-        '81084953153801269804906196669849986124054336368266435383120426750828',
-        '80921553623925136102837120782793736893291544351678576578072673072128',
-        '80921553623925136102837120782793736893291544351678576578072673072640',
-        '80921553623925136102837120782793736893291544351678576578072673071412',
-        '80921553623925136102837120782793736893291544351678576578072673071408',
-        '80921553623925136102837120782793736893291544351678576578072673071616',
-      ],
-    }[PUBLIC_NETWORK] ?? [];
-
-  const FEATURED_PROJECTS_ACCOUNT_IDS =
-    {
-      1: [
-        '80927338512810702724070905882237017022089417038277884279346528518144', // svelte
-        '80924437970685862336445237697146051810361983738382071874368862945280', // libgit2
-        '80926915932044874567662239576164593128211047641556385561345492254720', // rollup
-        '80926893484859255783823398448697429050946431358357802763937929756672', // ratatui
-        '80907185984472938178947231143934051564334108789056821384881454972928', // gitoxide
-        '80914552162673449833728329632154702021826817315592132431697405804544', // tanstack-query
-        '80928551845018914533911124940323675679718022636490642073491274203136', // vitest
-        '80928956680761126169933069488824219882230509123653596947248123478016', // websockets
-      ],
-    }[PUBLIC_NETWORK] ?? [];
-
   export let projects: DefaultExplorePageFeaturedProjectFragment[];
+  export let featuredProjectIds: string[] | undefined = undefined;
+  export let featuredWeb3ProjectIds: string[] | undefined = undefined;
   export let blogPosts: z.infer<typeof postsListingSchema>;
   export let featuredDripLists: ExplorePageFeaturedDripListsFragment[];
   export let totalDrippedPrices: Awaited<ReturnType<typeof cachedTotalDrippedPrices>>;
   export let tlv: number;
 
   $: featuredWeb3Projects = projects.filter((p) =>
-    FEATURED_WEB_3_PROJECTS_ACCOUNT_IDS.includes(p.account.accountId),
+    featuredWeb3ProjectIds?.includes(p.account.accountId),
   );
 
-  $: featuredProjects = projects.filter((p) =>
-    FEATURED_PROJECTS_ACCOUNT_IDS.includes(p.account.accountId),
-  );
+  $: featuredProjects = projects.filter((p) => featuredProjectIds?.includes(p.account.accountId));
 
   // 2 latest posts. Sort by date
   $: blogPosts = blogPosts
@@ -142,70 +103,76 @@
     </div>
   </Section>
 
-  <Section
-    header={{
-      icon: BoxIcon,
-      label: 'Featured projects',
-      actions: [
-        {
-          label: 'See all',
-          href: '/app/projects/all',
-          icon: Box,
-        },
-      ],
-    }}
-    skeleton={{
-      loaded: true,
-    }}
-  >
-    <div class="horizontal-scroll">
-      <ProjectsGrid projects={featuredProjects} />
-    </div>
-  </Section>
+  {#if featuredProjectIds}
+    <Section
+      header={{
+        icon: BoxIcon,
+        label: 'Featured projects',
+        actions: [
+          {
+            label: 'See all',
+            href: '/app/projects/all',
+            icon: Box,
+          },
+        ],
+      }}
+      skeleton={{
+        loaded: true,
+      }}
+    >
+      <div class="horizontal-scroll">
+        <ProjectsGrid projects={featuredProjects} />
+      </div>
+    </Section>
+  {/if}
 
-  <Section
-    header={{
-      icon: DripListIcon,
-      label: 'Featured Drip Lists',
-      actions: [
-        {
-          label: 'See all',
-          href: '/app/drip-lists/all',
-          icon: DripList,
-        },
-      ],
-    }}
-    skeleton={{
-      loaded: true,
-    }}
-  >
-    <div class="drip-list-cards-grid">
-      {#each featuredDripLists as dripList}
-        <DripListCard listingMode data={{ dripList: dripList }} />
-      {/each}
-    </div>
-  </Section>
+  {#if featuredDripLists?.length > 0}
+    <Section
+      header={{
+        icon: DripListIcon,
+        label: 'Featured Drip Lists',
+        actions: [
+          {
+            label: 'See all',
+            href: '/app/drip-lists/all',
+            icon: DripList,
+          },
+        ],
+      }}
+      skeleton={{
+        loaded: true,
+      }}
+    >
+      <div class="drip-list-cards-grid">
+        {#each featuredDripLists as dripList}
+          <DripListCard listingMode data={{ dripList: dripList }} />
+        {/each}
+      </div>
+    </Section>
+  {/if}
 
-  <Section
-    header={{
-      icon: BoxIcon,
-      label: 'Featured web3 projects',
-      actions: [
-        {
-          label: 'See all',
-          href: '/app/projects/all',
-          icon: Box,
-        },
-      ],
-    }}
-    skeleton={{
-      loaded: true,
-    }}
-  >
-    <div class="horizontal-scroll">
-      <ProjectsGrid projects={featuredWeb3Projects} />
-    </div>
-  </Section>
+  {#if featuredWeb3ProjectIds}
+    <Section
+      header={{
+        icon: BoxIcon,
+        label: 'Featured web3 projects',
+        actions: [
+          {
+            label: 'See all',
+            href: '/app/projects/all',
+            icon: Box,
+          },
+        ],
+      }}
+      skeleton={{
+        loaded: true,
+      }}
+    >
+      <div class="horizontal-scroll">
+        <ProjectsGrid projects={featuredWeb3Projects} />
+      </div>
+    </Section>
+  {/if}
 
   <RecentlyClaimedProjects {projects} />
 
--- src/routes/(pages)/app/(app)/components/load-default-explore-page-data.ts
+++ src/routes/(pages)/app/(app)/components/load-default-explore-page-data.ts
@@ -1,4 +1,3 @@
-import { PUBLIC_NETWORK } from '$env/static/public';
 import { postsListingSchema } from '../../../../api/blog/posts/schema';
 import mapFilterUndefined from '$lib/utils/map-filter-undefined';
 import { cachedTotalDrippedPrices } from '$lib/utils/total-dripped-approx';
@@ -6,34 +5,29 @@ import { redis } from '../../../../api/redis';
 import cached from '$lib/utils/cache/remote/cached';
 import queryCacheKey from '$lib/utils/cache/remote/query-cache-key';
 
-import { fetchBlogPosts } from '../../../../../lib/utils/blog-posts';
+import { fetchBlogPosts } from '$lib/utils/blog-posts';
 import { createFetchProjectsParameters, fetchProjects, fetchProjectsQuery } from './load-projects';
 import { featuredDripListQuery, fetchList } from './load-drip-list';
 
-const FEATURED_DRIP_LISTS =
-  {
-    1: [
-      '31017209032870028068280040871339261037749177808773684797297972107972',
-      '34625983682950977210847096367816372822461201185275535522726531049130',
-      '30178668158349445547603108732480118476541651095408979232800331391215',
-      '36167722434539895740687283110259945938004377627588501179309095983174',
-    ],
-    5: [
-      '43105784259047059587622297205437858441071428120535676155904083617631',
-      '28481327705385486963944368236369218710166051344540861155364610214366',
-    ],
-  }[PUBLIC_NETWORK] ?? [];
+export default async function loadDefaultExplorePageData(
+  f: typeof fetch,
+  config: {
+    featuredDripListIds?: string[];
+    featuredProjectIds?: string[];
+    featuredWeb3ProjectIds?: string[];
+  } = {},
+) {
+  const { featuredDripListIds, featuredProjectIds, featuredWeb3ProjectIds } = config;
 
-export default async function loadDefaultExplorePageData(f: typeof fetch) {
   const fetchProjectsParameters = createFetchProjectsParameters();
   const cacheKey = queryCacheKey(
     fetchProjectsQuery + featuredDripListQuery,
-    [Object.entries(fetchProjectsParameters), FEATURED_DRIP_LISTS],
+    [Object.entries(fetchProjectsParameters), featuredDripListIds],
     'explore-page',
   );
 
   const fetchFeaturedLists = async () => {
-    return await Promise.all(FEATURED_DRIP_LISTS.map(async (id) => await fetchList(id, f)));
+    return await Promise.all((featuredDripListIds ?? []).map(async (id) => await fetchList(id, f)));
   };
 
   const fetchTlv = async () => {
@@ -61,6 +55,8 @@ export default async function loadDefaultExplorePageData(f: typeof fetch) {
 
   return {
     projects,
+    featuredProjectIds,
+    featuredWeb3ProjectIds,
     blogPosts: postsListingSchema.parse(blogPosts),
     featuredDripLists: mapFilterUndefined(featuredDripLists, (v) =>
       v === null || v === undefined ? undefined : v,
