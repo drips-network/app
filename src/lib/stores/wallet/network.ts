bfc31d0978bd56ac0c3003aa53aa36c89ec01ee3
Jason Efstathiou <jason.efst@gmail.com>
2025-02-10 12:36:20 +0100

    fix network.ts

--- src/lib/stores/wallet/network.ts
+++ src/lib/stores/wallet/network.ts
@@ -434,7 +434,6 @@ export const NETWORK_CONFIG: ValueForEachSupportedChain<Network> = {
     ensSupported: false,
     ensAddress: undefined,
     gelatoRelayAvailable: true,
-    gelatoRelayAvailable: true,
     gaslessClaimAndCollect: true,
     addToWalletConfig: {
       blockExplorerUrls: ['https://explorer.metis.io/'],
@@ -444,19 +443,6 @@ export const NETWORK_CONFIG: ValueForEachSupportedChain<Network> = {
         symbol: 'METIS',
       },
       rpcUrls: ['https://andromeda.metis.io/?owner=1088'],
-    },
-    addToWalletConfig: {
-      blockExplorerUrls: ['https://filecoin.blockscout.com/'],
-      nativeCurrency: {
-        decimals: 18,
-        name: 'Filecoin',
-        symbol: 'FIL',
-      },
-      rpcUrls: [
-        'https://api.node.glif.io/rpc/v1',
-        'https://filecoin.chainup.net/rpc/v1',
-        'https://rpc.ankr.com/filecoin',
-      ],
     },
   },
 };
