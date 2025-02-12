29024e8266b950dd5f502d9b6546d3db323509c1
Morgan Brown <git@email.mhgbrown.is>
2025-02-11 16:17:05 -0800

    Tweak oso data according to validations
    - Root sourced edge weights should add up to 100

--- src/routes/(pages)/app/(app)/ecosystems/+page.ts
+++ src/routes/(pages)/app/(app)/ecosystems/+page.ts
@@ -13,7 +13,7 @@ import * as ecosystemsApi from '$lib/utils/ecosystems';
 const fetchedDataCache = makeFetchedDataCache<ProjectsPageQuery>('dashboard:projects');
 
 // const chicken = {
-//   "name": "Wild Ecosystem",
+//   "name": "OSO Ecosystem",
 //   "description": "It's wild and free",
 //   "chainId": "11155111",
 //   "ownerAccountId": "1295444165478540595942340304482567097034602638723",
@@ -27,134 +27,7 @@ const fetchedDataCache = makeFetchedDataCache<ProjectsPageQuery>('dashboard:proj
 //           "label": "label"
 //       }
 //   }],
-//   "graph": {
-//     "nodes": [
-//         {
-//             "projectName": "root"
-//         },
-//         {
-//             "projectName": "drips-network/app"
-//         },
-//         {
-//             "projectName": "drips-network/contracts"
-//         },
-//         {
-//             "projectName": "drips-network/multiplayer"
-//         },
-//         {
-//             "projectName": "drips-network/graphql-api"
-//         },
-//         {
-//             "projectName": "drips-network/events-processor"
-//         },
-//         {
-//             "projectName": "drips-network/sprinkler"
-//         },
-//         {
-//             "projectName": "drips-network/search-sync"
-//         },
-//         {
-//             "projectName": "drips-network/sdk"
-//         },
-//         {
-//             "projectName": "jtourkos/typed-drips-example"
-//         },
-//         {
-//             "projectName": "jtourkos/dt14"
-//         },
-//         {
-//             "projectName": "jtourkos/dt15"
-//         },
-//         {
-//             "projectName": "jtourkos/search-test"
-//         },
-//         {
-//             "projectName": "jtourkos/tx-actions"
-//         }
-//     ],
-//     "edges": [
-//         {
-//             "source": "root",
-//             "target": "drips-network/app",
-//             "weight": 40
-//         },
-//         {
-//             "source": "root",
-//             "target": "drips-network/contracts",
-//             "weight": 60
-//         },
-//         {
-//             "source": "drips-network/app",
-//             "target": "drips-network/multiplayer",
-//             "weight": 20
-//         },
-//         {
-//             "source": "drips-network/app",
-//             "target": "drips-network/graphql-api",
-//             "weight": 20
-//         },
-//         {
-//             "source": "drips-network/app",
-//             "target": "drips-network/events-processor",
-//             "weight": 60
-//         },
-//         {
-//             "source": "drips-network/events-processor",
-//             "target": "drips-network/sprinkler",
-//             "weight": 30
-//         },
-//         {
-//             "source": "drips-network/sprinkler",
-//             "target": "drips-network/multiplayer",
-//             "weight": 80
-//         },
-//         {
-//             "source": "drips-network/contracts",
-//             "target": "drips-network/events-processor",
-//             "weight": 40
-//         },
-//         {
-//             "source": "drips-network/events-processor",
-//             "target": "drips-network/search-sync",
-//             "weight": 60
-//         },
-//         {
-//             "source": "drips-network/search-sync",
-//             "target": "drips-network/sdk",
-//             "weight": 75
-//         },
-//         {
-//             "source": "drips-network/contracts",
-//             "target": "jtourkos/typed-drips-example",
-//             "weight": 30
-//         },
-//         {
-//             "source": "jtourkos/typed-drips-example",
-//             "target": "jtourkos/dt14",
-//             "weight": 60
-//         },
-//         {
-//             "source": "drips-network/contracts",
-//             "target": "jtourkos/search-test",
-//             "weight": 10
-//         },
-//         {
-//             "source": "jtourkos/search-test",
-//             "target": "jtourkos/tx-actions",
-//             "weight": 60
-//         },
-//         {
-//             "source": "drips-network/contracts",
-//             "target": "jtourkos/dt15",
-//             "weight": 20
-//         },
-//         {
-//             "source": "jtourkos/dt15",
-//             "target": "jtourkos/tx-actions",
-//             "weight": 50
-//         }
-//     ]
-// }
+//   "graph": osoJson
 // }
 
 export const load = async ({ fetch }) => {
@@ -185,7 +58,7 @@ export const load = async ({ fetch }) => {
 
   const ecosystems = await ecosystemsApi.getAll();
   // console.log(chicken)
-  // if (ecosystems.length < 2) {
+  // if (ecosystems.length < 1) {
   //   console.log('Creating a chicken')
   //   const eco = await ecosystemsApi.create(chicken);
   //   ecosystems = await ecosystemsApi.getAll();
