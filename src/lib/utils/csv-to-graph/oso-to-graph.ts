a0b27cde86656e3e67ad3197e1aa585f25d28aab
Morgan Brown <git@email.mhgbrown.is>
2025-02-12 16:31:24 -0800

    Add method to automatically correct graph errors; regenerate oso graph with corrections

--- src/lib/utils/csv-to-graph/oso-to-graph.ts
+++ src/lib/utils/csv-to-graph/oso-to-graph.ts
@@ -1,12 +1,15 @@
-import { assignRandomRealisticWeights, csvToGraph } from './csv-to-graph';
+import { assignRandomRealisticWeights, correctGraph, csvToGraph } from './csv-to-graph';
 import osoUnweighted from './__test__/data/oso-unweighted-graph.csv?raw';
 import fs from 'fs';
+import osoGraphErrors from './__test__/data/oso-unweighted-graph-errors.json';
 
 export async function osoToGraphJson(path: string = 'oso-unweighted-graph-fake-weighted.json') {
   const osoUnweightedFile = new File([new Blob([osoUnweighted], { type: 'text/csv' })], 'name');
   const graph = await csvToGraph(osoUnweightedFile, { source: 1, target: 5, startIndex: 1 });
 
   assignRandomRealisticWeights(graph);
+  correctGraph(graph, osoGraphErrors);
+
   graph.nodes.sort((a, b) => a.projectName.localeCompare(b.projectName));
   // @ts-expect-error: source might be undefined
   graph.edges.sort((a, b) => a.source.localeCompare(b.source));
