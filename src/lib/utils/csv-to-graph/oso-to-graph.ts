6d9afacf5796bf24108830553a08b2381e73f27c
Morgan Brown <git@email.mhgbrown.is>
2025-02-13 17:00:00 -0800

    Don't allow removal of root node; re-organize tests

--- src/lib/utils/csv-to-graph/oso-to-graph.ts
+++ src/lib/utils/csv-to-graph/oso-to-graph.ts
@@ -1,14 +1,25 @@
-import { assignRandomRealisticWeights, correctGraph, csvToGraph } from './csv-to-graph';
+import {
+  assignRandomRealisticWeights,
+  correctGraph,
+  csvToGraph,
+  reduceGraph,
+} from './csv-to-graph';
 import osoUnweighted from './__test__/data/oso-unweighted-graph.csv?raw';
 import fs from 'fs';
 import osoGraphErrors from './__test__/data/oso-unweighted-graph-errors.json';
 
-export async function osoToGraphJson(path: string = 'oso-unweighted-graph-fake-weighted.json') {
+export async function osoToGraphJson(
+  path: string = 'oso-unweighted-graph-fake-weighted.json',
+  reduce: number = Infinity,
+) {
   const osoUnweightedFile = new File([new Blob([osoUnweighted], { type: 'text/csv' })], 'name');
   const graph = await csvToGraph(osoUnweightedFile, { source: 1, target: 5, startIndex: 1 });
 
   assignRandomRealisticWeights(graph);
   correctGraph(graph, osoGraphErrors);
+  if (Number.isFinite(reduce) && reduce > 0) {
+    reduceGraph(graph, reduce);
+  }
 
   graph.nodes.sort((a, b) => a.projectName.localeCompare(b.projectName));
   // @ts-expect-error: source might be undefined
