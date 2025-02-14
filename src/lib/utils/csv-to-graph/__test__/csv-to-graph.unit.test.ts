db16dba5346b2c95b6d9e544d0a198aff55e057d
Morgan Brown <git@email.mhgbrown.is>
2025-02-13 17:00:00 -0800

    Don't allow removal of root node; re-organize tests

--- src/lib/utils/csv-to-graph/__test__/csv-to-graph.unit.test.ts
+++ src/lib/utils/csv-to-graph/__test__/csv-to-graph.unit.test.ts
@@ -4,88 +4,92 @@ import osoUnweighted from './data/oso-unweighted-graph.csv?raw';
 import osoGraphErrors from './data/oso-unweighted-graph-errors.json';
 
 describe('csv-to-graph', () => {
-  let osoUnweightedFile: File;
-  let graph: Graph;
+  const osoUnweightedFile = new File([new Blob([osoUnweighted], { type: 'text/csv' })], 'name');
 
-  beforeAll(async () => {
-    osoUnweightedFile = new File([new Blob([osoUnweighted], { type: 'text/csv' })], 'name');
-    graph = await csvToGraph(osoUnweightedFile);
-  });
+  describe('csvToGraph', () => {
+    let graph: Graph;
 
-  it('should successfully parse a csv', async () => {
-    // NOTE: relies on the order of the test csv file.
-    const node = graph.nodes.find((n) => n.projectName === 'eth-infinitism/account-abstraction');
-    const edge = graph.edges.find((e) => e.target === 'eslint/eslintrc');
-    expect(node).toBeDefined();
-    expect(edge).toBeDefined();
-    expect(edge?.source).toEqual('eth-infinitism/account-abstraction');
-  });
-
-  it('should produce a graph with non-empty nodes', () => {
-    expect(graph.nodes.every((n) => !!n.projectName)).toBeTruthy();
-  });
-
-  it('should produce a graph with non-empty edges', () => {
-    expect(graph.edges.every((e) => !!e.source && !!e.target)).toBeTruthy();
-  });
-
-  it('should produce a graph with no duplicate nodes', () => {
-    const set = new Set(graph.nodes.map((n) => n.projectName));
-    expect(graph.nodes.length).toEqual(set.size);
-  });
-
-  it('should produce a graph with no duplicate edges', () => {
-    const set = new Set(graph.edges.map((e) => `${e.source}${e.target}`));
-    expect(graph.edges.length).toEqual(set.size);
-  });
-
-  // prevent edges whose source and target are the same
-  // ensure edges are unidreictional
-  it('should produce a graph with no duplicate edges', () => {
-    const sourceEdges = new Set(graph.edges.map((e) => `${e.source}${e.target}`));
-    const targetEdges = new Set(graph.edges.map((e) => `${e.target}${e.source}`));
-    const intersection = sourceEdges.intersection(targetEdges);
-    expect(intersection.size).toEqual(0);
-  });
-
-  it('should produce a graph in which each nodes edges do not add up to 100', () => {
-    for (const node of graph.nodes) {
-      const edges = graph.edges.filter((e: Edge) => e.source === node.projectName);
-      const sum = edges.reduce((sum: number, e: Edge) => sum + Number(e.weight), 0);
-      expect(sum).toBeLessThan(100);
-    }
-  });
-
-  it('should produce a graph with a root node', () => {
-    const root = graph.nodes.find((n) => n.projectName === 'root');
-    expect(root).toBeDefined();
-  });
-
-  it('should produce a graph with a edges whose source is the root node', () => {
-    // TODO: check all root nodes
-    const edge = graph.edges.find((n) => n.source === 'root');
-    expect(edge).toBeDefined();
-  });
-
-  describe('when provided an alternative layout', () => {
     beforeAll(async () => {
-      graph = await csvToGraph(osoUnweightedFile, { source: 3, target: 4, startIndex: 1 });
+      graph = await csvToGraph(osoUnweightedFile);
     });
 
-    it('should successfully parse a csv', () => {
+    it('should successfully parse a csv', async () => {
       // NOTE: relies on the order of the test csv file.
-      const headerNode = graph.nodes.find((n) => n.projectName === 'package_name');
-      expect(headerNode).not.toBeDefined();
-
-      const node = graph.nodes.find((n) => n.projectName === '@eslint/eslintrc');
-      const edge = graph.edges.find((e) => e.target === 'eslint');
+      const node = graph.nodes.find((n) => n.projectName === 'eth-infinitism/account-abstraction');
+      const edge = graph.edges.find((e) => e.target === 'eslint/eslintrc');
       expect(node).toBeDefined();
       expect(edge).toBeDefined();
-      expect(edge?.source).toEqual('@eslint/eslintrc');
+      expect(edge?.source).toEqual('eth-infinitism/account-abstraction');
+    });
+
+    it('should produce a graph with non-empty nodes', () => {
+      expect(graph.nodes.every((n) => !!n.projectName)).toBeTruthy();
+    });
+
+    it('should produce a graph with non-empty edges', () => {
+      expect(graph.edges.every((e) => !!e.source && !!e.target)).toBeTruthy();
+    });
+
+    it('should produce a graph with no duplicate nodes', () => {
+      const set = new Set(graph.nodes.map((n) => n.projectName));
+      expect(graph.nodes.length).toEqual(set.size);
+    });
+
+    it('should produce a graph with no duplicate edges', () => {
+      const set = new Set(graph.edges.map((e) => `${e.source}${e.target}`));
+      expect(graph.edges.length).toEqual(set.size);
+    });
+
+    // prevent edges whose source and target are the same
+    // ensure edges are unidreictional
+    it('should produce a graph with no duplicate edges', () => {
+      const sourceEdges = new Set(graph.edges.map((e) => `${e.source}${e.target}`));
+      const targetEdges = new Set(graph.edges.map((e) => `${e.target}${e.source}`));
+      const intersection = sourceEdges.intersection(targetEdges);
+      expect(intersection.size).toEqual(0);
+    });
+
+    it('should produce a graph in which each nodes edges do not add up to 100', () => {
+      for (const node of graph.nodes) {
+        const edges = graph.edges.filter((e: Edge) => e.source === node.projectName);
+        const sum = edges.reduce((sum: number, e: Edge) => sum + Number(e.weight), 0);
+        expect(sum).toBeLessThan(100);
+      }
+    });
+
+    it('should produce a graph with a root node', () => {
+      const root = graph.nodes.find((n) => n.projectName === 'root');
+      expect(root).toBeDefined();
+    });
+
+    it('should produce a graph with a edges whose source is the root node', () => {
+      // TODO: check all root nodes
+      const edge = graph.edges.find((n) => n.source === 'root');
+      expect(edge).toBeDefined();
+    });
+
+    describe('when provided an alternative layout', () => {
+      beforeAll(async () => {
+        graph = await csvToGraph(osoUnweightedFile, { source: 3, target: 4, startIndex: 1 });
+      });
+
+      it('should successfully parse a csv', () => {
+        // NOTE: relies on the order of the test csv file.
+        const headerNode = graph.nodes.find((n) => n.projectName === 'package_name');
+        expect(headerNode).not.toBeDefined();
+
+        const node = graph.nodes.find((n) => n.projectName === '@eslint/eslintrc');
+        const edge = graph.edges.find((e) => e.target === 'eslint');
+        expect(node).toBeDefined();
+        expect(edge).toBeDefined();
+        expect(edge?.source).toEqual('@eslint/eslintrc');
+      });
     });
   });
 
   describe('correctGraph', () => {
+    let graph: Graph;
+
     beforeAll(async () => {
       graph = await csvToGraph(osoUnweightedFile, { source: 1, target: 5, startIndex: 1 });
       correctGraph(graph, osoGraphErrors);
@@ -151,12 +155,15 @@ describe('csv-to-graph', () => {
   });
 
   describe('removeNode', () => {
-    beforeAll(async () => {
+    let graph: Graph;
+
+    beforeEach(async () => {
       graph = await csvToGraph(osoUnweightedFile, { source: 1, target: 5, startIndex: 1 });
-      removeNode(graph, 'eth-infinitism/account-abstraction');
     });
 
     it('does not leave orphaned nodes', () => {
+      removeNode(graph, 'eth-infinitism/account-abstraction');
+
       for (const node of graph.nodes) {
         const edge = graph.edges.find(
           (e) => e.source === node.projectName || e.target === node.projectName,
@@ -164,9 +171,17 @@ describe('csv-to-graph', () => {
         expect(edge).toBeDefined();
       }
     });
+
+    it('does not allow removing root nodes', () => {
+      const previousOrder = graph.nodes.length;
+      removeNode(graph, 'root');
+      expect(graph.nodes.length).toBe(previousOrder);
+    });
   });
 
   describe('reduceGraph', () => {
+    let graph: Graph;
+
     beforeAll(async () => {
       graph = await csvToGraph(osoUnweightedFile, { source: 1, target: 5, startIndex: 1 });
       reduceGraph(graph, 100);
