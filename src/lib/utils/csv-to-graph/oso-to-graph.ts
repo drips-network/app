166d0de5788195dbbddf10886679859bce1d9eef
Morgan Brown <git@email.mhgbrown.is>
2025-02-19 18:08:44 -0800

    Make creation of fake data more flexible

--- src/lib/utils/csv-to-graph/oso-to-graph.ts
+++ src/lib/utils/csv-to-graph/oso-to-graph.ts
@@ -11,27 +11,49 @@ import fs from 'fs';
 import osoGraphErrors from './__test__/data/oso-unweighted-graph-errors.json';
 
 export async function osoToGraphJson(
-  path: string = 'oso-unweighted-graph-fake-weighted.json',
-  reduce: number = Infinity,
+  outputPath: string = 'oso-unweighted-graph-fake-weighted.json',
+  {
+    reduce = Infinity,
+    assignWeights = false,
+    addLevels = 0,
+    sortOutput = true,
+  }: {
+    reduce?: number;
+    assignWeights?: boolean;
+    addLevels?: number;
+    sortOutput?: boolean;
+  } = {},
 ) {
   const osoUnweightedFile = new File([new Blob([osoUnweighted], { type: 'text/csv' })], 'name');
   const graph = await csvToGraph(osoUnweightedFile, { source: 1, target: 5, startIndex: 1 });
 
   correctGraph(graph, osoGraphErrors);
   const originalGraph = JSON.parse(JSON.stringify(graph));
+
   if (Number.isFinite(reduce) && reduce > 0) {
     reduceGraph(graph, reduce);
   }
 
+  if (addLevels > 0) {
+    let i = addLevels;
+    while (i--) {
+      addLevel(graph, originalGraph);
+    }
+  }
+
   // console.log('longestPath', findLongestPath(graph));
-  addLevel(graph, originalGraph);
+  // addLevel(graph, originalGraph);
   // console.log('longestPath', findLongestPath(graph));
 
-  assignRandomRealisticWeights(graph);
+  if (assignWeights) {
+    assignRandomRealisticWeights(graph);
+  }
 
-  graph.nodes.sort((a, b) => a.projectName.localeCompare(b.projectName));
-  // @ts-expect-error: source might be undefined
-  graph.edges.sort((a, b) => a.source.localeCompare(b.source));
+  if (sortOutput) {
+    graph.nodes.sort((a, b) => a.projectName.localeCompare(b.projectName));
+    // @ts-expect-error: source might be undefined
+    graph.edges.sort((a, b) => a.source.localeCompare(b.source));
+  }
 
-  fs.writeFileSync(path, JSON.stringify(graph, null, 4));
+  fs.writeFileSync(outputPath, JSON.stringify(graph, null, 4));
 }
