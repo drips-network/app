604f8c1e5fc8d609ae4b23d64bb49d116bba2153
Jason Efstathiou <jason.efst@gmail.com>
2025-02-21 14:17:52 +0100

    fix blog redirect link from alt chain mode

--- src/routes/(pages)/blog/+page.server.ts
+++ src/routes/(pages)/blog/+page.server.ts
@@ -2,10 +2,10 @@ import network from '$lib/stores/wallet/network';
 import { redirect } from '@sveltejs/kit';
 import { fetchBlogPosts } from '$lib/utils/blog-posts';
 
-export const load = async ({ route }) => {
+export const load = async () => {
   if (network.alternativeChainMode) {
     // Serve from the `mainnet` instance
-    return redirect(308, `https://drips.network${route.id}`);
+    return redirect(308, 'https://drips.network/blog');
   }
 
   const posts = await fetchBlogPosts();
