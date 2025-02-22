5ead18afa859d85792c49d75822778ba39fbb010
Morgan Brown <git@email.mhgbrown.is>
2025-02-21 16:03:42 -0800

    First pass draw edge labels

--- src/routes/(pages)/app/(app)/ecosystems/components/ecosystem-graph.ts
+++ src/routes/(pages)/app/(app)/ecosystems/components/ecosystem-graph.ts
@@ -1,7 +1,100 @@
-// import { drawDiscNodeLabel } from 'sigma/rendering';
 import type { Attributes } from 'graphology-types';
-import type { NodeDisplayData, PartialButFor } from 'sigma/types';
+import type { NodeDisplayData, PartialButFor, EdgeDisplayData } from 'sigma/types';
 import type { Settings } from 'sigma/settings';
+
+export function drawStraightEdgeLabel<
+  N extends Attributes = Attributes,
+  E extends Attributes = Attributes,
+  G extends Attributes = Attributes,
+>(
+  context: CanvasRenderingContext2D,
+  edgeData: PartialButFor<EdgeDisplayData, 'label' | 'color' | 'size'>,
+  sourceData: PartialButFor<NodeDisplayData, 'x' | 'y' | 'size'>,
+  targetData: PartialButFor<NodeDisplayData, 'x' | 'y' | 'size'>,
+  settings: Settings<N, E, G>,
+): void {
+  const size = settings.edgeLabelSize,
+    font = settings.edgeLabelFont,
+    weight = settings.edgeLabelWeight,
+    color = settings.edgeLabelColor.attribute
+      ? edgeData[settings.edgeLabelColor.attribute] || settings.edgeLabelColor.color || '#000'
+      : settings.edgeLabelColor.color;
+
+  let label = edgeData.label;
+
+  if (!label) return;
+
+  context.fillStyle = color;
+  context.font = `${weight} ${size}px ${font}`;
+
+  // Computing positions without considering nodes sizes:
+  const sSize = sourceData.size;
+  const tSize = targetData.size;
+  let sx = sourceData.x;
+  let sy = sourceData.y;
+  let tx = targetData.x;
+  let ty = targetData.y;
+  let cx = (sx + tx) / 2;
+  let cy = (sy + ty) / 2;
+  let dx = tx - sx;
+  let dy = ty - sy;
+  let d = Math.sqrt(dx * dx + dy * dy);
+
+  if (d < sSize + tSize) return;
+
+  // Adding nodes sizes:
+  sx += (dx * sSize) / d;
+  sy += (dy * sSize) / d;
+  tx -= (dx * tSize) / d;
+  ty -= (dy * tSize) / d;
+  cx = (sx + tx) / 2;
+  cy = (sy + ty) / 2;
+  dx = tx - sx;
+  dy = ty - sy;
+  d = Math.sqrt(dx * dx + dy * dy);
+
+  // Handling ellipsis
+  let textLength = context.measureText(label).width;
+
+  if (textLength > d) {
+    const ellipsis = '…';
+    label = label + ellipsis;
+    textLength = context.measureText(label).width;
+
+    while (textLength > d && label.length > 1) {
+      label = label.slice(0, -2) + ellipsis;
+      textLength = context.measureText(label).width;
+    }
+
+    if (label.length < 4) return;
+  }
+
+  const PADDING_X = 8;
+  const PADDING_Y = 8;
+
+  const boxWidth = textLength + 2 * PADDING_X;
+  const boxHeight = size + 2 * PADDING_Y;
+
+  context.save();
+  context.translate(cx, cy);
+
+  context.beginPath();
+  context.fillStyle = edgeData.labelBackgroundColor;
+  context.roundRect(
+    -textLength / 2 - PADDING_X,
+    -PADDING_Y / 2,
+    boxWidth,
+    boxHeight,
+    [16, 0, 16, 16],
+  );
+  context.closePath();
+  context.fill();
+
+  context.fillStyle = color;
+  context.fillText(label, -textLength / 2, edgeData.size / 2 + size);
+
+  context.restore();
+}
 
 export function drawDiscNodeLabel<
   N extends Attributes = Attributes,
