df58ea3f46b4117868cebbe8f1231607a51b9772
Morgan Brown <git@email.mhgbrown.is>
2025-02-24 18:25:33 -0800

    Start crafting loading state for ecosystem project card

--- src/routes/(pages)/app/(app)/ecosystems/components/ecosystem-project-card.svelte
+++ src/routes/(pages)/app/(app)/ecosystems/components/ecosystem-project-card.svelte
@@ -9,51 +9,82 @@
   import ProjectBadge from '$lib/components/project-badge/project-badge.svelte';
   import filterCurrentChainData from '$lib/utils/filter-current-chain-data';
   import twemoji from '$lib/utils/twemoji';
+  import { onMount } from 'svelte';
   import type { ProjectProfileFragment } from '../[ecosystemId]/components/__generated__/gql.generated';
+  import { fetchProject } from './ecosystem-graph';
+  import Spinner from '$lib/components/spinner/spinner.svelte';
 
-  export let project: ProjectProfileFragment;
-  export let description: string =
-    'Complete Ethereum library and wallet implementation in JavaScript.';
+  export let forge: string = 'github';
+  export let repoOwner: string;
+  export let repoName: string;
 
-  let projectChainData = filterCurrentChainData(project.chainData);
+  export let project: ProjectProfileFragment | undefined = undefined;
+  export let description: string | undefined = undefined;
+
+  let loading: boolean = false;
+
+  $: projectChainData = project ? filterCurrentChainData(project.chainData) : undefined;
+  $: forge, repoName, repoOwner, loadProject();
+
+  async function loadProject() {
+    // TODO: if project return
+
+    try {
+      loading = true;
+      const projectData = await fetchProject(repoOwner, repoName, forge);
+      project = projectData.project;
+      description = projectData.description;
+    } finally {
+      loading = false;
+    }
+  }
+
+  onMount(loadProject);
 </script>
 
-<div class="ecosystem-project-card">
-  <div class="avatar">
-    <ProjectAvatar project={projectChainData} size="xlarge" outline />
-  </div>
-  <div class="details">
-    <h2>
-      <span class="pixelated">
-        {project.source.repoName}
-      </span>
-    </h2>
-    <div>
-      <ProjectBadge size="tiny" {project} />
+<div class="ecosystem-project-card" class:loading>
+  {#if loading}
+    <Spinner visibilityDelay={0} />
+  {:else if projectChainData && description && project}
+    <div class="avatar">
+      <ProjectAvatar project={projectChainData} size="xlarge" outline />
     </div>
-    <div>
-      <span class="line-clamp-2 twemoji-text">{@html twemoji(description)} </span>
+    <div class="details">
+      <h2>
+        <span class="pixelated">
+          {project.source.repoName}
+        </span>
+      </h2>
+      <div>
+        <ProjectBadge size="tiny" {project} />
+      </div>
+      <div>
+        <span class="line-clamp-2 twemoji-text">{@html twemoji(description)} </span>
+      </div>
     </div>
-  </div>
-  <div class="stats">
-    <div>
-      <Pie style="fill: var(--color-background)" /><strong class="ml-1 typo-text-bold">2.47%</strong
-      > of ecosystem funds
+    <div class="stats">
+      <div>
+        <Pie style="fill: var(--color-background)" /><strong class="ml-1 typo-text-bold"
+          >2.47%</strong
+        > of ecosystem funds
+      </div>
+      <div>
+        <DripList style="fill: var(--color-foreground)" /><strong class="ml-1 typo-text-bold"
+          >50%</strong
+        > to 8 dependencies
+      </div>
+      <div>
+        <User style="fill: var(--color-foreground)" /><strong class="ml-1 typo-text-bold"
+          >50%</strong
+        >
+        to 2 maintainers
+      </div>
     </div>
-    <div>
-      <DripList style="fill: var(--color-foreground)" /><strong class="ml-1 typo-text-bold"
-        >50%</strong
-      > to 8 dependencies
+    <div class="actions">
+      <Button circular icon={Trash}></Button>
+      <Button circular icon={Pen}></Button>
     </div>
-    <div>
-      <User style="fill: var(--color-foreground)" /><strong class="ml-1 typo-text-bold">50%</strong>
-      to 2 maintainers
-    </div>
-  </div>
-  <div class="actions">
-    <Button circular icon={Trash}></Button>
-    <Button circular icon={Pen}></Button>
-  </div>
+  {/if}
 </div>
 
 <style>
@@ -71,6 +102,14 @@
       transform 0.2s;
     background-color: var(--color-background);
     text-align: initial;
+  }
+
+  .ecosystem-project-card.loading {
+    width: 44rem;
+    height: 152px;
+    align-items: center;
+    justify-content: center;
+    /* aspect-ratio: 704 / 152; */
   }
 
   .avatar {
