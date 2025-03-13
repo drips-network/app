457de0681cf31e57b8f1560b903c63a241499dd1
Morgan Brown <git@email.mhgbrown.is>
2025-03-13 16:14:23 -0700

    Flesh out distribution table

---
+++ src/routes/(pages)/app/(app)/ecosystems/[ecosystemId]/components/ecosystem-distribution/ecosystem-distribution-dependencies.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  export let numDependencies: number;
+</script>
+
+<div class="ecosystem-distribution-dependencies">
+  {numDependencies}
+</div>
---
+++ src/routes/(pages)/app/(app)/ecosystems/[ecosystemId]/components/ecosystem-distribution/ecosystem-distribution-project.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  export let repoName: string;
+  export let repoOwner: string;
+</script>
+
+<div class="ecosystem-distribution-project">
+  {repoOwner}/<strong class="typo-text-bold">{repoName}</strong>
+</div>
