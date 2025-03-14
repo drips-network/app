0e4c76e3b0b4e1383a55bd50f5ea455e986195c9
Georgios Jason Efstathiou <j@jason-e.dev>
2025-03-14 12:41:33 +0100

    fixxes

--- src/lib/stores/ens/ens.store.ts
+++ src/lib/stores/ens/ens.store.ts
@@ -18,18 +18,12 @@ const mainnetProvider = getMainnetProvider();
 export default (() => {
   const state = writable<State>({});
 
-  function _pickProvider() {
-    return mainnetProvider ?? get(walletStore).provider;
-  }
-
   /**
    * Perform an ENS lookup for the provided address, and append the result to the
    * store state. Looks up ENS name & avatar URL.
    * @param address The address to attempt resolving.
    */
   async function lookup(address: string): Promise<ResolvedRecord | undefined> {
-    const provider = _pickProvider();
-
     const saved = get(state)[address];
     if (saved) return;
 
@@ -37,11 +31,11 @@ export default (() => {
     // for the same name
     state.update((s) => ({ ...s, [address]: {} }));
     try {
-      const name = await provider.lookupAddress(address);
+      const name = await mainnetProvider.lookupAddress(address);
 
       let avatarUrl: string | null = null;
       if (name) {
-        const resolver = await provider.getResolver(name);
+        const resolver = await mainnetProvider.getResolver(name);
         assert(resolver, 'Failed to get resolver');
 
         avatarUrl = resolver ? await resolver.getAvatar() : null;
@@ -77,25 +71,18 @@ export default (() => {
    * name in the store state.
    */
   async function reverseLookup(name: string): Promise<string | undefined> {
-    const provider = _pickProvider();
-
-    assert(
-      provider,
-      'You need to `connect` the store to a provider before being able to reverse lookup',
-    );
-
     const saved = Object.entries(get(state)).find((entry) => entry[1].name === name);
     if (saved) return saved[0];
 
     const address = await safeReverseLookp(
       get(walletStore).provider,
-      provider,
+      mainnetProvider,
       network.chainId,
       name,
     );
     if (address) lookup(address);
 
-    return address;
+    return address ?? undefined;
   }
 
   function clear() {
