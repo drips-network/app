6c5c4e0af93e9ec7b747d23d45d43146362af19d
Morgan Brown <git@email.mhgbrown.is>
2025-03-21 17:40:14 -0700

    Adjust ecosystem project pie with project percentage

--- src/lib/components/icons/Pie.svelte
+++ src/lib/components/icons/Pie.svelte
@@ -2,12 +2,27 @@
   import IconWrapper from './IconWrapper.svelte';
 
   export let style: string | undefined = undefined;
+  export let slice: number = 0.25;
+
+  const cy = 12;
+  const cx = 12;
+  const r = 11.5;
+
+  $: percent = slice * 100;
+  $: r0 = r / 2;
+  $: circumference = 2 * Math.PI * r0;
+  $: translate = -1 * 2 * cx;
 </script>
 
 <IconWrapper on:click {style}>
-  <circle cx="12" cy="12" r="11.5" stroke="var(--color-foreground)" />
-  <path
-    d="M18.2308 1.74235C16.4146 0.636792 14.2817 0 12 0V12L18.2308 1.74235Z"
-    fill="var(--color-primary)"
+  <circle {cx} {cy} {r} stroke="var(--color-foreground)" />
+  <circle
+    {cx}
+    {cy}
+    r={r0}
+    stroke="var(--color-primary)"
+    stroke-width={cx}
+    stroke-dasharray={`calc(${percent} * ${circumference} / 100) ${circumference}`}
+    transform={`rotate(-90) translate(${translate})`}
   />
 </IconWrapper>
