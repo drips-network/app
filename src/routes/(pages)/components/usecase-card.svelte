cc3e4aafe5b49ce9d301aa4b61e0d36551da3567
Georgios Jason Efstathiou <j@jason-e.dev>
2025-04-15 14:26:40 +0200

    wip

---
+++ src/routes/(pages)/components/usecase-card.svelte
@@ -0,0 +1,95 @@
+<script lang="ts">
+  import Button from "$lib/components/button/button.svelte";
+  import type { ComponentType } from "svelte";
+  import { fly, slide } from "svelte/transition";
+  import BezierEasing from "bezier-easing";
+
+  export let icon: ComponentType;
+  export let active = false;
+  export let autoActive = true;
+  export let padHeight = false;
+
+  function handleHover(hovering: boolean) {
+    if (!autoActive) return;
+
+    active = hovering;
+  }
+
+  const easing = BezierEasing(0.47, 0, 0.23, 2);
+</script>
+
+<div class="wrapper" class:pad-height={padHeight}>
+  <a href="wikipedia.com" class="usecase-card" class:pad-height={padHeight} class:active on:mouseenter={() => handleHover(true)} on:mouseleave={() => handleHover(false)}>
+    <div class="icon">
+      <svelte:component this={icon} style="height: 2rem; width: 2rem; fill: var(--color-primary)" />
+    </div>
+    <div class="text">
+      <h2 class="extra-bold">
+        <slot name="headline" />
+      </h2>
+      <p>
+        <slot name="description" />
+      </p>
+      <div>
+        {#if active}
+          <div transition:fly={{ duration: 300, y: 8, easing }} style:height="2.125rem">
+            <Button variant="primary">Read more</Button>
+          </div>
+        {/if}
+      </div>
+    </div>
+    <div class="illustration">
+      <slot name="illustration" {active} />
+    </div>
+  </a>
+</div>
+
+<style>
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .wrapper.pad-height {
+    height: 220px;
+  }
+
+  .usecase-card {
+    position: relative;
+    max-width: 332px;
+    padding: 1rem;
+    background-color: var(--color-background);
+    display: flex;
+    gap: 1.25rem;
+    flex-direction: column;
+    box-shadow: 0px 0px 0px 1px var(--color-foreground-level-2);
+    border-radius: 1rem 0 1rem 1rem;
+    box-sizing: border-box;
+    transition: height 0.3s, box-shadow 0.3s;
+  }
+
+  .usecase-card.pad-height {
+    height: 175px;
+  }
+
+  .usecase-card.active {
+    background-color: var(--color-primary-level-1);
+    box-shadow: 0px 0px 0px 1px var(--color-primary), 0 4px 0px 1px var(--color-primary);
+    height: 220px;
+  }
+
+  .text {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+  }
+
+  .illustration {
+    pointer-events: none;
+    position: absolute;
+    right: 0;
+    width: 0px;
+    transform: translate(-150px, -60%);
+  }
+</style>
---
+++ src/routes/(pages)/components/usecase-section.svelte
@@ -0,0 +1,160 @@
+<script lang="ts">
+  import Splits from "$lib/components/icons/Splits.svelte";
+  import FundDependenciesUsecase from "./usecase-illustrations/fund-dependencies-usecase.svelte";
+  import UsecaseCard from "./usecase-card.svelte";
+  import breakpointsStore from "$lib/stores/breakpoints/breakpoints.store";
+  import { onMount } from "svelte";
+
+  const usecaseCards = [
+    {
+      id: "fund-dependencies",
+      headline: "Fund your dependencies",
+      description: "Claim your project and pay it forward to the projects that you depend on.",
+      illustration: FundDependenciesUsecase,
+    },
+    {
+      id: "fund-dependencies",
+      headline: "Fund your dependencies",
+      description: "Claim your project and pay it forward to the projects that you depend on.",
+      illustration: FundDependenciesUsecase,
+    },
+    {
+      id: "fund-dependencies",
+      headline: "Fund your dependencies",
+      description: "Claim your project and pay it forward to the projects that you depend on.",
+      illustration: FundDependenciesUsecase,
+    },
+    {
+      id: "fund-dependencies",
+      headline: "Fund your dependencies",
+      description: "Claim your project and pay it forward to the projects that you depend on.",
+      illustration: FundDependenciesUsecase,
+    },
+    {
+      id: "fund-dependencies",
+      headline: "Fund your dependencies",
+      description: "Claim your project and pay it forward to the projects that you depend on.",
+      illustration: FundDependenciesUsecase,
+    },
+  ];
+
+  onMount(breakpointsStore.attach);
+  $: mobile = $breakpointsStore?.breakpoint === "mobile" || $breakpointsStore?.breakpoint === "tablet";
+  $: {
+    if (mobile) handleScroll();
+  }
+
+  let wrapperElement: HTMLDivElement;
+
+  const elements: HTMLDivElement[] = [];
+  let elementOffsets: number[] = [];
+  let elementScales: number [] = [];
+  let opacities: number[] = [];
+  let blurs: number[] = [];
+  let activeIndex = 0;
+
+  const HEADER_HEIGHT = 64;
+
+  function handleScroll() {
+    if (!mobile) return;
+    for (const [i, element] of elements.entries()) {
+      const scrollPosition = window.scrollY;
+      const distanceToTop = scrollPosition + HEADER_HEIGHT - element.offsetTop;
+
+      const newElementOffset = distanceToTop > 0
+        ? Math.abs(distanceToTop)
+        : 0;
+      elementOffsets[i] = newElementOffset;
+
+      const nextElement = elements[i + 1];
+
+      if (nextElement) {
+        const currentElementHeight = Math.round(element.getBoundingClientRect().height);
+        const distanceToNext = Math.max((nextElement.offsetTop - elementOffsets[i + 1]) - element.offsetTop - newElementOffset, 0);
+
+        const visiblePercentage = Math.min(Math.abs(distanceToNext / currentElementHeight), 1);
+
+        const scale = 0.1 * visiblePercentage + 0.9;
+        elementScales[i] = scale;
+
+        const opacity = visiblePercentage;
+        opacities[i] = opacity;
+
+        const blur = Math.abs(1 - visiblePercentage);
+        blurs[i] = blur;
+
+        if (visiblePercentage < 1) {
+          activeIndex = i + 1;
+        } else if (i === 0) {
+          activeIndex = 0;
+        }
+
+        if (i === 0) {
+          console.log({ distanceToNext,visiblePercentage, currentElementHeight, scale });
+        }
+      }
+    }
+  }
+  onMount(handleScroll);
+</script>
+
+<svelte:window on:scroll={handleScroll} />
+
+<div class="usecases mobile-only" bind:this={wrapperElement}>
+  {#each usecaseCards as { id, illustration }, index}
+  <div class="card" bind:this={elements[index]} style:opacity="{opacities[index]}" style:transform="translateY({elementOffsets[index]}px) scale({elementScales[index]})" style:filter="blur({blurs[index]}px)">
+    <UsecaseCard padHeight autoActive={!mobile} active={activeIndex === index} icon={Splits}>
+      <svelte:fragment slot="headline">Fund your dependencies</svelte:fragment>
+      <svelte:fragment slot="description">
+        Claim your project and pay it forward to the projects that you depend on.
+      </svelte:fragment>
+      <svelte:fragment slot="illustration" let:active>
+        <svelte:component this={illustration} {active} />
+      </svelte:fragment>
+    </UsecaseCard>
+  </div>
+  {/each}
+</div>
+
+<div class="usecases-desktop">
+  {#each usecaseCards.reverse() as { id, illustration }, index}
+  <div class="card">
+    <UsecaseCard padHeight icon={Splits}>
+      <svelte:fragment slot="headline">Fund your dependencies</svelte:fragment>
+      <svelte:fragment slot="description">
+        Claim your project and pay it forward to the projects that you depend on.
+      </svelte:fragment>
+      <svelte:fragment slot="illustration" let:active>
+        <svelte:component this={illustration} {active} />
+      </svelte:fragment>
+    </UsecaseCard>
+  </div>
+  {/each}
+</div>
+
+<style>
+  .usecases {
+    display: flex;
+    flex-direction: column;
+    gap: 0.25rem;
+    align-items: center;
+  }
+
+  .usecases-desktop {
+    display: none;
+  }
+
+  @media (min-width: 768px) {
+    .mobile-only {
+      display: none;
+    }
+
+    .usecases-desktop {
+      display: flex;
+      flex-direction: row-reverse;
+      flex-wrap: wrap;
+      justify-content: center;
+      gap: 1rem;
+    }
+  }
+</style>
