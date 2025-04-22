fcd5ed0ce389dd9848cda3954676b85c478431f3
Georgios Jason Efstathiou <j@jason-e.dev>
2025-04-22 15:12:15 +0200

    change speed

--- src/routes/(pages)/components/lp-hiw-entrypoint.svelte
+++ src/routes/(pages)/components/lp-hiw-entrypoint.svelte
@@ -1,19 +1,22 @@
 <script lang="ts">
-  import Button from "$lib/components/button/button.svelte";
-  import Marquee from "svelte-fast-marquee";
-  import DripList from "./svgs/drip-list.svelte";
-  import Address from "./svgs/address.svelte";
-  import Orcid from "./svgs/orcid.svelte";
-  import ProjectCard from "./svgs/project-card.svelte";
+  import Button from '$lib/components/button/button.svelte';
+  import Marquee from 'svelte-fast-marquee';
+  import DripList from './svgs/drip-list.svelte';
+  import Address from './svgs/address.svelte';
+  import Orcid from './svgs/orcid.svelte';
+  import ProjectCard from './svgs/project-card.svelte';
 </script>
 
 <div class="lp-hiw-entrypoint">
   <div class="text">
     <h1>How things work</h1>
-    <p>Achieve any funding mechanism you can dream of with our global splitting graph and powerful set of building blocks.</p>
+    <p>
+      Achieve any funding mechanism you can dream of with our global splitting graph and powerful
+      set of building blocks.
+    </p>
     <div><Button>Learn more</Button></div>
   </div>
-  <Marquee gap="1rem">
+  <Marquee gap="1rem" speed={50}>
     <DripList />
     <Address />
     <Orcid />
