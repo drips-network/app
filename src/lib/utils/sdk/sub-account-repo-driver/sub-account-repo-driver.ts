9dff6e8501703e3deb99e454e437e3c76051807b
Morgan Brown <git@email.mhgbrown.is>
2025-05-27 17:11:42 -0700

    Initial changes to add sub account split driver transations in project claim and update splits flows

---
+++ src/lib/utils/sdk/sub-account-repo-driver/sub-account-repo-driver.ts
@@ -0,0 +1,64 @@
+import wallet from '$lib/stores/wallet/wallet.store';
+import type {
+  AbiFunction,
+  AbiParametersToPrimitiveTypes,
+  ExtractAbiFunction,
+  ExtractAbiFunctionNames,
+} from 'abitype';
+import {
+  repoSubAccountDriverAbi,
+  type RepoSubAccountDriverAbi,
+} from './sub-account-repo-driver-abi';
+import { Contract } from 'ethers';
+import { get } from 'svelte/store';
+import type { ContractTransaction } from 'ethers';
+import txToSafeDripsTx from '../utils/tx-to-safe-drips-tx';
+import assert from '$lib/utils/assert';
+// import unwrapEthersResult from '../utils/unwrap-ethers-result';
+// import type { UnwrappedEthersResult } from '../sdk-types';
+import network from '$lib/stores/wallet/network';
+
+// export async function executeRepoDriverReadMethod<
+//   functionName extends ExtractAbiFunctionNames<RepoDriverAbi, 'pure' | 'view'>,
+//   abiFunction extends AbiFunction = ExtractAbiFunction<RepoDriverAbi, functionName>,
+// >(config: {
+//   functionName: functionName | ExtractAbiFunctionNames<RepoDriverAbi, 'pure' | 'view'>;
+//   args: AbiParametersToPrimitiveTypes<abiFunction['inputs'], 'inputs'>;
+// }): Promise<
+//   UnwrappedEthersResult<AbiParametersToPrimitiveTypes<abiFunction['outputs'], 'outputs'>>
+// > {
+//   const { provider } = get(wallet);
+//   const { functionName: func, args } = config;
+
+//   const repoDriver = new Contract(network.contracts.REPO_DRIVER, repoDriverAbi, provider);
+
+//   return unwrapEthersResult(await repoDriver[func](...args));
+// }
+
+export async function populateSubAccountRepoDriverWriteTx<
+  functionName extends ExtractAbiFunctionNames<RepoSubAccountDriverAbi, 'nonpayable' | 'payable'>,
+  abiFunction extends AbiFunction = ExtractAbiFunction<RepoSubAccountDriverAbi, functionName>,
+>(config: {
+  functionName:
+    | functionName
+    | ExtractAbiFunctionNames<RepoSubAccountDriverAbi, 'nonpayable' | 'payable'>;
+  args: AbiParametersToPrimitiveTypes<abiFunction['inputs'], 'inputs'>;
+}): Promise<ContractTransaction> {
+  const { signer } = get(wallet);
+  assert(signer, 'Sub Account Repo Driver contract call requires a signer but it is missing.');
+
+  const { functionName: func, args } = config;
+
+  assert(
+    network.contracts.SUB_ACCOUNT_REPO_DRIVER,
+    'Sub Account Repo Driver does not exist on this network',
+  );
+
+  const repoDriver = new Contract(
+    network.contracts.SUB_ACCOUNT_REPO_DRIVER,
+    repoSubAccountDriverAbi,
+    signer,
+  );
+
+  return txToSafeDripsTx(await repoDriver[func].populateTransaction(...args));
+}
