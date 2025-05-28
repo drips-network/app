ac018e408a15863e412fd6ddd8bd46c94aa57fe3
Morgan Brown <git@email.mhgbrown.is>
2025-05-28 14:09:43 -0700

    For non-ecosystems-enabled networks, give 404

--- src/routes/(pages)/app/(app)/ecosystems/+layout.server.ts
+++ src/routes/(pages)/app/(app)/ecosystems/+layout.server.ts
@@ -1,11 +1,10 @@
 import network from '$lib/stores/wallet/network';
-import { redirect } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
 
 export const load = async () => {
-  // allow ecosystems routes only for enabled
-  // networks
+  // allow ecosystems routes only for enabled networks
   if (!network.ecosystems) {
-    return redirect(307, '/app');
+    return error(404);
   }
 
   return {};
