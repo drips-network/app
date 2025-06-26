f23adcf5064999e5e45e605cc85c4836a2350f42
Morgan Brown <git@email.mhgbrown.is>
2025-06-26 17:03:51 +0100

    Try playwright defaults in CI env

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,10 +5,10 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
-  fullyParallel: true,
+  // fullyParallel: !process.env.CI,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  // retries: process.env.CI ? 2 : 0,
+  // workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
 
   projects: [
