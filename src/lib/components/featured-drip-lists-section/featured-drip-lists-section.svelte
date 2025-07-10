1b68bdf3abd3ad4cb343d3a7919ffb443e6ef592
Morgan Brown <git@email.mhgbrown.is>
2025-07-10 20:07:12 +0200

    Quick and dirty featured drip lists

---
+++ src/lib/components/featured-drip-lists-section/featured-drip-lists-section.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import type { DripListsSectionDripListFragment } from '../drip-lists-section/__generated__/gql.generated';
+  import Section from '../section/section.svelte';
+  import DripListIcon from '$lib/components/icons/DripList.svelte';
+  import DripListsGrid from '../../../routes/(pages)/app/(app)/components/drip-lists-grid.svelte';
+
+  export let featuredDripLists: DripListsSectionDripListFragment[] = [];
+</script>
+
+<Section
+  header={{
+    icon: DripListIcon,
+    label: 'Featured Drip Lists',
+  }}
+  skeleton={{
+    loaded: true,
+    empty: !featuredDripLists.length,
+    emptyStateEmoji: '🫙',
+    emptyStateHeadline: 'No Featured Drip Lists',
+    emptyStateText: 'We couldn’t find any featured Drip Lists.',
+  }}
+>
+  <DripListsGrid dripLists={featuredDripLists} />
+</Section>
