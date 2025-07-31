4ddea8f1f84e2210d37557ab74f1d32be8105588
Morgan Brown <git@email.mhgbrown.is>
2025-07-31 17:25:40 +0200

    Divide loading of explore page and distribution page

--- src/routes/(pages)/app/(app)/+page.server.ts
+++ src/routes/(pages)/app/(app)/+page.server.ts
@@ -2,34 +2,28 @@ import network from '$lib/stores/wallet/network';
 import { fetchBlogPosts } from '$lib/utils/blog-posts.js';
 import { cachedTotalDrippedPrices } from '$lib/utils/total-dripped-approx.js';
 import fetchTlv from './components/load-tlv.js';
-import fetchPageVariant from './components/load-page-variant.js';
 import { redis } from '../../../api/redis.js';
-import fetchWelcomeCardConfig from './components/load-welcomecard-config.js';
-import { fetchAndCategorizeProjects } from './projects/components/load-projects.js';
+import { fetchAndCategorizeProjects, fetchProjects } from './projects/components/load-projects.js';
 import fetchFeaturedDripLists from './drip-lists/components/load-drip-lists.js';
+import EXPLORE_PAGE_CONFIG, { type ExplorePageVariant } from './components/explore-page-config.js';
 
-export const load = async ({ fetch }) => {
-  // TODO: don't load all this stuff for the distribution page, switch with variant
+const loadDefaultExplorePage = async (fetch: typeof global.fetch) => {
   const [
     { featuredProjects, featuredWeb3Projects, restProjects },
     featuredDripLists,
     blogPosts,
     totalDrippedPrices,
     tlv,
-    variant,
-    welcomeCardConfig,
   ] = await Promise.all([
     fetchAndCategorizeProjects(network.chainId, fetch),
     fetchFeaturedDripLists(network.chainId, fetch),
     fetchBlogPosts(),
     cachedTotalDrippedPrices(redis, fetch),
     fetchTlv(fetch),
-    fetchPageVariant(network.chainId),
-    fetchWelcomeCardConfig(network.chainId),
   ]);
 
   return {
-    variant,
+    variant: 'default' as ExplorePageVariant,
     data: {
       projects: restProjects,
       featuredProjects,
@@ -38,7 +32,37 @@ export const load = async ({ fetch }) => {
       blogPosts,
       totalDrippedPrices,
       tlv,
+    },
+  };
+};
+const loadDistributionExplorePage = async (fetch: typeof global.fetch) => {
+  const currentConfig = EXPLORE_PAGE_CONFIG[network.chainId];
+  const showRecentProjects = currentConfig.showRecentProjects;
+  const welcomeCardConfig = currentConfig.welcomeCardConfig;
+
+  const [projects, featuredDripLists, blogPosts] = await Promise.all([
+    showRecentProjects ? fetchProjects(fetch) : null,
+    fetchFeaturedDripLists(network.chainId, fetch),
+    fetchBlogPosts(),
+  ]);
+
+  return {
+    variant: 'distribution' as ExplorePageVariant,
+    data: {
+      projects,
+      featuredDripLists,
+      blogPosts,
       welcomeCardConfig,
     },
   };
+};
+
+export const load = async ({ fetch }) => {
+  const explorePageVariant = EXPLORE_PAGE_CONFIG[network.chainId].variant;
+  switch (explorePageVariant) {
+    case 'distribution':
+      return loadDistributionExplorePage(fetch);
+    default:
+      return loadDefaultExplorePage(fetch);
+  }
 };
--- src/routes/(pages)/app/(app)/projects/+page.ts
+++ src/routes/(pages)/app/(app)/projects/+page.ts
@@ -1,15 +1,11 @@
-// import network from '$lib/stores/wallet/network';
 import { fetchAndCategorizeProjects } from './components/load-projects';
 import walletStore from '$lib/stores/wallet/wallet.store';
 import { get } from 'svelte/store';
-// import EXPLORE_PAGE_CONFIG from '../components/explore-page-config';
-import type {
-  ChainStatsQuery,
-  ExploreProjectsQuery,
-} from '../components/__generated__/gql.generated';
+import type { ChainStatsQuery } from '../components/__generated__/gql.generated';
 import { makeFetchedDataCache } from '$lib/stores/fetched-data-cache/fetched-data-cache.store';
 import fetchChainStats from '../components/load-chain-stats';
 import network from '$lib/stores/wallet/network';
+import type { ExploreProjectsQuery } from './components/__generated__/gql.generated';
 
 const fetchedDataCache = makeFetchedDataCache<{
   yourProjects: ExploreProjectsQuery['projects'];
@@ -31,26 +27,6 @@ export const load = async ({ fetch }) => {
     fetchAndCategorizeProjects(network.chainId, fetch, connectedAccountId),
     fetchChainStats(fetch),
   ]);
-
-  // const featuredProjects = [];
-  // const yourProjects = [];
-  // const restProjects = [];
-  // for (const project of projects) {
-  //   // TODO: necessary? Don't we already query using the current chain?
-  //   // const chainData = filterCurrentChainData(project.chainData)
-  //   if (project.chainData.some((chainData) => chainData.owner?.accountId === connectedAccountId)) {
-  //     yourProjects.push(project);
-  //   }
-
-  //   if (
-  //     EXPLORE_PAGE_CONFIG[network.chainId]?.featuredProjectIds?.includes(project.account.accountId)
-  //   ) {
-  //     featuredProjects.push(project);
-  //     continue;
-  //   }
-
-  //   restProjects.push(project);
-  // }
 
   fetchedDataCache.write({
     yourProjects,
