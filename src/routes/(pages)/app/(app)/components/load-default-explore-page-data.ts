d552d66fb90b8d2291812bfe92a538dc0bfa9295
Morgan Brown <git@email.mhgbrown.is>
2025-08-01 10:47:17 +0200

    Reestablish load default and distribution explore data pages

---
+++ src/routes/(pages)/app/(app)/components/load-default-explore-page-data.ts
@@ -0,0 +1,60 @@
+import network from '$lib/stores/wallet/network';
+import { fetchBlogPosts } from '$lib/utils/blog-posts';
+import { cachedTotalDrippedPrices } from '$lib/utils/total-dripped-approx';
+import fetchTlv from './load-tlv';
+import queryCacheKey from '$lib/utils/cache/remote/query-cache-key';
+import cached from '$lib/utils/cache/remote/cached';
+import FEATURED_DRIP_LISTS_CONFIG from '../drip-lists/components/featured-drip-lists-config';
+import type { ExplorePageVariant } from './explore-page-config';
+import fetchFeaturedDripLists, { dripListsQuery } from '../drip-lists/components/load-drip-lists';
+import {
+  createDefaultFetchProjectsParameters,
+  fetchAndCategorizeProjects,
+  fetchProjectsQuery,
+} from '../projects/components/load-projects';
+import { redis } from '../../../../api/redis';
+
+export default async function loadDefaultExplorePage(fetch: typeof global.fetch) {
+  const fetchProjectsParameters = createDefaultFetchProjectsParameters();
+  const featuredDripListIds = FEATURED_DRIP_LISTS_CONFIG[network.chainId].featuredDripListIds || [];
+
+  const cacheKey = queryCacheKey(
+    fetchProjectsQuery + dripListsQuery,
+    [Object.entries(fetchProjectsParameters), featuredDripListIds],
+    'explore-page',
+  );
+
+  const [
+    { featuredProjects, featuredWeb3Projects, restProjects },
+    featuredDripLists,
+    blogPosts,
+    totalDrippedPrices,
+    tlv,
+  ] = await cached(
+    redis,
+    cacheKey,
+    1 * 60 * 60, // 1 hr
+    async () =>
+      Promise.all([
+        fetchAndCategorizeProjects(network.chainId, fetch),
+        fetchFeaturedDripLists(network.chainId, fetch),
+        fetchBlogPosts(),
+        cachedTotalDrippedPrices(redis, fetch),
+        fetchTlv(fetch),
+      ]),
+  );
+
+  return {
+    variant: 'default' as ExplorePageVariant,
+    data: {
+      projects: restProjects,
+      featuredProjects,
+      featuredWeb3Projects,
+      featuredDripLists,
+      blogPosts,
+      totalDrippedPrices,
+      tlv,
+      blockWhileInitializing: false,
+    },
+  };
+}
---
+++ src/routes/(pages)/app/(app)/components/load-distribution-explore-page-data.ts
@@ -0,0 +1,52 @@
+import network from '$lib/stores/wallet/network';
+import { fetchBlogPosts } from '$lib/utils/blog-posts.js';
+import {
+  createDefaultFetchProjectsParameters,
+  fetchProjects,
+  fetchProjectsQuery,
+} from '../projects/components/load-projects.js';
+import fetchFeaturedDripLists, {
+  dripListsQuery,
+} from '../drip-lists/components/load-drip-lists.js';
+import queryCacheKey from '$lib/utils/cache/remote/query-cache-key.js';
+import cached from '$lib/utils/cache/remote/cached.js';
+import FEATURED_DRIP_LISTS_CONFIG from '../drip-lists/components/featured-drip-lists-config.js';
+import EXPLORE_PAGE_CONFIG, { type ExplorePageVariant } from './explore-page-config.js';
+import { redis } from '../../../../api/redis.js';
+
+export default async function loadDistributionExplorePage(fetch: typeof global.fetch) {
+  const currentConfig = EXPLORE_PAGE_CONFIG[network.chainId];
+  const showRecentProjects = currentConfig.showRecentProjects;
+  const welcomeCardConfig = currentConfig.welcomeCardConfig;
+
+  const fetchProjectsParameters = createDefaultFetchProjectsParameters();
+  const featuredDripListIds = FEATURED_DRIP_LISTS_CONFIG[network.chainId].featuredDripListIds || [];
+
+  const cacheKey = queryCacheKey(
+    fetchProjectsQuery + dripListsQuery,
+    [Object.entries(fetchProjectsParameters), featuredDripListIds],
+    'explore-page',
+  );
+
+  const [projects, featuredDripLists, blogPosts] = await cached(
+    redis,
+    cacheKey,
+    1 * 60 * 60, // 1 hr
+    async () =>
+      Promise.all([
+        showRecentProjects ? fetchProjects(fetch) : null,
+        fetchFeaturedDripLists(network.chainId, fetch),
+        fetchBlogPosts(),
+      ]),
+  );
+
+  return {
+    variant: 'distribution' as ExplorePageVariant,
+    data: {
+      projects,
+      featuredDripLists,
+      blogPosts,
+      welcomeCardConfig,
+    },
+  };
+}
