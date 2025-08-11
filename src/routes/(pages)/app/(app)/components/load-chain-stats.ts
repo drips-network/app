f557f4410975a1b60b50a61a2d6e4573bcebfe7b
Georgios Jason Efstathiou <j@jason-e.dev>
2025-08-11 12:33:38 +0200

    fix missing chains param on network stats query

--- src/routes/(pages)/app/(app)/components/load-chain-stats.ts
+++ src/routes/(pages)/app/(app)/components/load-chain-stats.ts
@@ -1,6 +1,7 @@
 import { gql } from 'graphql-request';
 import type { ChainStatsQuery, ChainStatsQueryVariables } from './__generated__/gql.generated';
 import query from '$lib/graphql/dripsQL';
+import network from '$lib/stores/wallet/network';
 
 export const chainStatsQuery = gql`
   query ChainStats($chains: [SupportedChain!]) {
@@ -14,10 +15,11 @@ export const chainStatsQuery = gql`
 `;
 
 export default async function fetchChainStats(f: typeof fetch) {
-  const chainStatsParameters = {};
   const res = await query<ChainStatsQuery, ChainStatsQueryVariables>(
     chainStatsQuery,
-    chainStatsParameters,
+    {
+      chains: [network.gqlName],
+    },
     f,
   );
 
