041f31b4fa0baffd64401d46c8f53296346d3de5
Georgios Jason Efstathiou <j@jason-e.dev>
2025-08-15 16:08:05 +0200

    revert accidentally made change to voting rounds

--- src/lib/flows/publish-voting-round-list/publish-voting-round-list-flow-steps.ts
+++ src/lib/flows/publish-voting-round-list/publish-voting-round-list-flow-steps.ts
@@ -1,11 +1,32 @@
 import { makeStep } from '$lib/components/stepper/types';
 import SuccessStep from '$lib/components/success-step/success-step.svelte';
-import { get } from 'svelte/store';
+import { get, writable } from 'svelte/store';
 import LoadData from './load-data.svelte';
 import Review from './review.svelte';
+import type { VoteReceiver } from '$lib/utils/multiplayer/schemas';
+import type { DripListConfig } from '$lib/components/drip-list-editor/drip-list-editor.svelte';
 import walletStore from '$lib/stores/wallet/wallet.store';
 
-export default (votingRoundId: string) => ({
+export interface State {
+  results: VoteReceiver[];
+  dripListConfig: DripListConfig;
+  publishedDripListId: string | undefined;
+}
+
+const state = (listName: string, listDescription?: string) =>
+  writable<State>({
+    results: [],
+    dripListConfig: {
+      title: listName,
+      description: listDescription,
+      items: {},
+      weights: {},
+    },
+    publishedDripListId: undefined,
+  });
+
+export default (votingRoundId: string, listName: string, listDescription?: string) => ({
+  context: () => state(listName, listDescription),
   steps: [
     makeStep({
       component: LoadData,
