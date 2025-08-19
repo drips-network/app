76d2c41d05d0813d2992b1e845a467d4922f3b85
Morgan Brown <git@email.mhgbrown.is>
2025-08-19 11:11:12 +0200

    Fetch orcid, get orcid account id

--- src/routes/(pages)/app/(app)/orcids/[orcidId]/+page.server.ts
+++ src/routes/(pages)/app/(app)/orcids/[orcidId]/+page.server.ts
@@ -1,3 +1,5 @@
+import isValidOrcidId from '$lib/utils/is-orcid-id/is-orcid-id';
+import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { fetchOrcid, fetchOrcidChainData, orcidIdToAccountId } from './components/fetch-orcid';
 
@@ -6,13 +8,23 @@ export const load = (async ({ params, fetch }) => {
   // if it's an actual orcId, then calc the account id here
   // if it's an account id, well...
 
+  if (!isValidOrcidId(params.orcidId)) {
+    return error(404);
+  }
+
   const orcid = await fetchOrcid(params.orcidId, fetch);
+  if (!orcid) {
+    return error(404);
+  }
 
   const accountId = await orcidIdToAccountId(params.orcidId);
-  const orcidChainData = await fetchOrcidChainData(accountId, fetch);
+  const orcidChainData = await fetchOrcidChainData(String(accountId), fetch);
 
   return {
     orcid,
     orcidChainData,
   };
 }) satisfies PageServerLoad;
+
+// 0009-0007-5482-8654 me in prod
+// 0009-0007-1106-8413 drips.network in sandbox
