effaec964f023c865b902d7d7cc8839e56d36ca4
Morgan Brown <git@email.mhgbrown.is>
2025-08-25 14:29:00 +0200

    Ensure serialization of ORCID entity

--- src/routes/(pages)/app/(app)/orcids/[orcidId]/+page.svelte
+++ src/routes/(pages)/app/(app)/orcids/[orcidId]/+page.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
+  import { hydrate } from '$lib/reviver';
+  import type Orcid from '$lib/utils/orcids/entities';
   import type { PageData } from './$types';
   import OrcidProfile from './components/orcid-profile.svelte';
 
   export let data: PageData;
+
+  // Revive the plain data object back into an Orcid class instance
+  const orcid: Orcid = hydrate(data).orcid;
 </script>
 
-<OrcidProfile orcid={data.orcid} orcidAccount={data.orcidAccount} />
+<OrcidProfile {orcid} orcidAccount={data.orcidAccount} />
