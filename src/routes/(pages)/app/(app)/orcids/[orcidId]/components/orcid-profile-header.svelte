6bc4e6bd187e2e1b1dd2be3b959434232fdce488
Morgan Brown <git@email.mhgbrown.is>
2025-08-25 17:21:15 +0200

    Refine ORCID badge, icon

--- src/routes/(pages)/app/(app)/orcids/[orcidId]/components/orcid-profile-header.svelte
+++ src/routes/(pages)/app/(app)/orcids/[orcidId]/components/orcid-profile-header.svelte
@@ -32,6 +32,9 @@
   import Button from '$lib/components/button/button.svelte';
   import ShareButton from '$lib/components/share-button/share-button.svelte';
   import IdentityBadge from '$lib/components/identity-badge/identity-badge.svelte';
+  // import CopyLinkButton from '$lib/components/copy-link-button/copy-link-button.svelte';
+  // import buildOrcidUrl from '$lib/utils/orcids/build-orcid-url';
+  // import { browser } from '$app/environment';
 
   export let orcid: Orcid;
   export let orcidAccount: OrcidProfileHeaderFragment;
@@ -39,6 +42,7 @@
   export let shareButton: ComponentProps<ShareButton> | undefined = undefined;
 
   $: orcidChainData = filterCurrentChainData(orcidAccount.chainData);
+  // $: currentDomain = browser && window ? window.location.origin : '';
 
   const dispatch = createEventDispatcher<{ editButtonClick: void }>();
 </script>
@@ -46,24 +50,24 @@
 <div class="flex flex-col gap-4 items-start sm:flex-row sm:justify-between relative">
   <div class="max-w-full flex-1 min-w-0 flex flex-col gap-2 sm:flex-row sm:gap-8 sm:items-center">
     <div class="avatar">
-      <OrcidAvatar size="huge" />
+      <OrcidAvatar size="huge" outline/>
     </div>
     <div class="flex-1 min-w-0 flex flex-col gap-1">
       <h1>{orcid.name}</h1>
-      <div style:display="flex" style:gap="0.75rem" style:flex-wrap="wrap">
-        {#if isClaimed(orcidChainData)}
-          <IdentityBadge address={orcidChainData.headerLinkedTo.address} />
-        {/if}
-        <OrcidBadge size="tiny" orcid={orcidAccount} forceUnclaimed tooltip={false} linkTo="external-url" />
-      </div>
       {#if orcid.bio}
         <span
-          class="typo-text-small line-clamp-2 twemoji-text"
-          style:margin-top="0.25rem"
-          style:color="var(--color-foreground-level-4)"
+          class="typo-text-small line-clamp-1 twemoji-text"
+          style:color="var(--color-foreground-level-6)"
           >{@html twemoji(orcid.bio)}
         </span>
       {/if}
+      <div style:display="flex" style:gap="0.75rem" style:margin-top="0.5rem" style:flex-wrap="wrap">
+        {#if isClaimed(orcidChainData)}
+          <IdentityBadge address={orcidChainData.headerLinkedTo.address} />
+        {/if}
+        <OrcidBadge size="tiny" orcid={orcidAccount} forceUnclaimed tooltip={false} linkTo="external-url" outlined copyable/>
+        <!-- <CopyLinkButton url={`${currentDomain}${buildOrcidUrl(orcid.id)}`} variant="muted" /> -->
+      </div>
     </div>
     {#if editButton || shareButton}
       <div class="actions">
