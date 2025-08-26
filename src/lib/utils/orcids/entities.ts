070e0c1d56bb14f8ccbaadffaf5b17d70d43ea57
Morgan Brown <git@email.mhgbrown.is>
2025-08-26 19:31:41 +0200

    Remove some comments, add some comments

--- src/lib/utils/orcids/entities.ts
+++ src/lib/utils/orcids/entities.ts
@@ -1,41 +1,49 @@
-import type { OrcidApiResponse } from "./schemas";
+import type { OrcidApiResponse } from './schemas';
 
-const CLAIMING_URL_NAME = 'DRIPS_OWNERSHIP_CLAIM'
+const CLAIMING_URL_NAME = 'DRIPS_OWNERSHIP_CLAIM';
 
+/**
+ * A convenience class for working with crazily nested
+ * ORCID API data.
+ */
 export default class Orcid {
   data: OrcidApiResponse;
 
   constructor(data: OrcidApiResponse) {
-    this.data = data
+    this.data = data;
   }
 
   get id(): string {
-    return this.data["orcid-identifier"].path
+    return this.data['orcid-identifier'].path;
   }
 
   get name(): string {
-    const name = this.data.person.name
-    return name['given-names']?.value || name['credit-name']?.value || name['family-name']?.value || ''
+    const name = this.data.person.name;
+    return (
+      name['given-names']?.value || name['credit-name']?.value || name['family-name']?.value || ''
+    );
   }
 
   get bio(): string {
-    const biography = this.data.person.biography
-    return biography?.content ?? ''
+    const biography = this.data.person.biography;
+    return biography?.content ?? '';
   }
 
   get url(): string {
-    return this.data['orcid-identifier'].uri
+    return this.data['orcid-identifier'].uri;
   }
 
   get claimingUrl(): string {
-    const urlObj = this.data.person["researcher-urls"]?.["researcher-url"].find(ru => ru["url-name"] === CLAIMING_URL_NAME);
+    const urlObj = this.data.person['researcher-urls']?.['researcher-url'].find(
+      (ru) => ru['url-name'] === CLAIMING_URL_NAME,
+    );
     return urlObj?.url?.value ?? '';
   }
 
   toJSON() {
     return {
       __class: 'Orcid', // A unique identifier for the reviver
-      data: this.data,  // The raw data needed for the constructor
+      data: this.data, // The raw data needed for the constructor
     };
   }
 }
