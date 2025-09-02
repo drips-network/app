d509e45554cf626a60ae01be404171cb2b36805b
Morgan Brown <git@email.mhgbrown.is>
2025-09-02 11:20:01 -0400

    Messy claiming components for ORCID iDs

---
+++ src/lib/flows/claim-orcid-flow/claim-orcid-stepper.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import MultiChain from '$lib/components/illustrations/multi-chain.svelte';
+  import Stepper from '$lib/components/stepper/stepper.svelte';
+  import StandaloneFlowSlots from '$lib/components/standalone-flow-slots/standalone-flow-slots.svelte';
+  import { slotsTemplate, state, steps } from './claim-orcid-flow';
+
+  export let orcidUrl: string | undefined = undefined;
+  export let skipWalletConnect = false;
+  export let displaySlots = false;
+
+  let currentStepIndex = 0;
+
+  const myState = state();
+
+  $: slots = slotsTemplate($myState, currentStepIndex);
+
+  function handleSlotEdit(e: CustomEvent<{ stepIndex: number }>) {
+    currentStepIndex = e.detail.stepIndex;
+  }
+</script>
+
+{#if displaySlots}
+  <StandaloneFlowSlots on:edit={handleSlotEdit} {slots} />
+{/if}
+
+<div class="icon">
+  <MultiChain strokeWidth={6} />
+</div>
+
+<Stepper
+  bind:currentStepIndex
+  on:stepChange={() => window.scrollTo({ top: 0 })}
+  context={() => myState}
+  steps={steps(myState, skipWalletConnect, true, orcidUrl)}
+  minHeightPx={0}
+/>
+
+<style>
+  .icon {
+    margin: 0 auto 0 auto;
+    padding-top: 1.5rem;
+    height: 5rem;
+    width: 5rem;
+  }
+</style>
