4effeb98f99d434896313e810011bcf702fe0536
Morgan Brown <git@email.mhgbrown.is>
2025-09-22 17:04:39 -0700

    Fix up some ORCID utility functions

--- src/lib/utils/orcids/display-name.ts
+++ src/lib/utils/orcids/display-name.ts
@@ -1,12 +1,10 @@
-type orcidWithMetadata = {
-  orcid: string;
-  orcidMetadata?: {
-    givenName?: string | null;
-    familyName?: string | null;
-  } | null;
-};
+import type { OrcidLinkedIdentity } from '$lib/graphql/__generated__/base-types';
 
-export default function getOrcidDisplayName(orcidAccount: orcidWithMetadata) {
+export default function getOrcidDisplayName(
+  orcidAccount: Pick<OrcidLinkedIdentity, 'orcid'> & {
+    orcidMetadata?: Omit<NonNullable<OrcidLinkedIdentity['orcidMetadata']>, '__typename'> | null;
+  },
+) {
   const firstName = orcidAccount.orcidMetadata?.givenName ?? '';
   const lastName = orcidAccount.orcidMetadata?.familyName ?? '';
 
