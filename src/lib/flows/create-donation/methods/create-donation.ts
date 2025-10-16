40d57a9dd56bb6b577c144c1e5ef1efcd84caedb
Morgan Brown <git@email.mhgbrown.is>
2025-10-16 18:18:24 +0200

    Verify one-time donation support for profiles

--- src/lib/flows/create-donation/methods/create-donation.ts
+++ src/lib/flows/create-donation/methods/create-donation.ts
@@ -290,7 +290,6 @@ export default function (
               );
               break;
             }
-            // TODO: Verify!
             case 'AddressDriverAccount': {
               await expect(
                 () =>
