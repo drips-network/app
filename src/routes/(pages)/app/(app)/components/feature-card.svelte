49d150fd4f079378d0aa1b1a17b656c8aa451cef
Georgios Jason Efstathiou <j@jason-e.dev>
2025-10-19 13:07:46 +0200

    fix z-index

--- src/routes/(pages)/app/(app)/components/feature-card.svelte
+++ src/routes/(pages)/app/(app)/components/feature-card.svelte
@@ -52,7 +52,7 @@
     min-width: 27rem;
     margin-top: 7rem;
     padding: 1rem;
-    z-index: 2;
+    z-index: 1;
   }
 
   img {
@@ -63,7 +63,7 @@
     top: 0;
     bottom: 0;
     right: 0;
-    z-index: 1;
+    z-index: 0;
     /* Approximation of an eased gradient in lieau of a css feature for this */
     mask-image: linear-gradient(
       to left,
