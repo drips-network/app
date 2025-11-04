87630c0229b2ac01aeeefaebccbbc5f3507b5242
Morgan Brown <git@email.mhgbrown.is>
2025-11-04 14:48:56 +0100

    First pass claim your project card on default explore page

---
+++ src/routes/(pages)/app/(app)/components/claim-your-project-card.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  import Button from '$lib/components/button/button.svelte';
+  import Plus from '$lib/components/icons/Plus.svelte';
+  import RepoGitProject from '$lib/components/illustrations/repo-git-project.svelte';
+</script>
+
+<div class="claim-card">
+  <div class="illustration">
+    <RepoGitProject />
+  </div>
+  <div class="content">
+    <h2 class="pixelated typo-header-1">Claim your project</h2>
+    <p>If you develop an open-source project, claim it on Drips to start collecting funds.</p>
+  </div>
+  <div class="actions">
+    <Button icon={Plus} href="/app/projects/new" variant="primary">Claim your project</Button>
+  </div>
+</div>
+
+<style>
+  .claim-card {
+    display: flex;
+    align-items: center;
+    gap: 3rem;
+    padding: 2rem 3rem;
+    background-color: var(--color-primary-level-1);
+    border-radius: 1rem;
+    border: 1px solid var(--color-primary-level-2);
+  }
+
+  .illustration {
+    display: contents;
+    color: var(--color-primary);
+  }
+
+  .illustration > :global(svg) {
+    width: 186px;
+    height: 186px;
+  }
+
+  .content {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+  }
+
+  @media (max-width: 768px) {
+    .claim-card {
+      flex-direction: column;
+      text-align: center;
+      gap: 1.5rem;
+      padding: 0 2rem 2rem 2rem;
+    }
+
+    .content {
+      flex: 0;
+    }
+  }
+</style>
