ff7480a6bba538a9e8397f701dbd5ec233dfdc3a
Georgios Jason Efstathiou <j@jason-e.dev>
2025-11-23 22:41:06 +0100

    allow editing rpgf round timestamps

--- src/routes/(pages)/app/(app)/rpgf/rounds/[slugOrId]/settings/schedule/+page.svelte
+++ src/routes/(pages)/app/(app)/rpgf/rounds/[slugOrId]/settings/schedule/+page.svelte
@@ -4,14 +4,14 @@
   import AnnotationBox from '$lib/components/annotation-box/annotation-box.svelte';
   import RpgfSettingsForm from '../../../../components/rpgf-settings-form.svelte';
   import { updateRound } from '$lib/utils/rpgf/rpgf.js';
+  import { onMount } from 'svelte';
 
   export let data;
+  $: round = data.round;
 
   $: published = data.round.published;
 
   let updatedRound = { ...data.round };
-
-  let now = new Date();
 
   $: changesMade =
     updatedRound.applicationPeriodStart?.getTime() !==
@@ -30,16 +30,25 @@
       resultsPeriodStart: updatedRound.resultsPeriodStart,
     });
   }
+
+  let interval: NodeJS.Timeout;
+  let now = new Date();
+  onMount(() => {
+    interval = setInterval(() => {
+      now = new Date();
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
 </script>
 
 <RpgfSettingsForm saveEnabled={changesMade} {saveHandler}>
   <div style:width="100%">
     {#if published}
       <div style:margin-bottom="1rem">
-        <AnnotationBox>
-          The schedule of an ongoing, published round can no longer be changed. Contact the Drips
-          team if necessary.
-        </AnnotationBox>
+        <AnnotationBox>You can only edit timestamps that are in the future.</AnnotationBox>
       </div>
     {/if}
 
@@ -52,43 +61,31 @@
   <FormField
     title="Application intake start*"
     description="From this date onwards, anyone can submit applications."
-    disabled={published}
+    disabled={round.applicationPeriodStart ? round.applicationPeriodStart < now : false}
   >
-    <DateInput
-      bind:value={updatedRound.applicationPeriodStart}
-      min={!published ? now : undefined}
-      timePrecision="minute"
-    />
+    <DateInput bind:value={updatedRound.applicationPeriodStart} min={now} timePrecision="minute" />
   </FormField>
 
   <FormField
     title="Application intake end*"
     description="Applications are no longer accepted. Admins can review pending applications until voting starts."
-    disabled={published}
+    disabled={round.applicationPeriodEnd ? round.applicationPeriodEnd < now : false}
   >
-    <DateInput
-      bind:value={updatedRound.applicationPeriodEnd}
-      min={!published ? now : undefined}
-      timePrecision="minute"
-    />
+    <DateInput bind:value={updatedRound.applicationPeriodEnd} min={now} timePrecision="minute" />
   </FormField>
 
   <FormField
     title="Voting start*"
     description="Badgeholders may now submit votes. Any applications still pending are automatically rejected."
-    disabled={published}
+    disabled={round.votingPeriodStart ? round.votingPeriodStart < now : false}
   >
-    <DateInput
-      bind:value={updatedRound.votingPeriodStart}
-      min={!published ? now : undefined}
-      timePrecision="minute"
-    />
+    <DateInput bind:value={updatedRound.votingPeriodStart} min={now} timePrecision="minute" />
   </FormField>
 
   <FormField
     title="Voting end*"
     description="The round no longer accepts ballot submissions. Admins can review the vote results, make final results public, and prepare the distribution."
-    disabled={published}
+    disabled={round.votingPeriodEnd ? round.votingPeriodEnd < now : false}
   >
     <DateInput bind:value={updatedRound.votingPeriodEnd} min={now} timePrecision="minute" />
   </FormField>
@@ -96,12 +93,8 @@
   <FormField
     title="Distribution start*"
     description="In this phase, admins are expected to pay out rewards based on the round results using Drip Lists."
-    disabled={published}
+    disabled={round.resultsPeriodStart ? round.resultsPeriodStart < now : false}
   >
-    <DateInput
-      bind:value={updatedRound.resultsPeriodStart}
-      min={!published ? now : undefined}
-      timePrecision="minute"
-    />
+    <DateInput bind:value={updatedRound.resultsPeriodStart} min={now} timePrecision="minute" />
   </FormField>
 </RpgfSettingsForm>
