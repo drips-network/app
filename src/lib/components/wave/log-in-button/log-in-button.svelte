9fa07b16b8dd5eabb28e58ddb9bbdb59d15c6633
Georgios Jason Efstathiou <j@jason-e.dev>
2025-11-26 16:18:06 +0100

    support dynamic backTo parameter on gh oauth

--- src/lib/components/wave/log-in-button/log-in-button.svelte
+++ src/lib/components/wave/log-in-button/log-in-button.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
+  import { page } from '$app/state';
   import Button from '$lib/components/button/button.svelte';
   import Github from '$lib/components/icons/Github.svelte';
   import getOptionalEnvVar from '$lib/utils/get-optional-env-var/public';
 
-  let { backTo }: { backTo?: string } = $props();
+  let { backTo = page.url.pathname + page.url.search }: { backTo?: string } = $props();
 
   const WAVE_API_URL = getOptionalEnvVar(
     'PUBLIC_WAVE_API_URL',
--- src/routes/(pages)/wave/(flows)/login/callback/+page.ts
+++ src/routes/(pages)/wave/(flows)/login/callback/+page.ts
@@ -1,11 +1,28 @@
-import { redirect } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 
 import performLogin from './perform-login.js';
+import z from 'zod';
 
 export const load = async ({ url }) => {
   await performLogin(url);
 
-  return redirect(302, `/wave/login?backTo=${url.searchParams.get('backTo') || ''}`);
+  // state param is a base 64 encoded JSON string
+  const decodedState = url.searchParams.get('state')
+    ? atob(url.searchParams.get('state') || '')
+    : null;
+  const stateJson = decodedState ? JSON.parse(decodedState) : null;
+
+  const parsedState = z
+    .object({
+      backTo: z.string().optional().nullable(),
+    })
+    .safeParse(stateJson);
+
+  if (!parsedState.success) {
+    throw error(400, 'Invalid state parameter');
+  }
+
+  return redirect(302, `/wave/login?backTo=${parsedState.data.backTo || ''}`);
 };
 
 export const ssr = false;
