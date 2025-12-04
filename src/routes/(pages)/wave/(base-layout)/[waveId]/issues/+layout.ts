8a2eec6820ee6a92b457ba59132f073fe41daf47
Georgios Jason Efstathiou <j@jason-e.dev>
2025-12-04 18:56:45 +0100

    fix wave issues view breadcrumbs

--- src/routes/(pages)/wave/(base-layout)/[waveId]/issues/+layout.ts
+++ src/routes/(pages)/wave/(base-layout)/[waveId]/issues/+layout.ts
@@ -1,13 +1,21 @@
 import { issuesPageLayoutLoad } from '$lib/components/wave/issues-page/load-fns/issues-page-layout-load.js';
 
-export const load = (context) =>
-  issuesPageLayoutLoad(context, () => ({
+export const load = async (context) => {
+  const { parent } = context;
+  const { wave } = await parent();
+
+  return issuesPageLayoutLoad(context, () => ({
     preappliedFilters: {
       waveId: context.params.waveId,
     },
     pathPrefix: `/wave/${context.params.waveId}/issues/`,
     filtersMode: 'wave',
-    breadcrumbs: [{ label: 'Maintainer Dashboard' }, { label: 'Issues' }],
+    breadcrumbs: [
+      { label: 'Waves', href: `/wave` },
+      { label: wave.name, href: `/wave/${context.params.waveId}` },
+      { label: 'Issues' },
+    ],
     viewKey: context.params.waveId,
     availableSortByOptions: ['updatedAt', 'createdAt', 'points'],
   }));
+};
