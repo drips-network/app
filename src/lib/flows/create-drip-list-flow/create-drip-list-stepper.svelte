546dd50aa9d2d78d72b6f98def0e983481d10a46
Georgios Jason Efstathiou <j@jason-e.dev>
2025-12-12 12:28:09 +0100

    wip

--- src/lib/flows/create-drip-list-flow/create-drip-list-stepper.svelte
+++ src/lib/flows/create-drip-list-flow/create-drip-list-stepper.svelte
@@ -1,30 +1,18 @@
 <script lang="ts">
   import Stepper from '$lib/components/stepper/stepper.svelte';
-  import { slotsTemplate, flowState, steps } from './create-drip-list-flow';
-  import StandaloneFlowSlots from '$lib/components/standalone-flow-slots/standalone-flow-slots.svelte';
+  import { flowState, steps } from './create-drip-list-flow';
 
   interface Props {
     skipWalletConnect?: boolean;
     isModal?: boolean;
-    displaySlots?: boolean;
   }
 
-  let { skipWalletConnect = false, isModal = false, displaySlots = false }: Props = $props();
+  let { skipWalletConnect = false, isModal = false }: Props = $props();
 
   let currentStepIndex = $state(0);
 
   const myState = flowState();
-
-  let slots = $derived(slotsTemplate($myState, currentStepIndex));
-
-  function handleSlotEdit(e: CustomEvent<{ stepIndex: number }>) {
-    currentStepIndex = e.detail.stepIndex;
-  }
 </script>
-
-{#if displaySlots}
-  <StandaloneFlowSlots on:edit={handleSlotEdit} {slots} />
-{/if}
 
 <Stepper
   bind:currentStepIndex
