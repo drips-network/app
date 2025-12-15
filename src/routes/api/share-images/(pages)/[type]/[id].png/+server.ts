05598369daed8cb18a8d3008d9f4b41897d9163b
Georgios Jason Efstathiou <j@jason-e.dev>
2025-12-15 20:19:16 +0100

    perhaps this

--- src/routes/api/share-images/(pages)/[type]/[id].png/+server.ts
+++ src/routes/api/share-images/(pages)/[type]/[id].png/+server.ts
@@ -1,7 +1,12 @@
+import { BASE_URL } from '$lib/utils/base-url.js';
 import renderScreenshot from '../../../../shared/render-screenshot.js';
 
-export const GET = async ({ url }) => {
-  const imageUrl = url.href.replace('.png', '');
+export const GET = async ({ params, url }) => {
+  const { type, id } = params;
+
+  // browser that will render the image is running alongside the app server in the same image, so we do localhost
+  // this will not work in dev mode - but you can still preview the image without the .png in the URL 🤷
+  const imageUrl = `${BASE_URL}/api/share-images/${type}/${encodeURIComponent(id)}?${url.searchParams.toString()}`;
   const imageBuffer = await renderScreenshot(imageUrl);
 
   return new Response(imageBuffer as BodyInit, {
