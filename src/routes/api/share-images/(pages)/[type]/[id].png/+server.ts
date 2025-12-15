171544c8f19b6ce58e8c014ac5fa0d86dd2e6b51
Georgios Jason Efstathiou <j@jason-e.dev>
2025-12-15 20:15:10 +0100

    does this work

--- src/routes/api/share-images/(pages)/[type]/[id].png/+server.ts
+++ src/routes/api/share-images/(pages)/[type]/[id].png/+server.ts
@@ -1,12 +1,7 @@
-import { env } from '$env/dynamic/private';
 import renderScreenshot from '../../../../shared/render-screenshot.js';
 
-export const GET = async ({ params }) => {
-  const { type, id } = params;
-
-  // browser that will render the image is running alongside the app server in the same image, so we do localhost
-  // this will not work in dev mode - but you can still preview the image without the .png in the URL 🤷
-  const imageUrl = `http://127.0.0.1:${env.PORT ?? 8080}/api/share-images-new/${type}/${encodeURIComponent(id)}`;
+export const GET = async ({ url }) => {
+  const imageUrl = url.href.replace('.png', '');
   const imageBuffer = await renderScreenshot(imageUrl);
 
   return new Response(imageBuffer as BodyInit, {
