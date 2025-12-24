24cc9d145075267506abe2334e6b5018dc2aaece
Morgan Brown <git@email.mhgbrown.is>
2025-12-24 17:33:35 +0100

    Simplify rendering in visual renderer

--- src/routes/api/share-images/(pages)/[type]/[id]/VisualRenderer.svelte
+++ src/routes/api/share-images/(pages)/[type]/[id]/VisualRenderer.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import type { Component } from 'svelte';
   import type { VisualBadge } from './types';
   import IdentityBadge from '$lib/components/identity-badge/identity-badge.svelte';
   import DripListBadge from '$lib/components/drip-list-badge/drip-list-badge.svelte';
@@ -13,71 +12,40 @@
   }
 
   let { visual, color }: Props = $props();
-
-  const BADGE_CONFIG: Record<
-    string,
-    {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      component: Component<any>;
-      getProps: (data: unknown) => Record<string, unknown>;
-    }
-  > = {
-    'coin-flying': {
-      component: CoinFlying as unknown as Component<Record<string, unknown>>,
-      getProps: () => ({
-        style: `fill: ${color}; height: 32px; width: 32px;`,
-      }),
-    },
-    'drip-list-icon': {
-      component: DripListIcon as unknown as Component<Record<string, unknown>>,
-      getProps: () => ({
-        style: `fill: ${color}; height: 32px; width: 32px;`,
-      }),
-    },
-
-    identity: {
-      component: IdentityBadge as unknown as Component<Record<string, unknown>>,
-      getProps: (data) => ({
-        address: data as string,
-        showIdentity: false,
-        showAvatar: true,
-        size: 'medium',
-        disableLink: true,
-        disableTooltip: true,
-      }),
-    },
-    'drip-list': {
-      component: DripListBadge as unknown as Component<Record<string, unknown>>,
-      getProps: (data) => ({
-        dripList:
-          data as import('$lib/components/drip-list-badge/__generated__/gql.generated').DripListBadgeFragment,
-        showName: false,
-        showAvatar: true,
-        avatarSize: 'small',
-        disabled: true,
-      }),
-    },
-    ecosystem: {
-      component: EcosystemBadge as unknown as Component<Record<string, unknown>>,
-      getProps: (data) => ({
-        ecosystem:
-          data as import('$lib/components/ecosystem-badge/__generated__/gql.generated').EcosystemBadgeFragment,
-        showName: false,
-        showAvatar: true,
-        avatarSize: 'small',
-        disabled: true,
-      }),
-    },
-  };
 </script>
 
-{#if BADGE_CONFIG[visual.type]}
-  {@const config = BADGE_CONFIG[visual.type]}
-  {@const Badge = config.component}
-  <div class="badge-wrapper">
-    <Badge {...config.getProps(visual.data)} />
-  </div>
-{/if}
+<div class="badge-wrapper">
+  {#if visual.type === 'coin-flying'}
+    <CoinFlying style="fill: {color}; height: 32px; width: 32px;" />
+  {:else if visual.type === 'drip-list-icon'}
+    <DripListIcon style="fill: {color}; height: 32px; width: 32px;" />
+  {:else if visual.type === 'identity'}
+    <IdentityBadge
+      address={visual.data}
+      showIdentity={false}
+      showAvatar={true}
+      size="medium"
+      disableLink={true}
+      disableTooltip={true}
+    />
+  {:else if visual.type === 'drip-list'}
+    <DripListBadge
+      dripList={visual.data}
+      showName={false}
+      showAvatar={true}
+      avatarSize="small"
+      disabled={true}
+    />
+  {:else if visual.type === 'ecosystem'}
+    <EcosystemBadge
+      ecosystem={visual.data}
+      showName={false}
+      showAvatar={true}
+      avatarSize="small"
+      disabled={true}
+    />
+  {/if}
+</div>
 
 <style>
   .badge-wrapper {
