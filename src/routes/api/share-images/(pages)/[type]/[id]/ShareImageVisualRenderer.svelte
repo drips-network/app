d30d901d4b7e52fc8d525d3931492a4956ecd8b9
Morgan Brown <git@email.mhgbrown.is>
2025-12-31 17:32:19 +0200

    Fix display of drip list for stream share image

--- src/routes/api/share-images/(pages)/[type]/[id]/ShareImageVisualRenderer.svelte
+++ src/routes/api/share-images/(pages)/[type]/[id]/ShareImageVisualRenderer.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import type { ShareImageVisual } from './types';
   import IdentityBadge from '$lib/components/identity-badge/identity-badge.svelte';
-  import DripListBadge from '$lib/components/drip-list-badge/drip-list-badge.svelte';
   import EcosystemBadge from '$lib/components/ecosystem-badge/ecosystem-badge.svelte';
   import DripListIcon from '$lib/components/icons/DripList.svelte';
   import CoinFlying from '$lib/components/icons/CoinFlying.svelte';
@@ -29,15 +28,25 @@
       disableTooltip={true}
     />
   {:else if visual.type === 'drip-list'}
-    <DripListBadge
-      dripList={visual.data}
-      showName={true}
-      showOwner={false}
-      isLinked={false}
-      showAvatar={true}
-      avatarSize="small"
-      disabled={true}
-    />
+    <div style="display: flex; align-items: center; gap: 8px;">
+      <div
+        style="
+        height: 32px;
+        width: 32px;
+        background-color: transparent;
+        border-radius: 50%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border: 1px solid {color};
+      "
+      >
+        <DripListIcon style="fill: {color}; height: 80%; width: 80%;" />
+      </div>
+      <span style="font-size: 24px; color: {color}; white-space: nowrap;">
+        {visual.data.name}
+      </span>
+    </div>
   {:else if visual.type === 'ecosystem'}
     <EcosystemBadge
       ecosystem={visual.data}
