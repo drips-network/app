b23241c6b6724b0e84d549f56904461ff4a114a3
Georgios Jason Efstathiou <j@jason-e.dev>
2026-01-11 17:59:07 +0100

    ffs

--- docker/e2e-entrypoint.sh
+++ docker/e2e-entrypoint.sh
@@ -7,13 +7,14 @@ printf " | | |_ | |\/| |\n"
 printf " | |__| | |  | |\n"
 printf "  \_____|_|  |_|\n\n"
 
-printf "\n🏗️ Building app...\n"
-npm run build
-
 # store original PUBLIC_GQL_URL
 ORIGINAL_PUBLIC_GQL_URL=${PUBLIC_GQL_URL}
 
 export PUBLIC_GQL_URL=${PUBLIC_INTERNAL_GQL_URL}
+
+printf "\n🏗️ Building app...\n"
+npm run build
+
 npm run preview -- --host 0.0.0.0 --port 5173
 
 # restore PUBLIC_GQL_URL
