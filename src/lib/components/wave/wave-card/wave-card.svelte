d627481d831147cb83050f849694a4a2b3641c7e
Georgios Jason Efstathiou <j@jason-e.dev>
2026-01-13 19:32:37 +0100

    improve wave card

--- src/lib/components/wave/wave-card/wave-card.svelte
+++ src/lib/components/wave/wave-card/wave-card.svelte
@@ -16,21 +16,50 @@
 
   let { wave, waveProgram }: Props = $props();
   const { status } = $derived(wave);
+
+  function getCountdownText(target: Date): string | null {
+    const now = new Date();
+
+    const todayStart = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    const targetStart = new Date(target.getFullYear(), target.getMonth(), target.getDate());
+
+    const diffTime = targetStart.getTime() - todayStart.getTime();
+
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+    if (diffDays < 0) {
+      return null;
+    } else if (diffDays === 0) {
+      return 'later today';
+    } else if (diffDays === 1) {
+      return 'in 1 day';
+    } else {
+      return `in ${diffDays} days`;
+    }
+  }
 </script>
 
 <Card
-  style="background-color: {status === 'active'
+  style="background-color: {status === 'active' || status === 'upcoming'
     ? 'var(--color-primary-level-1)'
     : 'var(--color-background)'}"
 >
   <div class="wave-card">
     <div class="wave-info">
-      <div class="wave-name">
-        {#if status === 'active'}
+      {#if status === 'active' || status === 'upcoming'}
+        <div class="status-badge typo-text">
           <div class="active-dot">
             <PulsatingCircle />
           </div>
-        {/if}
+
+          {#if status === 'active'}
+            <div class="status-text">Active until {formatDate(wave.endDate, 'onlyDay')}</div>
+          {:else if status === 'upcoming'}
+            <div class="status-text">Starts {getCountdownText(wave.startDate)}</div>
+          {/if}
+        </div>
+      {/if}
+      <div class="wave-name">
         <h2 class="pixelated">Wave {wave.waveNumber}</h2>
       </div>
       <div class="wave-dates">
@@ -70,10 +99,24 @@
     gap: 1rem;
   }
 
-  .wave-name {
+  .wave-info {
+    display: flex;
+    flex-direction: column;
+    gap: 0.25rem;
+  }
+
+  .status-badge {
     display: flex;
     align-items: center;
     gap: 0.5rem;
+    margin-bottom: 0.5rem;
+    background-color: var(--color-background);
+    color: var(--color-primary-level-9);
+    padding: 0.25rem 0.75rem;
+    border-radius: 2rem 0 2rem 2rem;
+    width: fit-content;
+    font-size: 0.875rem;
+    font-weight: 500;
   }
 
   .wave-dates {
