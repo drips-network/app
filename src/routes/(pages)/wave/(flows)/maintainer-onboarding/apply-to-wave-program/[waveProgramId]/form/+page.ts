cf3ce766adc30d2253abcd8c0a38ecd48879e650
Georgios Jason Efstathiou <j@jason-e.dev>
2026-03-05 15:21:20 +0100

    remove redundant load function

--- src/routes/(pages)/wave/(flows)/maintainer-onboarding/apply-to-wave-program/[waveProgramId]/form/+page.ts
+++
@@ -1,9 +0,0 @@
-import { getOwnRepos } from '$lib/utils/wave/orgs';
-
-export const load = async ({ fetch }) => {
-  const ownRepos = await getOwnRepos(fetch, { limit: 100 });
-
-  return {
-    ownRepos,
-  };
-};
