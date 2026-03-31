5813c819c3dd18f670ac8e6bb3aca1b6768f1bcf
Georgios Jason Efstathiou <j@jason-e.dev>
2026-03-31 02:03:22 +0200

    enable gasless TXs for lit-based owner update

--- src/lib/flows/claim-project-flow/steps/set-splits-and-emit-metadata/set-splits-and-emit-metadata.svelte
+++ src/lib/flows/claim-project-flow/steps/set-splits-and-emit-metadata/set-splits-and-emit-metadata.svelte
@@ -189,7 +189,7 @@
         ],
       });
 
-      // On networks without gasless support, the Lit signature timestamp may be
+      // The Lit signature timestamp may be
       // ahead of the chain's latest block. Simulate the tx in a retry loop to
       // wait for the chain to catch up before proposing it to the wallet.
       const { provider } = $walletStore;
@@ -218,10 +218,30 @@
         );
       }
 
+      const { address } = $walletStore;
+      assert(address, 'Wallet address is not defined');
+
       transactions.push(
         {
           title: 'Update repository owner',
           transaction: updateOwnerByLitTx,
+          gasless: $gaslessStore
+            ? {
+                nonceGetter: () => getCallerNonce(address),
+                ERC2771Data: (nonce) => ({
+                  domain: CallerERC2771Domain,
+                  types: CallSignedERC2771Types,
+                  payload: {
+                    sender: $walletStore.address,
+                    target: updateOwnerByLitTx.to,
+                    data: updateOwnerByLitTx.data,
+                    value: '0',
+                    nonce,
+                    deadline: Math.floor(Date.now() / 1000) + 3600, // 1 hour
+                  },
+                }),
+              }
+            : undefined,
           applyGasBuffer: false,
         },
         {
