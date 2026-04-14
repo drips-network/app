5d52ce9defded1ec34f7f7484de276d28342679f
Georgios Jason Efstathiou <j@jason-e.dev>
2026-04-14 19:13:33 +0200

    update scheduled maintenance page

--- src/routes/drippy-is-cooking/+page.svelte
+++ src/routes/drippy-is-cooking/+page.svelte
@@ -2,7 +2,11 @@
 
 <h1>Drippy is cooking...</h1>
 
-<p>...and it's starting to smell good in here. Drips Wave is launching soon!</p>
+<p>
+  We're currently down for brief scheduled maintenance. This shouldn't take long. Follow along at <a
+    href="https://status.drips.network">https://status.drips.network</a
+  >.
+</p>
 
 <a href="https://discord.gg/t8XBXZAEs5" target="_blank" rel="noopener noreferrer"
   >Join our Discord</a
